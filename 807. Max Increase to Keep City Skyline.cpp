d109225369434c0fe5e1aa43f309f05de4704e08
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-26 19:46:41 +0800

    add solution

--- 807. Max Increase to Keep City Skyline.cpp
+++ 807. Max Increase to Keep City Skyline.cpp
@@ -71,6 +71,24 @@ public:
 };
 **/
 
+/**
+Intuition and Algorithm
+
+The skyline looking from the top is col_maxes = [max(column_0), max(column_1), ...]. Similarly, the skyline from the left is row_maxes [max(row_0), max(row_1), ...]
+
+In particular, each building grid[r][c] could become height min(max(row_r), max(col_c)), and this is the largest such height. If it were larger, say grid[r][c] > max(row_r), then the part of the skyline row_maxes = [..., max(row_r), ...] would change.
+
+These increases are also independent (none of them change the skyline), so we can perform them independently.
+
+Complexity Analysis
+
+Time Complexity: O(N^2)O(N 
+2
+ ), where NN is the number of rows (and columns) of the grid. We iterate through every cell of the grid.
+
+Space Complexity: O(N)O(N), the space used by row_maxes and col_maxes.
+**/
+
 //optimized
 class Solution {
 public:
