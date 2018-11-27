29a8bc1e2060152a16c737bb34787a9453619267
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-28 00:43:27 +0800

    add time analysis

--- 654. Maximum Binary Tree.cpp
+++ 654. Maximum Binary Tree.cpp
@@ -61,6 +61,31 @@ The size of the given array will be in the range [1,1000].
 //     }
 // };
 
+/**
+Approach #1 Recursive Solution[Accepted]
+The current solution is very simple. We make use of a function construct(nums, l, r), which returns the maximum binary tree consisting of numbers within the indices ll and rr in the given numsnums array(excluding the r^{th}r 
+th element).
+
+The algorithm consists of the following steps:
+
+Start with the function call construct(nums, 0, n). Here, nn refers to the number of elements in the given numsnums array.
+
+Find the index, max_i, of the largest element in the current range of indices (l:r-1)(l:r−1). Make this largest element, $nums[max_i] as the local root node.
+
+Determine the left child using construct(nums, l, max_i). Doing this recursively finds the largest element in the subarray left to the current largest element.
+
+Similarly, determine the right child using construct(nums, max_i + 1, r).
+
+Return the root node to the calling function.
+
+Complexity Analysis
+
+Time complexity : O(n^2). 
+The function construct is called nn times. At each level of the recursive tree, we traverse over all the nn elements to find the maximum element. In the average case, there will be a log(n) levels leading to a complexity of O(nlog(n)). In the worst case, the depth of the recursive tree can grow upto n, which happens in the case of a sorted numsnums array, giving a complexity of O(n^2).
+
+Space complexity : O(n). The size of the setset can grow upto nn in the worst case. In the average case, the size will be log(n) for nn elements in numsnums, giving an average case complexity of O(log(n))
+**/
+
 //solution by karanrgoswami
 //Your runtime beats 71.37 % of cpp submissions.
 /*
