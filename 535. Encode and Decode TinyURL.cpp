66812af85cc4df6571a260671b313b65da8cc2ac
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-27 23:30:49 +0800

    add problem statement

--- 535. Encode and Decode TinyURL.cpp
+++ 535. Encode and Decode TinyURL.cpp
@@ -1,3 +1,10 @@
+/**
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+**/
+
 //adopted from https://leetcode.com/discuss/interview-question/124658/Design-a-URL-Shortener-(-TinyURL-)-System/
 //Your runtime beats 99.35 % of cpp submissions.
 class Solution {
