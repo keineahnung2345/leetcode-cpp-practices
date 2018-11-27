7f9970307fd847caa4d1035707b254962901d573
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-27 23:53:40 +0800

    804. Unique Morse Code Words.cpp

---
+++ 804. Unique Morse Code Words.cpp
@@ -0,0 +1,79 @@
+/**
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+Example:
+Input: words = ["gin", "zen", "gig", "msg"]
+Output: 2
+Explanation: 
+The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+There are 2 different transformations, "--...-." and "--...--.".
+Note:
+
+The length of words will be at most 100.
+Each words[i] will have length in range [1, 12].
+words[i] will only consist of lowercase letters.
+**/
+
+/**
+Approach #1: Hash Set [Accepted]
+Intuition and Algorithm
+
+We can transform each word into it's Morse Code representation.
+
+After, we put all transformations into a set seen, and return the size of the set.
+
+Java
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] MORSE = new String[]{".-","-...","-.-.","-..",".","..-.","--.",
+                         "....","..",".---","-.-",".-..","--","-.",
+                         "---",".--.","--.-",".-.","...","-","..-",
+                         "...-",".--","-..-","-.--","--.."};
+
+        Set<String> seen = new HashSet();
+        for (String word: words) {
+            StringBuilder code = new StringBuilder();
+            for (char c: word.toCharArray())
+                code.append(MORSE[c - 'a']);
+            seen.add(code.toString());
+        }
+
+        return seen.size();
+    }
+}
+
+Complexity Analysis
+
+Time Complexity: O(S), where S is the sum of the lengths of words in words. We iterate through each character of each word in words.
+
+Space Complexity: O(S).
+**/
+
+//Your runtime beats 85.31 % of cpp submissions.
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        string morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        set<string> uniqueMorses;
+        for(int i = 0; i < words.size(); i++){
+            string s;
+            for(int j = 0; j < words[i].size(); j++){
+                s+=morse[(int)(words[i][j]-'a')];
+            }
+            uniqueMorses.insert(s);
+        }
+        return uniqueMorses.size();
+    }
+};
