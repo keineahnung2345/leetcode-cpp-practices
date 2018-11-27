6b3dc048e41d83a863e0afe57f6426d8390de5bb
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-11-27 23:31:33 +0800

    add problem statement

--- 709. To Lower Case.cpp
+++ 709. To Lower Case.cpp
@@ -1,3 +1,22 @@
+/**
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+**/
+
 //Your runtime beats 100.00 % of cpp submissions.
 class Solution {
 public:
