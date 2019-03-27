5406cf0e7eee724a2050a44877d9de18e6741795
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-27 11:49:39 +0800

    add solutions

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -55,3 +55,137 @@ public:
     }
 };
 
+
+/**
+Solution
+Approach 1: Brute Force
+In this case, we simply calculate the profit corresponding to all the possible sets of transactions and find out the maximum profit out of them.
+**/
+
+/**
+//Java
+class Solution {
+    public int maxProfit(int[] prices) {
+        return calculate(prices, 0);
+    }
+
+    public int calculate(int prices[], int s) {
+        if (s >= prices.length)
+            return 0;
+        int max = 0;
+        for (int start = s; start < prices.length; start++) {
+            int maxprofit = 0;
+            for (int i = start + 1; i < prices.length; i++) {
+                if (prices[start] < prices[i]) {
+                    int profit = calculate(prices, i + 1) + prices[i] - prices[start];
+                    if (profit > maxprofit)
+                        maxprofit = profit;
+                }
+            }
+            if (maxprofit > max)
+                max = maxprofit;
+        }
+        return max;
+    }
+}
+**/
+
+/**
+Complexity Analysis
+
+Time complexity : O(n^n). Recursive function is called n^n times.
+
+Space complexity : O(n). Depth of recursion is nn. 
+**/
+
+/**
+Approach 2: Peak Valley Approach
+Algorithm
+
+Say the given array is:
+
+[7, 1, 5, 3, 6, 4].
+
+If we plot the numbers of the given array on a graph, we get:
+**/
+
+/**
+If we analyze the graph, we notice that the points of interest are the consecutive valleys and peaks.
+
+Mathematically speaking: Total Profit= \sum_{i}(height(peak_i)-height(valley_i)) TotalProfit=∑ i(height(peak i)−height(valley i))
+
+The key point is we need to consider every peak immediately following a valley to maximize the profit. In case we skip one of the peaks (trying to obtain more profit), we will end up losing the profit over one of the transactions leading to an overall lesser profit.
+
+For example, in the above case, if we skip peak_ipeak i and valley_jvalley j trying to obtain more profit by considering points with more difference in heights, the net profit obtained will always be lesser than the one obtained by including them, since CC will always be lesser than A+BA+B.
+**/
+
+/**
+Complexity Analysis
+Time complexity : O(n). Single pass.
+Space complexity : O(1). Constant space required. 
+**/
+
+/**
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        int ans = 0;
+        int valley = prices[0], peak = prices[0];
+        int cur = 0;
+        
+        while(cur < prices.size() - 1){
+            //find valley, it's smaller than its right point
+            while(cur < prices.size() - 1 && prices[cur] >= prices[cur+1]){
+                cur++;
+            }
+            valley = prices[cur];
+            
+            //find peak, it's larger than its right point
+            while(cur < prices.size() - 1 && prices[cur] <= prices[cur+1]){
+                cur++;
+            }
+            peak = prices[cur];
+            ans += (peak - valley);
+        }
+        return ans;
+    }
+};
+**/
+
+/**
+Approach 3: Simple One Pass
+Algorithm
+
+This solution follows the logic used in Approach 2 itself, but with only a slight variation. In this case, instead of looking for every peak following a valley, we can simply go on crawling over the slope and keep on adding the profit obtained from every consecutive transaction. In the end,we will be using the peaks and valleys effectively, but we need not track the costs corresponding to the peaks and valleys along with the maximum profit, but we can directly keep on adding the difference between the consecutive numbers of the array if the second number is larger than the first one, and at the total sum we obtain will be the maximum profit. This approach will simplify the solution. This can be made clearer by taking this example:
+
+[1, 7, 2, 3, 6, 7, 6, 7]
+
+The graph corresponding to this array is:
+**/
+
+/**
+From the above graph, we can observe that the sum A+B+CA+B+C is equal to the difference DD corresponding to the difference between the heights of the consecutive peak and valley.
+**/
+
+/**
+Complexity Analysis
+
+Time complexity : O(n)O(n). Single pass.
+
+Space complexity: O(1)O(1). Constant space needed.
+**/
+
+/**
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        int ans = 0;
+        for(int i = 0; i < prices.size() - 1; i++){
+            ans += max(0, prices[i+1] - prices[i]);
+        }
+        return ans;
+    }
+};
+**/
