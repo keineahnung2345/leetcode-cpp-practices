0d5b8efaba3207a3950d5d7a0cd58c1582f363b3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-28 16:56:22 +0800

    383. Ransom Note.cpp

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,32 @@
+/**
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+**/
+
+//Runtime: 32 ms, faster than 49.26% of C++ online submissions for Ransom Note.
+//Memory Usage: 10.8 MB, less than 98.90% of C++ online submissions for Ransom Note.
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> count(26);
+        
+        for(char c : magazine){
+            count[c-'a'] += 1;
+        }
+        
+        for(char c : ransomNote){
+            count[c-'a'] -= 1;
+            if(count[c-'a'] < 0) return false;
+        }
+        
+        return true;
+    }
+};
