19a79a04ff6b26b7ddd16793278fd1006f58618d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-28 15:14:24 +0800

    add solution

--- 697. Degree of an Array.cpp
+++ 697. Degree of an Array.cpp
@@ -59,3 +59,59 @@ public:
         return ans;
     }
 };
+
+/**
+Approach #1: Left and Right Index [Accepted]
+Intuition and Algorithm
+
+An array that has degree d, must have some element x occur d times. If some subarray has the same degree, then some element x (that occured d times), still occurs d times. The shortest such subarray would be from the first occurrence of x until the last occurrence.
+
+For each element in the given array, let's know left, the index of its first occurrence; and right, the index of its last occurrence. For example, with nums = [1,2,3,2,5] we have left[2] = 1 and right[2] = 3.
+
+Then, for each element x that occurs the maximum number of times, right[x] - left[x] + 1 will be our candidate answer, and we'll take the minimum of those candidates.
+**/
+
+/**
+Complexity Analysis
+
+Time Complexity: O(N)O(N), where NN is the length of nums. Every loop is through O(N)O(N) items with O(1)O(1) work inside the for-block.
+
+Space Complexity: O(N)O(N), the space used by left, right, and count.
+**/
+
+//Runtime: 76 ms, faster than 14.18% of C++ online submissions for Degree of an Array.
+//Memory Usage: 14.4 MB, less than 44.90% of C++ online submissions for Degree of an Array.
+/**
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        map<int, int> left, right, count;
+        int degree = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            int x = nums[i];
+            if(left.find(x) == left.end()){
+                left[x] = i;
+            }
+            right[x] = i;
+            if(count.find(x) == count.end()){
+                count[x] = 1;
+            }else{
+                count[x] += 1;
+            }
+            degree = max(degree, count[x]);
+        }
+        
+        int ans = nums.size();
+        
+        for(map<int, int>::iterator it = count.begin(); it != count.end(); it++){
+            int x = it->first;
+            if(count[x] == degree){
+                ans = min(ans, right[x] - left[x] + 1);
+            }
+        }
+        
+        return ans;
+    }
+};
+**/
