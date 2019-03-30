8a1945eb1afa5311e32a9ac50a6252d2eea0db3b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-03-30 11:28:58 +0800

    add comments

--- 703. Kth Largest Element in a Stream.cpp
+++ 703. Kth Largest Element in a Stream.cpp
@@ -80,13 +80,16 @@ public:
         myk = k;
         for(int e : nums){
             pq.push(e);
+            //pop the smallest value
             if(pq.size() > k) pq.pop();
         }
     }
     
     int add(int val) {
         pq.push(val);
+        //only retain myK elements in pq
         if(pq.size() > myk) pq.pop();
+        //toppest is the smallest
         return pq.top();
     }
 };
