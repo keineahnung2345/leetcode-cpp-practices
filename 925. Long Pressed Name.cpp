23009e881bd6a3748e0e5eba89c81015712c6d97
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-02 08:33:40 +0800

    add solution 1

--- 925. Long Pressed Name.cpp
+++ 925. Long Pressed Name.cpp
@@ -22,3 +22,56 @@ public:
         return true;
     }
 };
+
+/**
+Approach 1: Group into Blocks
+**/
+
+/**
+Complexity Analysis
+
+Time Complexity: O(N+T)O(N+T), where N, TN,T are the lengths of name and typed.
+
+Space Complexity: O(N+T)O(N+T). 
+**/
+
+/**
+class Group{
+public:
+    string key;
+    vector<int> count;
+    Group(string k, vector<int> c){
+        key = k;
+        count = c;
+    }
+};
+
+class Solution {
+public:
+    Group groupify(string s){
+        string key;
+        vector<int> count;
+        int anchor = 0;
+        int N = s.size();
+        for(int i = 0; i < N; i++){
+            if(i == N-1 || s[i] != s[i+1]){
+                key += s[i];
+                count.push_back(i - anchor + 1);
+                anchor = i+1;
+            }
+        }
+        return Group(key, count);
+    }
+    bool isLongPressedName(string name, string typed) {
+        Group g1 = groupify(name);
+        Group g2 = groupify(typed);
+        if(g1.key != g2.key) return false;
+        
+        for(int i = 0; i < g1.count.size(); i++){
+            if(g2.count[i] < g1.count[i]) return false;
+        }
+        return true;
+    }
+};
+**/
+
