80520f4986b799ea5ff18a810c0d4742af02ba2d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-04 11:56:19 +0800

    add solution 1

--- 849. Maximize Distance to Closest Person.cpp
+++ 849. Maximize Distance to Closest Person.cpp
@@ -33,3 +33,65 @@ public:
         return ans;
     }
 };
+
+/**
+Approach #1: Next Array [Accepted]
+first calculate the distance to left/right person
+**/
+
+/**
+time: O(N), space: O(N)
+**/
+
+//Runtime: 16 ms, faster than 93.26% of C++ online submissions for Maximize Distance to Closest Person.
+//Memory Usage: 10.7 MB, less than 19.28% of C++ online submissions for Maximize Distance to Closest Person.
+
+/**
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int N = seats.size();
+        //the distance of a seat to its left/right person
+        //note that we initialize the two vectors with value N
+        
+        //for [1,0,0,0]:
+        //right is [0,6,5,4]
+        //left is [0,1,2,3]
+        //and we use min(left, right), so when we look at the area behind the last 1, right is invalid
+        
+        //for [0,0,0,1]
+        //left is [4,5,6,0]
+        //right is [3,2,1,0]
+        //similarily, left is invalid when looking at the area behore the first 1
+        
+        vector<int> left(N, N), right(N, N);
+        
+        for(int i = 0; i < N; i++){
+            if(seats[i] == 1) left[i] = 0;
+            else if(i > 0) left[i] = left[i-1] + 1;
+        }
+        
+        for (int i = N-1; i >= 0; --i) {
+            if (seats[i] == 1) right[i] = 0;
+            else if(i < N-1) right[i] = right[i+1] + 1;
+        }
+        
+//         for(int i = 0; i < N; i++){
+//             cout << left[i] << " ";
+//         }
+//         cout << endl;
+        
+//         for(int i = 0; i < N; i++){
+//             cout << right[i] << " ";
+//         }
+//         cout << endl;
+        
+        int ans = 0;
+        for(int i = 0; i < N; i++){
+            if(seats[i] == 0)
+                ans = max(ans, min(left[i], right[i]));
+        }
+        return ans;
+    }
+};
+**/
