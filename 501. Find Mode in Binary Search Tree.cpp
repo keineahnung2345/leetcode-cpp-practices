3849e7f3ad4baab9546814f944181480f33d9061
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-04 16:27:33 +0800

    add solution from discussion

--- 501. Find Mode in Binary Search Tree.cpp
+++ 501. Find Mode in Binary Search Tree.cpp
@@ -47,3 +47,45 @@ public:
         return ans;
     }
 };
+
+/**Without map**/
+//https://leetcode.com/problems/find-mode-in-binary-search-tree/discuss/98100/Java-4ms-Beats-100-Extra-O(1)-solution-No-Map
+
+class Solution {
+public:
+    vector<int> ans;
+    int prev = INT_MIN;
+    int curcount = 1; //since we only update curcount when it's not first node
+    int maxcount = 0;
+    
+    void traverse(TreeNode* root){
+        if(!root) return;
+        traverse(root->left);
+        //we only update curcount when it's not first node
+        if(prev != INT_MIN){
+            if(root->val == prev){
+                curcount++;
+            }else{
+                curcount = 1;
+            }
+        }
+        prev = root->val;
+        if(curcount > maxcount){
+            maxcount = curcount;
+            ans.clear();
+            // cout << "if: " << curcount << " " << maxcount << " " << root->val << endl;
+            ans.push_back(root->val);
+        }else if(curcount == maxcount){
+            // cout << "else: " << curcount << " " << maxcount << " " << root->val << endl;
+            ans.push_back(root->val);
+        }
+        traverse(root->right);
+    }
+    
+    vector<int> findMode(TreeNode* root) {
+        if(!root) return vector<int>();
+        traverse(root);
+        cout << endl;
+        return ans;
+    }
+};
