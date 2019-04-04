ac044d41da3cdfb8cd1726411cb316c16f47875a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-04 17:45:37 +0800

    205. Isomorphic Strings.cpp

---
+++ 205. Isomorphic Strings.cpp
@@ -0,0 +1,28 @@
+//Runtime: 24 ms, faster than 8.57% of C++ online submissions for Isomorphic Strings.
+//Memory Usage: 9.3 MB, less than 7.37% of C++ online submissions for Isomorphic Strings.
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        //need to ensure 1-to-1 mapping,
+        //so we need to maps
+        map<char, char> m, im;
+        set<char> values;
+        for(int i = 0; i < s.size(); i++){
+            if(m.find(s[i]) == m.end()){
+                m[s[i]] = t[i];
+            }else if(m[s[i]] != t[i]){
+                return false;
+            }
+            if(im.find(t[i]) == im.end()){
+                im[t[i]] = s[i];
+            }else if(im[t[i]] != s[i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
