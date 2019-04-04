f257637b3c71b6154b6d8a0d32b57061f2f0d32b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-04 17:56:31 +0800

    add solution from discussion

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -26,3 +26,24 @@ public:
         return true;
     }
 };
+
+//https://leetcode.com/problems/isomorphic-strings/discuss/57796/My-6-lines-solution
+/**
+indirect mapping
+**/
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<int> ms(128, 0), mt(128, 0);
+        
+        for(int i = 0; i < s.size(); i++){
+            if(ms[s[i]] != mt[t[i]]){
+                return false;
+            }
+            ms[s[i]] = i+1;
+            mt[t[i]] = i+1;
+        }
+        
+        return true;
+    }
+};
