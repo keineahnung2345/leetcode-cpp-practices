a83f13edd3aaa7804faf3a6d73a0eaf65a0c2289
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-17 09:26:29 +0800

    add solution

--- 155. Min Stack.cpp
+++ 155. Min Stack.cpp
@@ -36,3 +36,44 @@ public:
  * int param_4 = obj->getMin();
  */
  
+//https://leetcode.com/problems/min-stack/discuss/49014/Java-accepted-solution-using-one-stack
+//Runtime: 36 ms, faster than 84.44% of C++ online submissions for Min Stack.
+//Memory Usage: 16.7 MB, less than 99.73% of C++ online submissions for Min Stack.
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> stk;
+    int m = INT_MAX;
+    MinStack() {
+    }
+    
+    void push(int x) {
+        if(x <= m){ //note it's <=, not <!
+            //2nd smallest value is under 1st smallest value
+            stk.push(m);
+            m = x;
+        }
+        stk.push(x);
+    }
+    
+    void pop() {
+        int tmp = stk.top();
+        stk.pop();
+        //pop 2nd smallest value along 1st smallest value
+        if(tmp == m){
+            //when pop 1st smallest value
+            //update m to 2nd smallest value
+            m = stk.top();
+            stk.pop();
+        }
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return m;
+    }
+};
