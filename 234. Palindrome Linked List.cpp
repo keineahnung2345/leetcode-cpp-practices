cf91de1dc1dd36ab5d462b1f40949c1255b0b041
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-17 11:17:24 +0800

    slow and fast pointer

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -24,3 +24,62 @@ public:
         return true;
     }
 };
+
+//https://leetcode.com/problems/palindrome-linked-list/discuss/64501/Java-easy-to-understand
+//slow and fast pointer
+//Runtime: 24 ms, faster than 98.19% of C++ online submissions for Palindrome Linked List.
+//Memory Usage: 12.6 MB, less than 68.90% of C++ online submissions for Palindrome Linked List.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        if(fast != NULL){
+            //the length of list is odd
+            //in this situation we should ignore the middle node
+            slow = slow->next;
+        }
+        
+        //slow: later part of original list
+        //fast: former part of original list
+        slow = reverse(slow);
+        fast = head;
+        
+        while(slow != NULL){
+            if(slow->val != fast->val) return false;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
+        return true;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        //reverse the later part of original list
+        ListNode* prev = NULL;
+        ListNode* tmp;
+        while(head != NULL){
+            tmp = head->next;
+            head->next = prev;
+            prev = head;
+            head = tmp;
+        }
+        //when the loop finishes, head is NULL, 
+        //and prev is the last node in the original list
+        return prev;
+    }
+};
