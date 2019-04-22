8d5237ba3f71768dc011eaa85dbe2cff7e1faf97
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-22 09:27:01 +0800

    add solution from discussion

--- 211. Add and Search Word - Data structure design.cpp
+++ 211. Add and Search Word - Data structure design.cpp
@@ -175,4 +175,67 @@ public:
  * obj->addWord(word);
  * bool param_2 = obj->search(word);
  */
- 
+
+//solution from discussion
+//https://leetcode.com/problems/add-and-search-word-data-structure-design/discuss/59554/My-simple-and-clean-Java-code
+//Runtime: 192 ms, faster than 22.70% of C++ online submissions for Add and Search Word - Data structure design.
+//Memory Usage: 111.5 MB, less than 6.70% of C++ online submissions for Add and Search Word - Data structure design.
+
+class TrieNode{
+public:
+    vector<TrieNode*> links;
+    string item;
+    
+    TrieNode(){
+        links = vector<TrieNode*>(26);
+        item = "";
+    }
+};
+
+class WordDictionary {
+public:
+    TrieNode* root = new TrieNode();
+    
+    /** Initialize your data structure here. */
+    WordDictionary() {
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        TrieNode* node = root;
+        for(char c : word){
+            if(node->links[c-'a'] == NULL){
+                node->links[c-'a'] = new TrieNode();
+            }
+            node = node->links[c-'a'];
+        }
+        //mark it as end
+        node->item = word;
+    }
+    
+    bool match(string word, int depth, TrieNode* node){
+        //node->item != "" means the node is end
+        if(depth == word.size()) return (node->item != "");
+        char c = word[depth];
+        // cout << depth << " " << c << endl;
+        if(c != '.'){
+            //recursive search
+            return (node->links[c-'a'] != NULL) && match(word, depth+1, node->links[c-'a']);
+        }else{
+            //search for all its children
+            for(int i = 0; i < node->links.size(); i++){
+                if(node->links[i] != NULL){
+                    if(match(word, depth+1, node->links[i])){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        return match(word, 0, root);
+    }
+};
