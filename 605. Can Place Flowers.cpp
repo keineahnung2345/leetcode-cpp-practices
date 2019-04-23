fecdca147baba3daaa25b9020403b21135880a4f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-04-23 11:31:34 +0800

    add solution

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -27,3 +27,26 @@ public:
         return false;
     }
 };
+
+//single scan & optimized
+//Runtime: 20 ms, faster than 91.12% of C++ online submissions for Can Place Flowers.
+//Memory Usage: 10.1 MB, less than 100.00% of C++ online submissions for Can Place Flowers.
+//time: O(n), space: O(1)
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for(int i = 0; i < flowerbed.size(); i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.size()-1 || flowerbed[i+1] ==0)) {
+                //case 1: i == 0 && i == flowerbed.size()-1(length 1, empty flowerbed)
+                //case 2: i == 0 && flowerbed[i+1] == 0(0th and 1st places are empty)
+                //case 3: flowerbed[i-1] == 0 && i == flowerbed.size()-1(last and 2nd last places are empty)
+                //case 4: flowerbed[i-1] == 0 && flowerbed[i+1] == 0(middle empty place)
+                flowerbed[i] = 1;
+                count ++;
+            }
+            if(count >= n) return true;
+        }
+        return count >= n;
+    }
+};
