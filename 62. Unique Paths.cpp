775d03df44e841452b002469c3659988af71e35a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-14 08:47:23 +0800

    62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,28 @@
+//Runtime: 4 ms, faster than 54.06% of C++ online submissions for Unique Paths.
+//Memory Usage: 8.6 MB, less than 51.56% of C++ online submissions for Unique Paths.
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > grid(m, vector<int>(n));
+        // cout << m << " " << n << endl;
+        //range of the length of diagonal: 0 ~ (max possible i) + (max possible j)
+        for(int diag = 0; diag <= (n-1) + (m-1); diag++){
+            //i starts from the first position that j >= 0
+            //i is increased in the loop and its upper bound is m-1
+            //j + i must = diag
+            //j is decreased in the loop and its lower bound is 0
+            //dynamic programming
+            for(int i = max(0, diag-(n-1)), j = diag - i; i <= m-1 && j >= 0; i++, j--){
+                // cout << i << " " << j << endl;
+                if(i == 0 || j == 0){
+                    grid[i][j] = 1;
+                }else{
+                    grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                }
+            }
+        }
+        
+        return grid[m-1][n-1];
+    }
+};
