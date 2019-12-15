39e903456aacdae8d370c9c864da0fac441b66f1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-15 21:31:31 +0800

    add solution from discussion

--- 1255. Maximum Score Words Formed by Letters.cpp
+++ 1255. Maximum Score Words Formed by Letters.cpp
@@ -87,3 +87,65 @@ public:
         return maxScore;
     }
 };
+
+//Runtime: 48 ms, faster than 20.85% of C++ online submissions for Maximum Score Words Formed by Letters.
+//Memory Usage: 36.2 MB, less than 100.00% of C++ online submissions for Maximum Score Words Formed by Letters.
+//https://leetcode.com/problems/maximum-score-words-formed-by-letters/discuss/425104/Detailed-Explanation-using-Recursion
+class Solution {
+public:
+    int maxScore = INT_MIN;
+    vector<bool> taken;
+    vector<int> count;
+    //same as the argument of maxScoreWords
+    vector<string> words;
+    vector<int> score;
+    
+    void updateScore(){
+        int curScore = 0;
+        vector<int> curCount = vector<int>(26, 0);
+        for(int i = 0; i < words.size(); i++){
+            if(taken[i]){
+                for(char c : words[i]){
+                    // cout << "curCount: " << curCount[c-'a'] << endl;
+                    // cout << "count: " << count[c-'a'] << endl;
+                    // cout << "curScore: " << curScore << endl;
+                    curCount[c-'a']++;
+                    if(curCount[c-'a'] > count[c-'a']){
+                        return;
+                    }
+                    curScore += score[c-'a'];
+                }
+            }
+        }
+        maxScore = max(maxScore, curScore);
+    };
+    
+    void generateSubset(int n){
+        //every time a subset("taken") is generated, 
+        //call updateScore to calculate the score of current combination,
+        //and update maxScore
+        if(n == 0){
+            updateScore();
+            return;
+        }
+        
+        taken[n-1] = true;
+        generateSubset(n-1);
+        
+        taken[n-1] = false;
+        generateSubset(n-1);
+    };
+    
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+        taken = vector<bool>(words.size(), false);
+        count = vector<int>(26, 0);
+        for(char c : letters){
+            count[c-'a']++;
+        }
+        this->words = words;
+        this->score = score;
+        
+        generateSubset(words.size());
+        return maxScore;
+    }
+};
