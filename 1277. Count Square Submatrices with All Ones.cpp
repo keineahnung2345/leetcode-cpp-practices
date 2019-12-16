e886b82c55aa610e72f93a0911f0cf589ad1c0d6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-16 14:33:05 +0800

    DP solution

--- 1277. Count Square Submatrices with All Ones.cpp
+++ 1277. Count Square Submatrices with All Ones.cpp
@@ -28,3 +28,26 @@ public:
         return count;
     }
 };
+
+//https://leetcode.com/problems/count-square-submatrices-with-all-ones/discuss/441306/Python-DP-solution
+//Runtime: 76 ms, faster than 36.81% of C++ online submissions for Count Square Submatrices with All Ones.
+//Memory Usage: 16 MB, less than 100.00% of C++ online submissions for Count Square Submatrices with All Ones.
+
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int count = 0;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i > 0 && j > 0 && matrix[i][j]){
+                    matrix[i][j] = min({matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]}) + 1;
+                }
+                //matrix[i][j]: the number of squares(of any size) whose right-bottom corner is at (i, j)
+                count += matrix[i][j];
+            }
+        }
+        return count;
+    }
+};
