6548d24dc3145aacb35a02d907160a7d6ad5d73f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-12-17 18:05:21 +0800

    986. Interval List Intersections.cpp

---
+++ 986. Interval List Intersections.cpp
@@ -0,0 +1,59 @@
+//Runtime: 52 ms, faster than 77.74% of C++ online submissions for Interval List Intersections.
+//Memory Usage: 15.7 MB, less than 100.00% of C++ online submissions for Interval List Intersections.
+
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        int i = 0, j = 0;
+        vector<vector<int>> intersection;
+        
+        while(i < A.size() && j < B.size()){
+            // cout << i << " " << j << endl;
+            vector<int> &block1 = A[i], &block2 = B[j];
+            if(block1[0] <= block2[0] && block1[1] >= block2[1]){
+                //block1 contains block2
+                intersection.push_back(block2);
+            }else if(block1[0] >= block2[0] && block1[1] <= block2[1]){
+                //block2 contains block1
+                intersection.push_back(block1);
+            }else if(min(block1[1] - block2[0], block2[1] - block1[0]) >= 0){
+                //block1 and block2 share one part
+                if(block1[1] - block2[0] <= block2[1] - block1[0]){
+                    intersection.push_back({block2[0], block1[1]});
+                }else{
+                    intersection.push_back({block1[0], block2[1]});
+                }
+            }
+            //wrong!
+            // if(block1[0] < block2[0]){
+            //     i++;
+            // }else if(block1[0] > block2[0]){
+            //     j++;
+            // }else if(block1[1] > block2[1]){
+            //     //two blocks' left boundaries are the same
+            //     j++;
+            // }else{
+            //     i++;
+            // }
+            
+            if(block1[1] < block2[1]){
+                //move the one whose right boundary is smaller
+                i++;
+            }else if(block1[1] > block2[1]){
+                //move the one whose right boundary is smaller
+                j++;
+            }else if(block1[0] <= block2[0]){
+                //two blocks' right boundaries match
+                //, then move the one whose left boundary is smaller
+                i++;
+            }else{
+                //two blocks' right boundaries match
+                //, then move the one whose left boundary is smaller
+                j++;
+            }
+        }
+        // cout << endl;
+        
+        return intersection;
+    }
+};
