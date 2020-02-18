2af4b793e8ebf53cae58bcc11da6222e19f76fcb
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-18 10:54:52 +0800

    add solution

--- 1114. Print in Order.cpp
+++ 1114. Print in Order.cpp
@@ -33,3 +33,43 @@ public:
         count += 1;
     }
 };
+
+//https://leetcode.com/problems/print-in-order/discuss/343384/C%2B%2BWhy-most-of-the-solutions-using-mutex-are-wrong%2Bsolution
+//Runtime: 152 ms, faster than 63.21% of C++ online submissions for Print in Order.
+//Memory Usage: 9.3 MB, less than 100.00% of C++ online submissions for Print in Order.
+class Foo {
+public:
+    int count;
+    mutex mtx;
+    condition_variable cv;
+    
+    Foo() {
+        count = 1;
+    }
+
+    void first(function<void()> printFirst) {
+        unique_lock<mutex> lck(mtx);
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        count += 1;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond) {
+        unique_lock<mutex> lck(mtx);
+        // printSecond() outputs "second". Do not change or remove this line.
+        cv.wait(lck, [this](){return count == 2;});
+        printSecond();
+        count += 1;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) {
+        unique_lock<mutex> lck(mtx);
+        // printThird() outputs "third". Do not change or remove this line.
+        cv.wait(lck, [this](){return count == 3;});
+        printThird();
+        count += 1;
+        cv.notify_all();
+    }
+};
