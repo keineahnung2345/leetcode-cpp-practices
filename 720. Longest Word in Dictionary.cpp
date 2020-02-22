40ed71e254c5613cfdc5e614213f99840558c05f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 23:23:29 +0800

    720. Longest Word in Dictionary.cpp

---
+++ 720. Longest Word in Dictionary.cpp
@@ -0,0 +1,45 @@
+/*
+WA
+success:
+["w","wo","wor","worl","world"]
+["a", "banana", "app", "appl", "ap", "apply", "apple"]
+["m","mo","moc","moch","mocha","l","la","lat","latt","latte","c","ca","cat"]
+["yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"]
+["ogz","eyj","e","ey","hmn","v","hm","ogznkb","ogzn","hmnm","eyjuo","vuq","ogznk","og","eyjuoi","d"]
+
+fail:
+["rac","rs","ra","on","r","otif","o","onpdu","rsf","rs","ot","oti","racy","onpd"]
+*/
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());
+        copy(words.begin(), words.end(), ostream_iterator<string>(cout, " "));
+        cout << endl;
+        string acc = words[0].size() == 1 ? words[0] : "-";
+        vector<string> candidates;
+        for(int i = 1; i < words.size(); i++){
+            //the accumulation process doesn't start or it ends
+            if(acc == "-" || words[i].size() <= acc.size()){
+                if(acc != "-") candidates.push_back(acc);
+                acc = words[i].size() == 1 ? words[i] : "-";
+            }
+            if(words[i].size() == acc.size()+1 && words[i].rfind(acc, 0) == 0){
+                acc = words[i];
+            }
+        }
+        candidates.push_back(acc);
+        copy(candidates.begin(), candidates.end(), ostream_iterator<string>(cout, " "));
+        cout << endl;
+        
+        sort(candidates.begin(), candidates.end(), 
+             [](const string& a, const string& b) -> bool{
+                 //first sort by length and then by lexicographical order
+                 //if length are the same, check their lexicographical order
+                 //ow, directly sort by their length
+                 return (a.size() == b.size()) ? a < b : a.size() > b.size();
+             }
+        );
+        return candidates[0];
+    }
+};
