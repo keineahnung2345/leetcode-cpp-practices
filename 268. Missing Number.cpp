114303b2f6e90abcea34a09fe9becf24cdf661f4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-22 12:42:10 +0800

    add xor solution

--- 268. Missing Number.cpp
+++ 268. Missing Number.cpp
@@ -14,3 +14,22 @@ public:
         return full[0];
     }
 };
+
+//https://leetcode.com/problems/missing-number/discuss/69777/C%2B%2B-solution-using-bit-manipulation
+//Runtime: 24 ms, faster than 80.72% of C++ online submissions for Missing Number.
+//Memory Usage: 9.9 MB, less than 70.59% of C++ online submissions for Missing Number.
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = 0;
+        //exclusive-or from (a) [0 to n] and (b) [all number in "nums"]
+        //same numbers in (a) and (b) will counteract each other
+        //so it will remain 0 ^ the missing number = the missing nuber
+        for(int i = 0; i < nums.size(); i++){
+            result ^= nums[i];
+            result ^= i;
+        }
+        result ^= nums.size();
+        return result;
+    }
+};
