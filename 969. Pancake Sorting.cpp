7db0904f214f457e46de0b300e1dc0ab276ada5b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-25 21:28:14 +0800

    969. Pancake Sorting.cpp

---
+++ 969. Pancake Sorting.cpp
@@ -0,0 +1,37 @@
+//Approach 1: Sort Largest to Smallest
+//Runtime: 8 ms, faster than 69.75% of C++ online submissions for Pancake Sorting.
+//Memory Usage: 8.7 MB, less than 100.00% of C++ online submissions for Pancake Sorting.
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& A) {
+        vector<int> ans;
+        
+        //skip curmax = 1 because 1 must be in correct place 
+        // when all other elements all in correct place
+        for(int curmax = A.size(); curmax >= 2; curmax--){
+            int index = find(A.begin(), A.end(), curmax) - A.begin();
+            //make index 1-based
+            index += 1;
+            // cout << curmax << ", " << index << endl;
+            
+            if(index > 1){
+                //"last" is exclusive, so A[0], ..., A[index-1]
+                reverse(A.begin(), A.begin() + index);
+                // copy(A.begin(), A.end(), ostream_iterator<int>(cout, " "));
+                // cout << endl;
+                ans.push_back(index);
+            }
+            
+            if(curmax > 1){
+                //A[0], ..., A[curmax-1]
+                reverse(A.begin(), A.begin() + curmax);
+                // copy(A.begin(), A.end(), ostream_iterator<int>(cout, " "));
+                // cout << endl;
+                ans.push_back(curmax);
+            }
+            
+        }
+        
+        return ans;
+    }
+};
