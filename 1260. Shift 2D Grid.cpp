4ab467f4cc78e0af5431e829c4a8ded77c3e5391
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-25 23:35:28 +0800

    1260. Shift 2D Grid.cpp

---
+++ 1260. Shift 2D Grid.cpp
@@ -0,0 +1,46 @@
+//Runtime: 88 ms, faster than 23.45% of C++ online submissions for Shift 2D Grid.
+//Memory Usage: 14.4 MB, less than 100.00% of C++ online submissions for Shift 2D Grid.
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<pair<int, int>>> pos(m, vector<pair<int, int>>(n));
+        vector<vector<int>> ans(m, vector<int>(n));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                pos[i][j] = make_pair(i, j);
+            }
+        }
+        
+        //calculate each index is shifted to where
+        for(int time = 0; time < k; time++){
+            // cout << "time: " << time << endl;
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    // cout << pos[i][j].first << ", " << pos[i][j].second << endl;
+                    if(pos[i][j].second < n-1){
+                        //(i,j)->(i,j+1)
+                        pos[i][j].second += 1;
+                    }else if(pos[i][j].first < m-1){
+                        //(i,n-1)->(i+1,0)
+                        pos[i][j] = make_pair(pos[i][j].first+1, 0);
+                    }else{
+                        //(m-1,n-1) -> (0,0)
+                        pos[i][j] = make_pair(0,0);
+                    }
+                }
+            }
+        }
+        
+        //(pos[i][j].first, pos[i][j].second): new position of original (i,j)
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                //(i, j) now move to (pos[i][j].first, pos[i][j].second)
+                ans[pos[i][j].first][pos[i][j].second] = grid[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
