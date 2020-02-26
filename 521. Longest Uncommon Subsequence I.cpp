6e731778ca5fd66c65d6a3afde94bc73504dbc1d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-26 13:11:40 +0800

    complexity

--- 521. Longest Uncommon Subsequence I.cpp
+++ 521. Longest Uncommon Subsequence I.cpp
@@ -1,3 +1,4 @@
+//time: O(min(x,y)), space: O(1)
 //Runtime: 0 ms, faster than 100.00% of C++ online submissions for Longest Uncommon Subsequence I .
 //Memory Usage: 8.5 MB, less than 40.00% of C++ online submissions for Longest Uncommon Subsequence I .
 class Solution {
