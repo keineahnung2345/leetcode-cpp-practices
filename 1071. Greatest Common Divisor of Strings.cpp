cc9fe24c8eaf1c50771e1151bd6f505b5e3275eb
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-26 20:17:27 +0800

    1071. Greatest Common Divisor of Strings.cpp

---
+++ 1071. Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,43 @@
+//Runtime: 4 ms, faster than 91.98% of C++ online submissions for Greatest Common Divisor of Strings.
+//Memory Usage: 9.1 MB, less than 100.00% of C++ online submissions for Greatest Common Divisor of Strings.
+
+class Solution {
+public:
+    int greatestCommonDivisor(int x, int y){
+        if(x < y){
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+        
+        while(y != 0){
+            int org_y = y;
+            y = x % y;
+            x = org_y;
+        }
+        
+        return x;
+    }
+    string gcdOfStrings(string str1, string str2) {
+        int l1 = str1.size(), l2 = str2.size();
+        int gcd = greatestCommonDivisor(l1, l2);
+        string sgcd = str1.substr(0, gcd);
+        // cout << sgcd << endl;
+            
+        // cout << "str1" << endl;
+        for(int i = 0; i < l1/gcd; i++){
+            // cout << i*gcd << endl;
+            if(str1.substr(i*gcd, gcd) != sgcd)
+                return "";
+        }
+          
+        // cout << "str2" << endl;
+        for(int i = 0; i < l2/gcd; i++){
+            // cout << i*gcd << endl;
+            if(str2.substr(i*gcd, gcd) != sgcd)
+                return "";
+        }
+        
+        return sgcd;
+    }
+};
