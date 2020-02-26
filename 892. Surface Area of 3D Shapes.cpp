c3add96d2156b3afb7aac532c81f6467ab517626
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-26 11:36:17 +0800

    add official sol

--- 892. Surface Area of 3D Shapes.cpp
+++ 892. Surface Area of 3D Shapes.cpp
@@ -30,3 +30,38 @@ public:
         return ud + lat;
     }
 };
+
+//official sol
+//Runtime: 12 ms, faster than 27.88% of C++ online submissions for Surface Area of 3D Shapes.
+//Memory Usage: 9.3 MB, less than 80.00% of C++ online submissions for Surface Area of 3D Shapes.
+//time: O(N^2), space: O(1)
+
+class Solution {
+public:
+    int surfaceArea(vector<vector<int>>& grid) {
+        vector<pair<int, int>> dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        int n = grid.size();
+        int ans = 0;
+        
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                if(grid[r][c]){
+                    ans += 2;
+                    for(pair<int, int> dir : dirs){
+                        //nv : neighboring grid's value
+                        int nr = r + dir.first;
+                        int nc = c + dir.second;
+                        int nv = 0;
+                        if(nr >= 0 && nr < n && nc >= 0 && nc < n)
+                            nv = grid[nr][nc];
+                        ans += max(grid[r][c] - nv, 0);
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
