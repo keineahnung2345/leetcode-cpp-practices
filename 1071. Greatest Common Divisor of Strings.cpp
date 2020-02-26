c9cb9bdc62d54ba830a4c4337dd0206288e86bbd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-26 20:37:53 +0800

    regex sol

--- 1071. Greatest Common Divisor of Strings.cpp
+++ 1071. Greatest Common Divisor of Strings.cpp
@@ -41,3 +41,24 @@ public:
         return sgcd;
     }
 };
+
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/discuss/303781/JavaPython-3-3-codes-each%3A-Recursive-iterative-and-regex-w-brief-comments-and-analysis.
+//Method 3: Regular Expression
+//Runtime: 16 ms, faster than 14.18% of C++ online submissions for Greatest Common Divisor of Strings.
+//Memory Usage: 13.8 MB, less than 100.00% of C++ online submissions for Greatest Common Divisor of Strings.
+
+class Solution {
+public:
+    int greastestCommonDivisor(int x, int y){
+        return y == 0 ? x : greastestCommonDivisor(y, x%y);
+    }
+    
+    string gcdOfStrings(string str1, string str2) {
+        int l1 = str1.size(), l2 = str2.size();
+        int gcd = greastestCommonDivisor(l1, l2);
+        string sgcd = str1.substr(0, gcd);
+        // regex pattern = regex("(" + sgcd + ")+");
+        regex pattern(("(" + sgcd + ")+"));
+        return  regex_match(str1, pattern) && regex_match(str2, pattern) ? sgcd: "";
+    }
+};
