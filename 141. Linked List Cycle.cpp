7f3c9e44ddd7172320a4da55afb8b3f421b1201a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-28 23:23:11 +0800

    slow fast pointer

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -28,3 +28,31 @@ public:
         return false;
     }
 };
+
+//Approach 2: Two Pointers
+//time: O(n), space: O(1)
+//Runtime: 8 ms, faster than 97.58% of C++ online submissions for Linked List Cycle.
+//Memory Usage: 8.9 MB, less than 100.00% of C++ online submissions for Linked List Cycle.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return false;
+        ListNode *slow = head, *fast = head->next;
+        //if there is a cycle, fast will eventually catch up slow
+        while(slow != fast){
+            if(fast == NULL || fast->next == NULL)
+                return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};
