36a359db6498cd3a250582adf5a5b5a251d60926
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-28 22:00:45 +0800

    add official sol

--- 443. String Compression.cpp
+++ 443. String Compression.cpp
@@ -60,3 +60,32 @@ public:
         return chars.size();
     }
 };
+
+//Approach #1: Read and Write Heads
+//time: O(N), space: O(1)
+//Runtime: 8 ms, faster than 86.14% of C++ online submissions for String Compression.
+//Memory Usage: 8.1 MB, less than 100.00% of C++ online submissions for String Compression.
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int N = chars.size();
+        int anchor = 0, write = 0;
+        for(int read = 0; read < N; read++){
+            if(read == N-1 || chars[read] != chars[read+1]){
+                //write char
+                chars[write++] = chars[anchor];
+                //write number
+                //more than one chars[anchor]
+                if(read > anchor){
+                    int count = read - anchor + 1;
+                    for(char c : to_string(count)){
+                        chars[write++] = c;
+                    }
+                }
+                //point to next char
+                anchor = read+1;
+            }
+        }
+        return write;
+    }
+};
