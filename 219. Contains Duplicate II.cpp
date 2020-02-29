2351ce14b234234e1c2fdfd408bfe8f012c0c77f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-02-29 10:52:23 +0800

    sliding window sol

--- 219. Contains Duplicate II.cpp
+++ 219. Contains Duplicate II.cpp
@@ -31,3 +31,24 @@ public:
         return false;
     }
 };
+
+//https://leetcode.com/problems/contains-duplicate-ii/discuss/61372/Simple-Java-solution
+//Sliding window
+//Runtime: 40 ms, faster than 34.36% of C++ online submissions for Contains Duplicate II.
+//Memory Usage: 14.8 MB, less than 82.35% of C++ online submissions for Contains Duplicate II.
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        set<int> myset;
+        for(int i = 0; i < nums.size(); i++){
+            //window : [i-k, i]
+            //so (i-k-1)th goes out of the window, need to remove it
+            if(i-k-1 >= 0) myset.erase(nums[i-k-1]);
+            /*
+            The single element versions (1) return a pair, with its member pair::first set to an iterator pointing to either the newly inserted element or to the equivalent element already in the set. The pair::second element in the pair is set to true if a new element was inserted or false if an equivalent element already existed.
+            */
+            if(!myset.insert(nums[i]).second) return true;
+        }
+        return false;
+    }
+};
