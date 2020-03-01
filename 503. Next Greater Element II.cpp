c1d8249dd627e4e221d88a69f36fb5487a53ecb9
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-01 19:36:38 +0800

    503. Next Greater Element II.cpp

---
+++ 503. Next Greater Element II.cpp
@@ -0,0 +1,21 @@
+//Runtime: 260 ms, faster than 8.26% of C++ online submissions for Next Greater Element II.
+//Memory Usage: 11.1 MB, less than 100.00% of C++ online submissions for Next Greater Element II.
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> ans(N, -1);
+        
+        for(int i = 0; i < N; i++){
+            for(int j = i+1; j < i+N; j++){
+                if(nums[j%N] > nums[i]){
+                    ans[i] = nums[j%N];
+                    break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
