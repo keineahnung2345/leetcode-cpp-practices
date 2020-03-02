49396a629775b56fb214b7566c903231820e1446
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-02 09:11:40 +0800

    add stack sol

--- 503. Next Greater Element II.cpp
+++ 503. Next Greater Element II.cpp
@@ -20,3 +20,24 @@ public:
         return ans;
     }
 };
+
+//Approach #3 Using Stack
+//time: O(n), space: O(n)
+//Runtime: 88 ms, faster than 78.59% of C++ online submissions for Next Greater Element II.
+//Memory Usage: 11.9 MB, less than 100.00% of C++ online submissions for Next Greater Element II.
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> ans(N);
+        stack<int> stk;
+        for(int i = 2*N-1; i >= 0; i--){
+            while(!stk.empty() && nums[stk.top()] <= nums[i%N]){
+                stk.pop();
+            }
+            ans[i%N] = stk.empty() ? -1 : nums[stk.top()];
+            stk.push(i%N);
+        }
+        return ans;
+    }
+};
