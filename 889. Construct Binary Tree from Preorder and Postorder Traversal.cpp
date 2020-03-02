911c10ac7391748a9e127732ce989d6e5faba14e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-02 22:41:30 +0800

    add official sol

--- 889. Construct Binary Tree from Preorder and Postorder Traversal.cpp
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal.cpp
@@ -71,3 +71,41 @@ public:
         return root;
     }
 };
+
+//Approach 2: Recursion (Space Saving Variant)
+//time: O(N^2), space: O(N)
+//Runtime: 12 ms, faster than 72.49% of C++ online submissions for Construct Binary Tree from Preorder and Postorder Traversal.
+//Memory Usage: 15.1 MB, less than 16.67% of C++ online submissions for Construct Binary Tree from Preorder and Postorder Traversal.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> pre, post;
+    
+    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
+        this->pre = pre;
+        this->post = post;
+        return make(0, 0, pre.size());
+    }
+    
+    TreeNode* make(int preStart, int postStart, int N){
+        if(N == 0) return NULL;
+        TreeNode* root = new TreeNode(pre[preStart]);
+        if(N == 1) return root;
+        
+        //left subtree's size
+        int L = find(post.begin()+postStart, post.begin()+postStart+N, pre[preStart+1]) - (post.begin()+postStart)+1;
+        
+        root->left = make(preStart+1, postStart, L);
+        root->right = make(preStart+1+L, postStart+L,N-L-1);
+        return root;
+    }
+};
