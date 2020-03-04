97e858a1da45e2d186df357c84877f46e4748b33
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-04 20:56:01 +0800

    add stack sol

--- 739. Daily Temperatures.cpp
+++ 739. Daily Temperatures.cpp
@@ -42,3 +42,25 @@ public:
         return ans;
     }
 };
+
+//Approach #2: Stack
+//Runtime: 188 ms, faster than 88.60% of C++ online submissions for Daily Temperatures.
+//Memory Usage: 13.9 MB, less than 100.00% of C++ online submissions for Daily Temperatures.
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        vector<int> ans(T.size());
+        stack<int> stk;
+        
+        for(int i = T.size()-1; i >= 0; i--){
+            //pop all values < current value
+            while(!stk.empty() && T[stk.top()] <= T[i]){
+                stk.pop();
+            }
+            ans[i] = stk.empty()?0:stk.top()-i;
+            stk.push(i);
+        }
+        return ans;
+    }
+};
