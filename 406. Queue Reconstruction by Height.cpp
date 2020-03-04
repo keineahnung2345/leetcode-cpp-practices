caabfadc40b946799d3e443c5c9784ae8090d6ba
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-04 16:31:58 +0800

    add sol from discussion

--- 406. Queue Reconstruction by Height.cpp
+++ 406. Queue Reconstruction by Height.cpp
@@ -35,3 +35,36 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/queue-reconstruction-by-height/discuss/89345/Easy-concept-with-PythonC%2B%2BJava-Solution
+//construct a subarray an then insert one by one
+//Runtime: 80 ms, faster than 76.98% of C++ online submissions for Queue Reconstruction by Height.
+//Memory Usage: 11.8 MB, less than 100.00% of C++ online submissions for Queue Reconstruction by Height.
+
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        int N = people.size();
+        //need to return empty 2d array for empty input
+        if(N == 0) return vector<vector<int>>();
+        vector<vector<int>> ans;
+        //cannot simply use this?
+        // vector<vector<int>> ans = vector<vector<int>>();
+        
+        //first sort by "h"(descending), and then by "k"(ascending)
+        sort(people.begin(), people.end(), [](vector<int>& a, vector<int>& b){return (a[0] == b[0]) ? (a[1] < b[1]) : a[0] > b[0];});
+        
+        int maxh = people[0][0];
+        
+        //create subarray containing people with max height
+        for(int i = 0; i < N; i++){
+            if(people[i][0] == maxh){
+                ans.push_back(people[i]);
+            }else{
+                ans.insert(ans.begin()+people[i][1], people[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
