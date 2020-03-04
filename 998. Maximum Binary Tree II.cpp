c3ccc4cb0f7ec4b12122899f2c2b51c1dc3d2a41
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-04 19:10:33 +0800

    add iterative sol

--- 998. Maximum Binary Tree II.cpp
+++ 998. Maximum Binary Tree II.cpp
@@ -1,5 +1,6 @@
 //https://leetcode.com/problems/maximum-binary-tree-ii/discuss/242936/JavaC%2B%2BPython-Recursion-and-Iteration
 //Solution 1: Recursion
+//time: O(n), space: O(n)
 //Runtime: 4 ms, faster than 89.46% of C++ online submissions for Maximum Binary Tree II.
 //Memory Usage: 13.1 MB, less than 20.00% of C++ online submissions for Maximum Binary Tree II.
 
@@ -24,5 +25,35 @@ public:
         TreeNode* node = new TreeNode(val);
         node->left = root;
         return node;
+    }
+};
+
+//Solution 2: Iteration
+//Runtime: 4 ms, faster than 89.46% of C++ online submissions for Maximum Binary Tree II.
+//Memory Usage: 13.5 MB, less than 20.00% of C++ online submissions for Maximum Binary Tree II.
+//time: O(n), space: O(1)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        TreeNode *node = new TreeNode(val), *cur = root;
+        if(root->val < val){
+            node->left = root;
+            return node;
+        }
+        while(cur->right && cur->right->val > val){
+            cur = cur->right;
+        }
+        node->left = cur->right;
+        cur->right = node;
+        return root;
     }
 };
