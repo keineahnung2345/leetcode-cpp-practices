91589fd9e2d1e35d3e349874ebbf410936f8c2cd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-06 15:47:21 +0800

    add recursion sol

--- 1306. Jump Game III.cpp
+++ 1306. Jump Game III.cpp
@@ -86,3 +86,21 @@ public:
         return canReachRecursive(arr, start) == Index::GOOD;
     }
 };
+
+//Recursion
+//https://leetcode.com/problems/jump-game-iii/discuss/464083/C%2B%2BJava-Recursion
+//Runtime: 52 ms, faster than 77.17% of C++ online submissions for Jump Game III.
+//Memory Usage: 11.9 MB, less than 100.00% of C++ online submissions for Jump Game III.
+//time: O(n), space: O(n)
+class Solution {
+public:
+    set<int> visited;
+    
+    bool canReach(vector<int>& arr, int start) {
+        if((start >= 0) && (start < arr.size()) && visited.insert(start).second){
+            return arr[start] == 0 || canReach(arr, start+arr[start]) || canReach(arr, start-arr[start]);
+        }
+        //not valid "start" or already visited
+        return false;
+    }
+};
