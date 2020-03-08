d60cdf62222b08009107bbb6ce4392adab61107a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-09 00:28:18 +0800

    add iterative dfs sol

--- 695. Max Area of Island.cpp
+++ 695. Max Area of Island.cpp
@@ -29,3 +29,53 @@ public:
         return ans;
     }
 };
+
+//Approach #2: Depth-First Search (Iterative) [Accepted]
+//Runtime: 32 ms, faster than 17.35% of C++ online submissions for Max Area of Island.
+//Memory Usage: 16 MB, less than 19.05% of C++ online submissions for Max Area of Island.
+//time: O(R*C), space: O(R*C)
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<bool>> visited = vector<vector<bool>>(m, vector<bool>(n, false));
+        vector<int> dr = {1, -1, 0, 0};
+        vector<int> dc = {0, 0, 1, -1};
+        
+        int ans = 0;
+        
+        for(int r0 = 0; r0 < m; r0++){
+            for(int c0 = 0; c0 < n; c0++){
+                if(grid[r0][c0] && !visited[r0][c0]){
+                    int area = 0;
+                    stack<vector<int>> stk;
+                    stk.push({r0, c0});
+                    //FLAG1
+                    visited[r0][c0] = true;
+                    //dfs for a cell, find the area
+                    while(!stk.empty()){
+                        vector<int> node = stk.top(); stk.pop();
+                        int r = node[0], c = node[1];
+                        area++;
+                        //why put "visited[r][c] = true;" in FLAG1 rather than FLAG2?
+                        //FLAG2
+                        // visited[r][c] = true;
+                        for(int k = 0; k < 4; k++){
+                            int nr = r + dr[k];
+                            int nc = c + dc[k];
+                            if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] && !visited[nr][nc])
+                            {
+                                stk.push({nr, nc});
+                                //FLAG1
+                                visited[nr][nc] = true;
+                            }
+                        }
+                    }
+                    ans = max(ans, area);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
