1489aa4299a8c5c5933478457a4cce18980f993d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-09 12:48:07 +0800

    backtarck

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -35,3 +35,27 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
+//Runtime: 4 ms, faster than 97.95% of C++ online submissions for Subsets.
+//Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions for Subsets.
+class Solution {
+public:
+    void backtrack(vector<vector<int>>& ans, vector<int>& subset, vector<int>& nums, int start){
+        ans.push_back(subset);
+        for(int i = start; i < nums.size(); i++){
+            subset.push_back(nums[i]);
+            backtrack(ans, subset, nums, i+1);
+            subset.pop_back();
+        }
+    };
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        
+        backtrack(ans, subset, nums, 0);
+        
+        return ans;
+    }
+};
