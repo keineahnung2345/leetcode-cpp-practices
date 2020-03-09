5a6327870703a07da296f4925444c24b6fc847f4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-09 14:51:54 +0800

    Approach 3: Lexicographic (Binary Sorted) Subsets

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -87,3 +87,40 @@ public:
         return ans;
     }
 };
+
+//Approach 3: Lexicographic (Binary Sorted) Subsets
+//time: O(N*2^N), space: O(N*2^N)
+//Runtime: 8 ms, faster than 53.38% of C++ online submissions for Subsets.
+//Memory Usage: 8.4 MB, less than 100.00% of C++ online submissions for Subsets.
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        //its binary representation has length (n+1)
+        int N = nums.size();
+        int nthBit = 1 << N;
+        vector<vector<int>> ans;
+        
+        // cout << N << " " << nthBit << endl;
+        
+        //(1 << n) equals to (int)pow(2, n)
+        for(int i = 0; i < nthBit; i++){
+            const size_t bitcount = sizeof(nthBit)*8;
+            string binary = bitset<bitcount>(i | nthBit).to_string();
+            //the first (bitcount -N) bit is just used for keeping the leading 0, so here ignore it
+            //select the last N bits
+            string bitmask = binary.substr(bitcount-N);
+            
+            // cout << i << " " << bitmask << endl;
+            
+            vector<int> subset;
+            for(int j = 0; j < N; j++){
+                if(bitmask[j] == '1'){
+                    subset.push_back(nums[j]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        
+        return ans;
+    }
+};
