9cf16f05f321fbf1d745236898a7dc56c2ea1b3e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-09 22:22:25 +0800

    stack

--- 1249. Minimum Remove to Make Valid Parentheses.cpp
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -47,3 +47,32 @@ public:
         return s;
     }
 };
+
+//Stack
+//https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/discuss/419402/JavaC%2B%2B-Stack
+//Runtime: 64 ms, faster than 9.24% of C++ online submissions for Minimum Remove to Make Valid Parentheses.
+//Memory Usage: 12.6 MB, less than 100.00% of C++ online submissions for Minimum Remove to Make Valid Parentheses.
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> stk;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                //convert to 1-based so that we can use '-' as mark for 
+                stk.push(i+1);
+            }else if(s[i] == ')'){
+                //stk's top is '('
+                if(stk.size() > 0 && stk.top() > 0) stk.pop();
+                else stk.push(-(i+1));
+            }
+        }
+        
+        //now stk contains redundant char's indices
+        while(!stk.empty()){
+            int i = stk.top(); stk.pop();
+            s.erase(abs(i)-1, 1);
+        }
+        
+        return s;
+    }
+};
