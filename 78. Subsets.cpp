483d771404fd7902cd048d49e99f2280f393c9d7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-09 13:20:15 +0800

    add Recursion sol

--- 78. Subsets.cpp
+++ 78. Subsets.cpp
@@ -59,3 +59,29 @@ public:
         return ans;
     }
 };
+
+//Approach 1: Recursion
+//time: O(N*2^N), space: O(N*2^N)
+//Runtime: 4 ms, faster than 97.95% of C++ online submissions for Subsets.
+//Memory Usage: 8.3 MB, less than 100.00% of C++ online submissions for Subsets.
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans = {vector<int>()};
+        
+        for(int num : nums){
+            // cout << "num: " << num << endl;
+            int ans_size = ans.size();
+            // for(vector<int>& subset : ans){
+            for(int i = 0; i < ans_size; i++){
+                vector<int> cur = ans[i];
+                // cout << "subset size: " << cur.size() << endl;
+                cur.push_back(num);
+                ans.push_back(cur);
+                // cout << "ans size: " << ans.size() << endl;
+            }
+        }
+        
+        return ans;
+    }
+};
