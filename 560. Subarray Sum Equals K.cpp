4b37dac32b399a971c7935f3836f9a527ae7e4c9
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 17:20:19 +0800

    add map solution

--- 560. Subarray Sum Equals K.cpp
+++ 560. Subarray Sum Equals K.cpp
@@ -41,3 +41,32 @@ public:
         return ans;
     }
 };
+
+//Approach #4 Using hashmap
+//Runtime: 44 ms, faster than 54.94% of C++ online submissions for Subarray Sum Equals K.
+//Memory Usage: 13.8 MB, less than 70.67% of C++ online submissions for Subarray Sum Equals K.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ans = 0, sum = 0;
+        
+        //count the occurrence of cumulative sum
+        map<int, int> counter;
+        
+        counter[0] = 1;
+        
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            //"sum"(current cumulative sum) - last cumulative sum(counter[?]) = k
+            //we can substract last cumulative sum from current cumulative sum to get k, and there are counter[sum - k] possibilities
+            if(counter.find(sum - k) != counter.end()){
+                ans += counter[sum - k];
+            }
+            //occurrence of prefix sum
+            counter[sum]++;
+        }
+        
+        return ans;
+    }
+};
