216fa7805c545efb2d6d03adb74095be19ec19bd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 21:22:34 +0800

    add sliding window sol

--- 1074. Number of Submatrices That Sum to Target.cpp
+++ 1074. Number of Submatrices That Sum to Target.cpp
@@ -84,3 +84,46 @@ public:
         return ans;
     }
 };
+
+//Treat submatrix as subarray and use sliding window
+//https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/discuss/303773/C%2B%2B-O(n3)-Simple-1D-Subarray-target-sum-applied-to-2D-array
+//Runtime: 5772 ms, faster than 59.72% of C++ online submissions for Number of Submatrices That Sum to Target.
+//Memory Usage: 455.5 MB, less than 100.00% of C++ online submissions for Number of Submatrices That Sum to Target.
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int target){
+        int res = 0, sum = 0;
+        map<int, int> counter;
+        counter[0] = 1;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr[i];
+            if(counter.find(sum - target) != counter.end()){
+                res += counter[sum - target];
+            }
+            counter[sum]++;
+        }
+        return res;
+    };
+    
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int ans = 0;
+        vector<int> row(n);
+        
+        for(int row_low = 0; row_low < m; row_low++){
+            //cumulative sum start from row_low
+            fill(row.begin(), row.end(), 0);
+            for(int row_high = row_low; row_high < m; row_high++){
+                //cumulative sum until row_high
+                for(int col = 0; col < n; col++){
+                    row[col] += matrix[row_high][col];
+                }
+                //think the submatrix as an array
+                //and then find its subarray's sum
+                ans += subarraySum(row, target);
+            }
+        }
+        
+        return ans;
+    }
+};
