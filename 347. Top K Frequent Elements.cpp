5acbfdb4c1004a6b70f20e4e87d34e63e6ed6ab3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-11 00:03:22 +0800

    347. Top K Frequent Elements.cpp

---
+++ 347. Top K Frequent Elements.cpp
@@ -0,0 +1,43 @@
+//Runtime: 24 ms, faster than 39.54% of C++ online submissions for Top K Frequent Elements.
+//Memory Usage: 10.7 MB, less than 100.00% of C++ online submissions for Top K Frequent Elements.
+
+class valuecomparator
+{
+public:
+    valuecomparator(){}
+    bool operator() (const pair<int, int>& lhs, const pair<int, int>&rhs) const{
+        return lhs.second < rhs.second;
+    }
+};
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> counter;
+        vector<int> ans;
+        
+        for(int e : nums){
+            counter[e]++;
+        }
+        
+        //cannot sort map!
+        // sort(counter.begin(), counter.end(), 
+        //      [](const pair<int, int>& a, const pair<int, int>& b){
+        //          return a.second < b.second;
+        // });
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, valuecomparator> pq1;
+
+        for(auto it = counter.begin(); it != counter.end(); it++){
+            pq1.push(*it);
+        }
+
+        for(int i = 0; i < k; i++){
+            pair<int, int> p = pq1.top();
+            ans.push_back(p.first);
+            pq1.pop();
+        }
+        
+        return ans;
+    }
+};
