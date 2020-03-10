61a1b66f0a972d318eeb3eb2b81c1c9997ba0600
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 16:16:28 +0800

    calculate cumulative sum for every start point

--- 560. Subarray Sum Equals K.cpp
+++ 560. Subarray Sum Equals K.cpp
@@ -22,3 +22,22 @@ public:
         return ans;
     }
 };
+
+//Approach #3 : calculate cumulative sum for every start point
+//Runtime: 476 ms, faster than 24.49% of C++ online submissions for Subarray Sum Equals K.
+//Memory Usage: 9.4 MB, less than 100.00% of C++ online submissions for Subarray Sum Equals K.
+//time: O(n^2), space: O(1)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ans = 0, n = nums.size();
+        for(int start = 0; start < n; start++){
+            int sum = 0;
+            for(int end = start; end < n; end++){
+                sum += nums[end];
+                if(sum == k) ans++;
+            }
+        }
+        return ans;
+    }
+};
