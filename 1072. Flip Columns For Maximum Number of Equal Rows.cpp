7b04e78d0ea7b9134c61f6aec1721485129fc012
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-10 23:09:10 +0800

    1072. Flip Columns For Maximum Number of Equal Rows.cpp

---
+++ 1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -0,0 +1,71 @@
+//TLE
+
+class Solution {
+public:
+    bool all0(vector<int>& arr){
+        return accumulate(arr.begin(), arr.end(), 0) == 0;
+    };
+    
+    bool all1(vector<int>& arr){
+        return accumulate(arr.begin(), arr.end(), 1, multiplies<int>()) == 1;
+    };
+    
+    bool allSame(vector<int>& arr1, vector<int>& arr2){
+        bool flag = true;
+        for(int i = 0; i < arr1.size(); i++){
+            if(arr1[i] != arr2[i]){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    };
+    
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> sum(n), reverted(n), ones(n, 1);
+        int ans = 0;
+        
+        for(int i = 0; i < m; i++){
+            set<vector<int>> group;
+            int row_friend = 0;
+            for(int j = 0; j < m; j++){
+                if(j == i) continue;
+                //sum two vector element-wise
+                transform(matrix[i].begin(), matrix[i].end(),
+                    matrix[j].begin(), sum.begin(), plus<int>());
+                //if their sum is all 0 or all 1, 
+                //row_friend++
+                if(allSame(matrix[i], matrix[j]) || all1(sum)){
+                // if(all1(sum)){
+                    //ensure the 0th element is 0 and then record it into the set
+                    reverted = matrix[i];
+                    if(matrix[i][0] != 0){
+                        transform(ones.begin(), ones.end(), matrix[i].begin(), reverted.begin(), minus<int>());
+                    }
+                    if(group.find(reverted) == group.end()){
+                        row_friend += 2;
+                    }else{
+                        row_friend++;
+                    }
+                    group.insert(reverted);
+                }
+            }
+            ans = max(ans, row_friend);
+        }
+        
+        if(ans == 0){
+            for(int i = 0; i < m; i++){
+                if(all0(matrix[i]) || all1(matrix[i])){
+                // if(all1(matrix[i])){
+                    ans++;
+                }
+            }
+        }
+        
+        //we can always do custom operations for a specific row
+        if(ans == 0) ans = 1;
+        
+        return ans;
+    }
+};
