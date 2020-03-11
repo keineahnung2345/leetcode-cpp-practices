5380624b391d2107b1aea5de56d1236149254b6e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-12 00:05:35 +0800

    add neighbor solution

--- 676. Implement Magic Dictionary.cpp
+++ 676. Implement Magic Dictionary.cpp
@@ -51,3 +51,65 @@ public:
  * bool param_2 = obj->search(word);
  */
  
+//Approach #2: Generalized Neighbors
+//Runtime: 4 ms, faster than 69.86% of C++ online submissions for Implement Magic Dictionary.
+//Memory Usage: 9.3 MB, less than 66.67% of C++ online submissions for Implement Magic Dictionary.
+//wi: length of words[i], K: length of the word to be searched
+//time: build: O(sigma(w_i^2)), search: O(K^2)
+//space: counter: O(sigma(w_i^2)), neighbors of word to be searched : O(K^2)
+class MagicDictionary {
+public:
+    set<string> dictset;
+    map<string, int> counter;
+    
+    /** Initialize your data structure here. */
+    MagicDictionary() {
+        
+    }
+    
+    vector<string> genneighbors(string& word){
+        vector<string> neighbors;
+        
+        for(int i = 0; i < word.size(); i++){
+            neighbors.push_back(word.substr(0, i) + "*" + word.substr(i+1));
+        }
+        
+        return neighbors;
+    }
+    
+    /** Build a dictionary through a list of words */
+    void buildDict(vector<string> dict) {
+        //counter : record possible neighbors' counts
+        for(string& word : dict){
+            vector<string> neighbors = genneighbors(word);
+            for(string& neighbor : neighbors){
+                counter[neighbor]++;
+            }
+            dictset.insert(word);
+        }
+    }
+    
+    /** Returns if there is any word in the trie that equals to the given word after modifying exactly one character */
+    bool search(string word) {
+        bool flag = false;
+        vector<string> neighbors = genneighbors(word);
+        
+        for(string& neighbor : neighbors){
+            //if word has more than 1 neighbor, then one of them must not be word
+            //if word has only 1 neighbor, we need to check whether the neighbor is generated from word itself
+            if(counter[neighbor] > 1 || (counter[neighbor] == 1 && dictset.find(word) == dictset.end())){
+                flag = true;
+                break;
+            }
+        }
+        
+        return flag;
+    }
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * MagicDictionary* obj = new MagicDictionary();
+ * obj->buildDict(dict);
+ * bool param_2 = obj->search(word);
+ */
