e5474cdfef302ef1282d3c71f1a48214f34eb813
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-15 00:24:30 +0800

    add split sol

--- 725. Split Linked List in Parts.cpp
+++ 725. Split Linked List in Parts.cpp
@@ -57,3 +57,51 @@ public:
         return ans;
     }
 };
+
+//Approach #2: Split Input List
+//Runtime: 12 ms, faster than 74.84% of C++ online submissions for Split Linked List in Parts.
+//Memory Usage: 9.1 MB, less than 100.00% of C++ online submissions for Split Linked List in Parts.
+//time: O(N+k), space: O(k)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        ListNode* cur = root;
+        int N = 0;
+        while(cur){
+            N++;
+            cur = cur->next;
+        }
+        
+        int width = N/k, rem = N%k;
+        vector<ListNode*> ans(k, NULL);
+        
+        cur = root;
+        for(int i = 0; i < k; i++){
+            //the head of current part
+            ListNode* head = cur;
+            ListNode* prev = cur;
+            //prev becomes the last node of current part
+            //(a.k.a, the previous node of next part)
+            for(int j = 0; j < width + (i < rem ? 1 : 0) - 1; j++){
+                if(prev) prev = prev->next;
+            }
+            if(prev){
+                //make cur points to the start of next part 
+                cur = prev->next;
+                //cur current part and next part
+                prev->next = NULL;
+            }
+            ans[i] = head;
+        }
+        
+        return ans;
+    }
+};
