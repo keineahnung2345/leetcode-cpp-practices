7d3d9e9797f07b91fa8541d4b7264e622fef54e6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-14 22:47:24 +0800

    add stack sol

--- 445. Add Two Numbers II.cpp
+++ 445. Add Two Numbers II.cpp
@@ -60,3 +60,55 @@ public:
         return head;
     }
 };
+
+//Stack
+//Runtime: 32 ms, faster than 54.73% of C++ online submissions for Add Two Numbers II.
+//Memory Usage: 13.7 MB, less than 22.22% of C++ online submissions for Add Two Numbers II.
+//time: O(n)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> stk1, stk2;
+        ListNode* cur = l1;
+        while(cur){
+            stk1.push(cur->val);
+            cur = cur->next;
+        }
+        
+        cur = l2;
+        while(cur){
+            stk2.push(cur->val);
+            cur = cur->next;
+        }
+        
+        ListNode* head = new ListNode(0);
+        int d = 0;
+        
+        while(!stk1.empty() || !stk2.empty()){
+            if(!stk1.empty()){
+                d += stk1.top();
+                stk1.pop();
+            }
+            if(!stk2.empty()){
+                d += stk2.top();
+                stk2.pop();
+            }
+            head->val = d%10;
+            ListNode* prehead = new ListNode(d/10);
+            prehead->next = head;
+            head = prehead;
+            d = d/10;
+        }
+        
+        return head->val == 0 ? head->next : head;
+    }
+};
