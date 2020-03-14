e8fb7dd830d3e13debfdd09f7a2ea547fd4491fe
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-14 23:21:33 +0800

    add official sol

--- 328. Odd Even Linked List.cpp
+++ 328. Odd Even Linked List.cpp
@@ -35,3 +35,31 @@ public:
         return oddHead->next;
     }
 };
+
+//Runtime: 12 ms, faster than 99.78% of C++ online submissions for Odd Even Linked List.
+//Memory Usage: 10.3 MB, less than 8.57% of C++ online submissions for Odd Even Linked List.
+//time: O(N), space: O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return NULL;
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+        while(even && even->next){
+            //this loop stops when even->next is meaningless
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
