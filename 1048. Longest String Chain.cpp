d2a8d5376bd0a138e78a22a39f29827ac5239e54
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-14 19:12:45 +0800

    add faster implementation

--- 1048. Longest String Chain.cpp
+++ 1048. Longest String Chain.cpp
@@ -38,3 +38,31 @@ public:
         return maxLen;
     }
 };
+
+//https://leetcode.com/problems/longest-string-chain/discuss/294890/C%2B%2BJavaPython-DP-Solution
+//Runtime: 152 ms, faster than 32.27% of C++ online submissions for Longest String Chain.
+//Memory Usage: 49.9 MB, less than 100.00% of C++ online submissions for Longest String Chain.
+//time: O(NlogN) for sorting
+//time: O(NSS) for the for loop, S for the length of each word, S <= 16, second S for the generation of substring
+//space: O(NS)
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        int N = words.size();
+        unordered_map<string, int> length;
+        int ans = 1;
+        
+        sort(words.begin(), words.end(), [](string& a, string& b){
+            return a.size() < b.size();
+        });
+        
+        for(string& word : words){
+            for(int i = 0; i < word.size(); i++){
+                length[word] = max(length[word], length[word.substr(0, i)+word.substr(i+1)]+1);
+            }
+            ans = max(ans, length[word]);
+        }
+        
+        return ans;
+    }
+};
