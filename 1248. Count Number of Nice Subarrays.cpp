3e2fbf774b9cefb0496b6357c46d73e7a83b8539
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-15 22:39:49 +0800

    two pointer sol

--- 1248. Count Number of Nice Subarrays.cpp
+++ 1248. Count Number of Nice Subarrays.cpp
@@ -87,3 +87,33 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/count-number-of-nice-subarrays/discuss/508217/C%2B%2B%3A-Visual-explanation.-O(1)-space.-Two-pointers
+//Runtime: 172 ms, faster than 95.48% of C++ online submissions for Count Number of Nice Subarrays.
+//Memory Usage: 15.7 MB, less than 100.00% of C++ online submissions for Count Number of Nice Subarrays.
+//time: O(n), space: O(1)
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int left = 0, odds = 0, count = 0, ans = 0;
+        for(int right = 0; right < nums.size(); right++){
+            if(nums[right] % 2 == 1){
+                odds++;
+                if(odds >= k){
+                    count = 1;
+                    //move left bound while counting
+                    while(nums[left++] % 2 == 0){
+                        count++;
+                    }
+                    ans += count;
+                }
+            }else if(odds >= k){
+                //we already have "count" subarrays of [left, last right with odd number]
+                //now right points to an even number, we get "count" subarrays of [left, right]
+                ans += count;
+            }
+            // cout << "[" << left << ", " << right << "]: " << odds << ", " << count << ", " << ans << endl;
+        }
+        return ans;
+    }
+};
