251b51a381021d367c9c5055adf0fed8ee794c76
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-15 14:08:40 +0800

    add priority queue sol

--- 1383. Maximum Performance of a Team.cpp
+++ 1383. Maximum Performance of a Team.cpp
@@ -54,3 +54,40 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/maximum-performance-of-a-team/discuss/539687/Python-Priority-Queue
+//Solution 2: Priority Queue
+//Runtime: 124 ms, faster than 33.33% of C++ online submissions for Maximum Performance of a Team.
+//Memory Usage: 18.2 MB, less than 100.00% of C++ online submissions for Maximum Performance of a Team.
+//time: O(nlogk), space: O(k)
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        vector<pair<int, int>> ess;
+        for (int i = 0; i < n; i++){
+            ess.push_back({efficiency[i], speed[i]});
+        }
+        //sort by efficiency and then by speed, descending
+        sort(ess.rbegin(), ess.rend());
+
+        long sumS = 0, res = 0;
+        priority_queue <int, vector<int>, greater<int> > pq;
+        //itereate through efficiency
+        for (int i = 0; i < n; i++) {
+            //for each efficiency, find the maximum possible combination(of size <= k) of speed
+            //possible speeds are the speed whose efficiency >= current efficiency
+            pq.push(ess[i].second);
+            //sumS: the sum of the combination of speed for current efficiency
+            sumS += ess[i].second;
+            // cout << "push " << ess[i].second << ", sumS: " << sumS << endl;
+            if (pq.size() > k) {
+                sumS -= pq.top();
+                // cout << "pop " << pq.top() << ", sumS: " << sumS << endl;
+                pq.pop();
+            }
+            // cout << "current res: " << sumS * ess[i].first << endl;
+            res = max(res, sumS * ess[i].first);
+        }
+        return res % (int)(1e9+7);
+    }
+};
