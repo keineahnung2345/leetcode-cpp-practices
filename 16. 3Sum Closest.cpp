c4e878f7bb34aac953d8bf635dd36e84cb4c9f32
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-17 00:14:45 +0800

    16. 3Sum Closest.cpp

---
+++ 16. 3Sum Closest.cpp
@@ -0,0 +1,42 @@
+//Two pointer
+//https://leetcode.com/problems/3sum-closest/discuss/8026/Python-solution-(two-pointer).
+//Runtime: 8 ms, faster than 81.20% of C++ online submissions for 3Sum Closest.
+//Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions for 3Sum Closest.
+//time: O(nlogn + n^2)
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int N = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        int ans = nums[0]+nums[1]+nums[2];
+        
+        // for(int i = 0; i < N; i++){
+        //     cout << nums[i] << " ";
+        // }
+        // cout << endl;
+        
+        for(int i = 0; i < N-2; i++){
+            //keep nums[i] in the combination and find best left and right
+            int left = i+1, right = N-1;
+            while(left < right){
+                int cur = nums[i] + nums[left] + nums[right];
+                // cout << i << " " << left << " " << right << " " << cur << endl;
+                if(ans == INT_MAX || abs(cur - target) < abs(ans - target)){
+                    ans = cur;
+                }
+                //update left and right
+                if(cur == target){
+                    return cur;
+                }else if(cur < target){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
