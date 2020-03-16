02e0b9de963efe984585295de841140bd029eb18
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-16 19:17:22 +0800

    Approach 1: Brute force

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -61,3 +61,28 @@ public:
         return ans;
     }
 };
+
+//Approach 1: Brute force
+//Runtime: 312 ms, faster than 5.10% of C++ online submissions for Trapping Rain Water.
+//Memory Usage: 8.5 MB, less than 100.00% of C++ online submissions for Trapping Rain Water.
+//time: O(n^2), space: O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        
+        for(int i = 0; i < height.size(); i++){
+            int maxLeft = 0, maxRight = 0;
+            for(int j = i; j >= 0 ; j--){
+                maxLeft = max(maxLeft, height[j]);
+            }
+            for(int j = i; j < height.size() ; j++){
+                maxRight = max(maxRight, height[j]);
+            }
+            ans += min(maxLeft, maxRight) - height[i];
+            // cout << maxLeft << " " << maxRight << " " << ans << endl;
+        }
+        
+        return ans;
+    }
+};
