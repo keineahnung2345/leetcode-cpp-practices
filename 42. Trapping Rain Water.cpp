fbf6e249ebfe49d071d970e87fce2fad1b227881
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-16 20:36:36 +0800

    Dynamic Programming

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -86,3 +86,33 @@ public:
         return ans;
     }
 };
+
+//Approach 2: Dynamic Programming
+//Runtime: 4 ms, faster than 95.59% of C++ online submissions for Trapping Rain Water.
+//Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions for Trapping Rain Water.
+//time: O(n), space: O(n)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int N = height.size();
+        if(N == 0) return 0;
+        int ans = 0;
+        
+        vector<int> leftMax(N), rightMax(N);
+        leftMax[0] = height[0];
+        for(int i = 1; i < N; i++){
+            leftMax[i] = max(leftMax[i-1], height[i]);
+        }
+        
+        rightMax[N-1] = height[N-1];
+        for(int i = N-2; i >= 0; i--){
+            rightMax[i] = max(rightMax[i+1], height[i]);
+        }
+        
+        for(int i = 1; i < N-1; i++){
+            ans += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        
+        return ans;
+    }
+};
