0aad18a2d1a741247bcb6b2686ff43d3c826337b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-16 12:17:55 +0800

    Approach 2: Two Pointer Approach

--- 11. Container With Most Water.cpp
+++ 11. Container With Most Water.cpp
@@ -34,7 +34,9 @@ public:
     }
 };
 
+//Approach 1: Brute Force
 //TLE
+//time: O(n^2), space: O(1)
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -46,6 +48,30 @@ public:
             }
         }
             
+        return ans;
+    }
+};
+
+//Approach 2: Two Pointer Approach
+//Runtime: 16 ms, faster than 96.00% of C++ online submissions for Container With Most Water.
+//Memory Usage: 9 MB, less than 100.00% of C++ online submissions for Container With Most Water.
+//time: O(n), space: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int N = height.size();
+        int left = 0, right = N-1, ans = 0;
+        while(left < right){
+            ans = max(ans, min(height[left], height[right]) * (right-left));
+            //move the pointer pointing to lower bar forward,
+            //because if we move the pointer pointing to higher bar forward, 
+            //the area can only be shrinked
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
         return ans;
     }
 };
