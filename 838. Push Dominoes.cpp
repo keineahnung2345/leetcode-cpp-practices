cbb9805b660c86a7516fd27a5d594bef07953f41
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-16 23:43:08 +0800

    two pointer

--- 838. Push Dominoes.cpp
+++ 838. Push Dominoes.cpp
@@ -56,3 +56,45 @@ public:
         return dominoes;
     }
 };
+
+//Two pointer
+//https://leetcode.com/problems/push-dominoes/discuss/132332/C%2B%2BJavaPython-Two-Pointers
+//Runtime: 32 ms, faster than 40.77% of C++ online submissions for Push Dominoes.
+//Memory Usage: 13.1 MB, less than 25.00% of C++ online submissions for Push Dominoes.
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        dominoes = 'L' + dominoes + 'R';
+        // cout << dominoes << endl;
+        string res = "";
+        for (int slow = 0, fast = 1; fast < dominoes.length(); fast++) {
+            if (dominoes[fast] == '.') continue;
+            //we've found a interval('R......R', 'R......L', 'L......R' or 'L......R')
+            int middle = fast - slow - 1;
+            
+            //deal with slow(left bound)
+            //slow == 0 is padding
+            if (slow > 0){
+                res += dominoes[slow];
+            }
+            
+            //deal with middle(between left and right bound)
+            if (dominoes[slow] == dominoes[fast]){
+                //'R......R' => 'RRRRRRRR'
+                //'L......L' => 'LLLLLLLL'
+                res += string(middle, dominoes[slow]);
+            }else if (dominoes[slow] == 'L' && dominoes[fast] == 'R'){
+                //'L......R' => 'L......R'
+                res += string(middle, '.');
+            }else{
+                //'R......L' => 'RRRRLLLL' or 'RRRR.LLLL'
+                res += string(middle / 2, 'R') + string(middle % 2,'.') + string(middle / 2, 'L');
+            }
+            
+            //don't need to deal with right bound because the rightmost char is padding
+            // cout << slow << " " << fast << " " << middle << " " << res << endl;
+            slow = fast;
+        }
+        return res;
+    }
+};
