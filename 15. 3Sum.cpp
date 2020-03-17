bc71e9c36e695184d7a73ba30dda74687d811f0f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-17 23:52:22 +0800

    hashmap sol

--- 15. 3Sum.cpp
+++ 15. 3Sum.cpp
@@ -64,3 +64,36 @@ public:
         return ans;
     }
 };
+
+//Hashmap, without sorting
+// /https://leetcode.com/problems/3sum/discuss/227156/Java-O(N2)-using-Map
+//TLE
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> ansSet;
+        map<int, int> lookup;
+        int N = nums.size();
+        
+        //map from the value to find to its index
+        for(int i = 0; i < N; i++){
+            lookup[-nums[i]] = i;
+        }
+        
+        for(int i = 0; i < N; i++){
+            for(int j = i+1; j < N; j++){
+                auto it = lookup.find(nums[i] + nums[j]);
+                if(it != lookup.end() && it->second != i && it->second != j){
+                    vector<int> triplet = {nums[i], nums[j], nums[it->second]};
+                    //sort it so that we can check if it's unique in the set
+                    sort(triplet.begin(), triplet.end());
+                    ansSet.insert(triplet);
+                }
+            }
+        }
+        
+        vector<vector<int>> ans(ansSet.begin(), ansSet.end());
+        
+        return ans;
+    }
+};
