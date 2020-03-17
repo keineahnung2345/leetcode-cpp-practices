d4d92670bc09e7f9d09192ca6a49a90a6bc9647f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-17 21:41:39 +0800

    keep pq size <= k, become slower?

--- 373. Find K Pairs with Smallest Sums.cpp
+++ 373. Find K Pairs with Smallest Sums.cpp
@@ -27,3 +27,37 @@ public:
         return ans;
     }
 };
+
+//Runtime: 860 ms, faster than 5.02% of C++ online submissions for Find K Pairs with Smallest Sums.
+//Memory Usage: 43.1 MB, less than 20.00% of C++ online submissions for Find K Pairs with Smallest Sums.
+class pairSumLess{
+public:
+    pairSumLess(){};
+    bool operator() (const vector<int>& a, const vector<int>& b){
+        return a[0] + a[1] < b[0] + b[1];
+    };
+};
+
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, pairSumLess> pq;
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j< nums2.size(); j++){
+                pq.push({nums1[i], nums2[j]});
+                if(pq.size() > k){
+                    //the greater the earlier be popped
+                    pq.pop();
+                }
+            }
+        }
+        
+        vector<vector<int>> ans;
+        while(k-- > 0 && !pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
