6fcf69b83f2c1151e67dd9742615261018cf1515
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-18 22:28:58 +0800

    Binary search

--- 454. 4Sum II.cpp
+++ 454. 4Sum II.cpp
@@ -90,3 +90,93 @@ public:
         return ans;
     }
 };
+
+//Binary search
+//https://leetcode.com/problems/4sum-ii/discuss/93923/How-to-use-Binary-Search-with-%224Sum-II%22
+//Runtime: 256 ms, faster than 47.71% of C++ online submissions for 4Sum II.
+//Memory Usage: 24.7 MB, less than 95.45% of C++ online submissions for 4Sum II.
+class Solution {
+public:
+    int biSearch(vector<int> & nums, int x, bool LEFT) {
+        //if not found return 0 when LEFT, return -1 when not LEFT
+        int l = 0, r = nums.size()-1, m;
+        // cout << "finding " << x << ", left? " << LEFT << endl;
+        while (l <= r) {
+            m = (l+r) / 2;
+            // cout << l << " " << m << " " << r << " " << endl;
+            //if r = l + 1, then m = l
+            if (LEFT) {
+                /*
+                in the situation of there are multiple x in nums:
+                finding 0 in the array -1 0 0 1
+                0 1 3
+                0 0 0
+                if LEFT, we only care left bound
+                if nums[m] == x
+                we will set r = m-1, so r will equal to l, 
+                in next iteration, we will set l = m+1 which becomes original m,
+                then break the loop and return l
+                */
+                if (nums[m] >= x) r = m - 1;
+                else l = m + 1;
+            }
+            else {
+                /*
+                in the situation of there are multiple x in nums:
+                finding 0 in the array -1 0 0 1
+                0 1 3 
+                2 2 3 
+                3 3 3 
+                if not LEFT, we only care right bound
+                if nums[m]== x
+                we will set l = m+1, so l will equal to r
+                in next iteration, we will set r = m-1 which becomes original m, 
+                then break the loop and return r
+                */
+                if (nums[m] <= x) l = m + 1;
+                else r = m - 1;
+            }
+        }
+        // cout << "found at " << (LEFT?l:r) << endl;
+        return LEFT?l:r;
+    }
+    
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        vector<int> abSum, cdSum;
+        int N = A.size();
+        
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                abSum.push_back(A[i] + B[j]);
+                cdSum.push_back(C[i] + D[j]);
+            }
+        }
+        
+        sort(abSum.begin(), abSum.end());
+        sort(cdSum.begin(), cdSum.end());
+        
+//         for(int e : abSum){
+//             cout << e << " ";
+//         }
+//         cout << endl;
+        
+//         for(int e : cdSum){
+//             cout << e << " ";
+//         }
+//         cout << endl;
+        
+        int ans = 0, curCount;
+        for(int i = 0; i < abSum.size(); i++){
+            if(i > 0 && abSum[i] == abSum[i-1]){
+                // cout << i << " add last curCount" << endl;
+                ans += curCount;
+                continue;
+            }
+            curCount = biSearch(cdSum, -abSum[i], false) - 
+                biSearch(cdSum, -abSum[i], true) + 1;
+            ans += curCount;
+        }
+        
+        return ans;
+    }
+};
