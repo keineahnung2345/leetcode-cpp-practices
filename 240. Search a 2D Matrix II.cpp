5bc2b3aac97cf341b83437510adbd112de87cddc
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-19 13:08:20 +0800

    add comment

--- 240. Search a 2D Matrix II.cpp
+++ 240. Search a 2D Matrix II.cpp
@@ -14,6 +14,8 @@ public:
             //upper_bound: Unlike lower_bound, the value pointed by the iterator returned by this function cannot be equivalent to val, only greater.
             //the search range becomes smaller and smaller
             auto it = upper_bound(row.begin(), row.begin() + r, target);
+            //if we've found an element larger than target, we can shrink the range
+            //o.w. the range remains the same
             if(it !=  row.begin() + r){
                 r =  it - row.begin();
             }
