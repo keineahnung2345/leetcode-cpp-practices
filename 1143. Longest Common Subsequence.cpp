f1558bfccca433b56f152c0620ad93673779b0ca
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-20 00:22:02 +0800

    DP, memory optimization

--- 1143. Longest Common Subsequence.cpp
+++ 1143. Longest Common Subsequence.cpp
@@ -1,5 +1,7 @@
+//DP
 //Runtime: 20 ms, faster than 27.49% of C++ online submissions for Longest Common Subsequence.
 //Memory Usage: 14.2 MB, less than 100.00% of C++ online submissions for Longest Common Subsequence.
+//time: O(nm), space: O(nm)
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
@@ -22,5 +24,35 @@ public:
         }
         
         return dp[m-1][n-1];
+    }
+};
+
+//DP, memory optimization
+//https://leetcode.com/problems/longest-common-subsequence/discuss/348884/C%2B%2B-with-picture-O(nm)
+//Runtime: 12 ms, faster than 87.59% of C++ online submissions for Longest Common Subsequence.
+//Memory Usage: 8 MB, less than 100.00% of C++ online submissions for Longest Common Subsequence.
+//time: O(nm), space: O(min(m,n))
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(2, vector<int>(n, 0));
+        
+        int i1 = 0, i2 = 0;
+        
+        for(int i1 = 0; i1 < m; i1++){
+            for(int i2 = 0; i2 < n; i2++){
+                //distinguish the case whether text1[i1] == text2[i2] or not so the same char won't be counted twice
+                if(text1[i1] == text2[i2]){
+                    dp[i1%2][i2] = ((i1 > 0 && i2 > 0) ? dp[(i1-1)%2][i2-1] : 0) + 1;
+                }else{
+                    dp[i1%2][i2] = max(((i1 > 0) ? dp[(i1-1)%2][i2] : 0), 
+                                     ((i2 > 0) ? dp[i1%2][i2-1] : 0));
+                }
+                // cout << i1 << " " << i2 << " " << dp[i1][i2] << endl;
+            }
+        }
+        
+        return dp[(m-1)%2][n-1];
     }
 };
