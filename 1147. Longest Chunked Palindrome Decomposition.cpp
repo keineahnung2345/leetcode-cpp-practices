8acf3ed618a60b1a12e30ade6821a0cf2c0899bd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-19 23:13:05 +0800

    dp

--- 1147. Longest Chunked Palindrome Decomposition.cpp
+++ 1147. Longest Chunked Palindrome Decomposition.cpp
@@ -88,3 +88,51 @@ public:
         return ans;
     }
 };
+
+//DP
+//https://leetcode.com/problems/longest-chunked-palindrome-decomposition/discuss/351919/Java-DP-Solution.-Straight-Forward-With-Explanation
+//Runtime: 600 ms, faster than 5.44% of C++ online submissions for Longest Chunked Palindrome Decomposition.
+//Memory Usage: 461.4 MB, less than 100.00% of C++ online submissions for Longest Chunked Palindrome Decomposition.
+class Solution {
+public:
+    int longestDecomposition(string text) {
+        int N = text.size();
+        //dp[i]: max number of palindrome decompositions that s[0:i] + s[len-i:len] can form, not counting the strings between [i+1:len-i-1].
+        vector<int> dp(N/2+1, 0);
+        int ans = 0;
+        for(int r = 0; r < N/2; r++){
+            for(int l = 0; l <= r; l++){
+                /*
+                [l,r] is all possible substrings formed by the former part of text
+                for odd N, the middle char is not included
+                */
+                // cout << l << " " << r << endl;
+                //compare [l,r] and [N-r-1,N-l-1]
+                //dp[l] != 0: [0,l-1] must be valid so we can construct dp[r+1] upon it
+                //dp[0] is always valid
+                if(text.substr(l, r-l+1) == text.substr(N-r-1, r-l+1) && 
+                   (l == 0 || dp[l] != 0)){
+                    //dp[r+1] for s[0:r]
+                    //dp[l] for s[0:l-1]
+                    //dp[r+1] is for [0,r], and we are calculate it from [0,l-1] and [l,r]
+                    //[l,r] is the current substring
+                    //dp[l] is for [0,l-1], which is all chars before current substring
+                    //dp[r+1] is equal to dp[l] plus current 2 tokens
+                    // cout << r << " " << l-1 << " " << text.substr(l, r-l+1) << endl;
+                    dp[r+1] = max(dp[r+1], dp[l]+2);
+                    //ans: we can find "ans" palindromes from [0:r] and [N-1-r:N-1]
+                    //dp[r+1] can be 0 if r is not the end of any palindrome, so we need a max function here
+                    // ans = max(ans, dp[l]+2);
+                    ans = max(ans, dp[r+1]);
+                }
+            }
+        }
+        
+        // for(int i = 0; i < dp.size(); i++){
+        //     if(dp[i] > 0) cout << "i: " << i << ", " << dp[i] << endl;
+        // }
+        
+        //((N % 2 == 0) && dp[N/2] != 0): the string's length is even and there is a palindrome ends just before the middle point
+        return ((N % 2 == 0) && dp[N/2] != 0) ? dp[N/2] : ans+1;
+    }
+};
