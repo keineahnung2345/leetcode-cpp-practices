7f83b7164cf74539bcd5b17fe8cc35eb41848447
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 00:05:39 +0800

    Thousand-and-One-Stops

--- 1094. Car Pooling.cpp
+++ 1094. Car Pooling.cpp
@@ -34,3 +34,26 @@ public:
         return true;
     }
 };
+
+//https://leetcode.com/problems/car-pooling/discuss/317611/C%2B%2BJava-O(n)-Thousand-and-One-Stops
+//Runtime: 12 ms, faster than 62.92% of C++ online submissions for Car Pooling.
+//Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions for Car Pooling.
+//time: O(#trips), space: O(#stops)
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> stops(1001, 0);
+        for(auto trip : trips){
+            //how many people get in the car at this stop
+            stops[trip[1]] += trip[0];
+            stops[trip[2]] -= trip[0];
+        }
+        
+        for(int i = 0; i < stops.size(); i++){
+            capacity -= stops[i];
+            if(capacity < 0) return false;
+        }
+        
+        return true;
+    }
+};
