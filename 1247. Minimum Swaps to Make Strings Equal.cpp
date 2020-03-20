99ec08f33bf8b4997876817462c4b5feb2a5dc70
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-20 23:22:57 +0800

    optimized sol

--- 1247. Minimum Swaps to Make Strings Equal.cpp
+++ 1247. Minimum Swaps to Make Strings Equal.cpp
@@ -31,3 +31,37 @@ public:
         return (xcount == 0 && ycount == 0) ? ans : -1;
     }
 };
+
+//optimized
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Minimum Swaps to Make Strings Equal.
+//Memory Usage: 6.5 MB, less than 100.00% of C++ online submissions for Minimum Swaps to Make Strings Equal.
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int N = s1.size();
+        int xcount = 0, ycount = 0;
+        int ans = 0;
+        
+        for(int i = 0; i < N; i++){
+            if(s1[i] == s2[i]) continue;
+            
+            if(s1[i] == 'x'){
+                xcount++;
+            }else{
+                ycount++;
+            }
+        }
+        
+        //("xx", "yy") => 1 swap
+        ans += xcount/2 + ycount/2;
+        xcount %= 2; ycount %= 2;
+        
+        //("xy", "yx") => 2 swaps
+        if(xcount == 1 && ycount == 1){
+            ans += 2;
+            xcount--; ycount--;
+        }
+        
+        return (xcount == 0 && ycount == 0) ? ans : -1;
+    }
+};
