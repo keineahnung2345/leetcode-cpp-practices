358782a89ed1549fa49d9c9e4a636de667ece25b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-20 16:08:37 +0800

    Divide and Conquer

--- 856. Score of Parentheses.cpp
+++ 856. Score of Parentheses.cpp
@@ -35,3 +35,36 @@ public:
         return ans;
     }
 };
+
+//Approach 1: Divide and Conquer
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Score of Parentheses.
+//Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Score of Parentheses.
+//time: O(N^2), space: O(N)
+class Solution {
+public:
+    int F(string S, int start, int end){
+        //end is exclusive
+        int ans = 0, balance = 0;
+        
+        for(int i = start; i < end; i++){
+            balance += (S[i] == '(') ? 1 : -1;
+            if(balance == 0){
+                if(i - start == 1){
+                    //')' 's mathcing '(' is its previous char
+                    ans++;
+                }else{
+                    //')' matches to the '(' far before
+                    ans += 2 * F(S, start+1, i);
+                }
+                //the start position of new '('
+                start = i+1;
+            }
+        }
+        
+        return ans;
+    };
+    
+    int scoreOfParentheses(string S) {
+        return F(S, 0, S.size());
+    }
+};
