8e85e82c9544489d556f9d9fe2393f18c02c657d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 17:33:26 +0800

    add sol2

--- 1007. Minimum Domino Rotations For Equal Row.cpp
+++ 1007. Minimum Domino Rotations For Equal Row.cpp
@@ -81,3 +81,49 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/discuss/252242/JavaC%2B%2BPython-Different-Ideas
+//Runtime: 120 ms, faster than 83.05% of C++ online submissions for Minimum Domino Rotations For Equal Row.
+//Memory Usage: 16.7 MB, less than 100.00% of C++ online submissions for Minimum Domino Rotations For Equal Row.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    int minDominoRotations(vector<int>& A, vector<int>& B) {
+        int N = A.size();
+        int aSwap = 0, bSwap = 0;
+        
+        //try make row A(maintained by aSwap) or row B(aSwap) all A[0]
+        for(int i = 0; i < N; i++){
+            if(A[i] == A[0] || B[i] == A[0]){
+                if(A[i] != A[0]) aSwap++;
+                if(B[i] != A[0]) bSwap++;
+            }else{
+                /*
+                for position i, 
+                we cannot swap the element in A or B to make it A[0],
+                so we fail it
+                */
+                break;
+            }
+            /*
+            the condition: A[i] == A[0] || B[i] == A[0] holds for all i
+            now we can determine whether to make row A or row B all A[0]
+            */
+            if(i == N-1) return min(aSwap, bSwap);
+        }
+        
+        //try make row A or row B all B[0]
+        aSwap = 0, bSwap = 0;
+        for(int i = 0; i < N; i++){
+            if(A[i] == B[0] || B[i] == B[0]){
+                if(A[i] != B[0]) aSwap++;
+                if(B[i] != B[0]) bSwap++;
+            }else{
+                break;
+            }
+            if(i == N-1) return min(aSwap, bSwap);
+        }
+        
+        return -1;
+    }
+};
