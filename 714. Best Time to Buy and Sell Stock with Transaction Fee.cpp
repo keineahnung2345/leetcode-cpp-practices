8fa03e6ea8e12dccc9f27d90017f4c8c84cc5e4d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 10:56:04 +0800

    dp

--- 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -35,3 +35,27 @@ public:
         return profits[N-1];
     }
 };
+
+//Approach #1: Dynamic Programming
+//Runtime: 148 ms, faster than 32.31% of C++ online submissions for Best Time to Buy and Sell Stock with Transaction Fee.
+//Memory Usage: 13 MB, less than 100.00% of C++ online submissions for Best Time to Buy and Sell Stock with Transaction Fee.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            /*
+            cash[i] = cash[i-1]: do nothing
+            cash[i] = hold+prices[i]-fee: sell the stock held in last day
+            */
+            cash = max(cash, hold + prices[i] - fee);
+            /*
+            hold[i] = hold[i-1]: do nothing
+            hold = cash[i-1] - prices[i]: buy stock today
+            */
+            hold = max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+};
