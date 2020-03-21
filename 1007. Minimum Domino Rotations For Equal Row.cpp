902620c8ec087ade8330eb365b56019b0fb99d0d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-21 17:21:20 +0800

    add sol1

--- 1007. Minimum Domino Rotations For Equal Row.cpp
+++ 1007. Minimum Domino Rotations For Equal Row.cpp
@@ -35,3 +35,49 @@ public:
         return min(aSwap, bSwap);
     }
 };
+
+//https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/discuss/252242/JavaC%2B%2BPython-Different-Ideas
+//Runtime: 128 ms, faster than 42.29% of C++ online submissions for Minimum Domino Rotations For Equal Row.
+//Memory Usage: 17.1 MB, less than 100.00% of C++ online submissions for Minimum Domino Rotations For Equal Row.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    int minDominoRotations(vector<int>& A, vector<int>& B) {
+        int N = A.size();
+        vector<int> countA(7, 0), countB(7, 0), same(7, 0);
+        for(int i = 0; i < N; i++){
+            countA[A[i]]++;
+            countB[B[i]]++;
+            if(A[i] == B[i]) same[A[i]]++;
+        }
+        
+        int ans = -1;
+        for(int i = 1; i <= 6; i++){
+            /*
+            countA[i] + countB[i] - same[i]: i can fill how many position,
+            this value will always <= N
+            */
+            if(countA[i] + countB[i] - same[i] == N){
+                /*
+                if countA[i] > countB[i],
+                we flip N - countA[i] times so that 
+                row A will be all i
+                */
+                ans = N - max(countA[i], countB[i]);
+                /*
+                we can break the loop immediately because when there are 2 "i"
+                (say x and y) meets the condition 
+                "countA[i] + countB[i] - same[i] == N", 
+                same[x] and same[y] will be 0, 
+                and countA[x] will equal countB[y], 
+                countA[y] will equal countB[x],
+                so max(countA[i], countB[i]) and so ans will be the same
+                */
+                break;
+            }
+            // cout << i << " " << countA[i] << " " << countB[i] << " " << same[i] << " " << ans << endl;
+        }
+        
+        return ans;
+    }
+};
