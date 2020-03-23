f83fd285bf77027d3f9faa56c1dce4e2967f2e8d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-23 13:35:36 +0800

    Precompute, partial sort

--- 1387. Sort Integers by The Power Value.cpp
+++ 1387. Sort Integers by The Power Value.cpp
@@ -45,3 +45,42 @@ public:
         return ans;
     }
 };
+
+//Precompute, partial sort
+//https://leetcode.com/problems/sort-integers-by-the-power-value/discuss/547055/C%2B%2B-8-ms-precompute-and-partial-sort
+//Runtime: 28 ms, faster than 96.47% of C++ online submissions for Sort Integers by The Power Value.
+//Memory Usage: 7.5 MB, less than 100.00% of C++ online submissions for Sort Integers by The Power Value.
+class Solution {
+public:
+    int getPower(int x){
+        if(x == 1) return 0;
+        
+        int ans;
+        
+        if(x % 2 == 0){
+            ans = 1 + getPower(x/2);
+        }else{
+            ans = 1 + getPower(3*x+1);
+        }
+        
+        return ans;
+    };
+    
+    int getKth(int lo, int hi, int k) {
+        vector<int> computed = vector<int>(hi-lo+1, 0);
+        
+        for(int i = lo; i <= hi; i++){
+            computed[i-lo] = getPower(i);
+        }
+        
+        vector<int> ids(hi-lo+1);
+        iota(ids.begin(), ids.end(), lo);
+        
+        nth_element(ids.begin(), ids.begin()+k-1, ids.end(), 
+            [&computed, lo](const int i, const int j){
+                return (computed[i-lo] == computed[j-lo]) ? (i < j) : (computed[i-lo] < computed[j-lo]);
+            });
+        
+        return ids[k-1];
+    }
+};
