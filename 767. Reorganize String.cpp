57d681c3e4b10fe6a34290f9db65331b1502164b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-23 18:03:31 +0800

    Greedy with Heap

--- 767. Reorganize String.cpp
+++ 767. Reorganize String.cpp
@@ -1,3 +1,4 @@
+//priority queue
 //Runtime: 12 ms, faster than 13.62% of C++ online submissions for Reorganize String.
 //Memory Usage: 6.4 MB, less than 100.00% of C++ online submissions for Reorganize String.
 class Solution {
@@ -107,5 +108,59 @@ public:
         }
         
         return string(ans.begin(), ans.end());
+    }
+};
+
+//Approach #2: Greedy with Heap
+//priority queue, process two char at a time
+//Runtime: 4 ms, faster than 64.77% of C++ online submissions for Reorganize String.
+//Memory Usage: 6.6 MB, less than 100.00% of C++ online submissions for Reorganize String.
+//time: O(NlogA), space: O(A)
+class Solution {
+public:
+    string reorganizeString(string S) {
+        int N = S.size();
+        unordered_map<char, int> counter;
+        
+        for(char c : S){
+            counter[c]++;
+        }
+        
+        auto comp = [](const pair<char, int>& a, const pair<char, int>& b){
+            return (a.second == b.second) ? (a.first > b.first) : a.second < b.second;};
+        
+        //the larger count, the earlier be popped
+        //the smaller char order, the earlier be popped
+        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(comp)> pq(comp);
+        
+        for(auto it = counter.begin(); it != counter.end(); it++){
+            //early stopping
+            if(it->second > (N+1)/2) return "";
+            pq.push(make_pair(it->first, it->second));
+        }
+        
+        string ans = "";
+        
+        while(pq.size() >= 2){
+            //pop two elements at a time
+            pair<char, int> cur = pq.top(); pq.pop();
+            pair<char, int> next = pq.top(); pq.pop();
+            
+            //we don't need this check now, because we process two different char at one time 
+            // if(ans.size() > 0 && cur.first == ans[ans.size()-1]) return "";
+            ans += cur.first;
+            
+            // if(ans.size() > 0 && next.first == ans[ans.size()-1]) return "";
+            ans += next.first;
+            
+            //push them back to the queue
+            if(--cur.second > 0) pq.push(cur);
+            if(--next.second > 0) pq.push(next);
+        }
+        
+        //we jump out the loop when its size is 0 or 1
+        if(!pq.empty()) ans += pq.top().first;
+        
+        return ans;
     }
 };
