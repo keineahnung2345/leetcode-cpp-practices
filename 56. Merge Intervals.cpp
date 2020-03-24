8d9dfafb32696da53f0ff3797839b0dd21089130
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 11:14:25 +0800

    graph, dfs

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -24,6 +24,7 @@ public:
     }
 };
 
+//Approach 2: Sorting
 //the above method removes merged intervals, this method add unmerged intervals into ans
 //Runtime: 32 ms, faster than 29.99% of C++ online submissions for Merge Intervals.
 //Memory Usage: 10 MB, less than 100.00% of C++ online submissions for Merge Intervals.
@@ -46,5 +47,85 @@ public:
         }
         
         return ans;
+    }
+};
+
+//Approach 1: Connected Components
+//TLE
+//168 / 169 test cases passed.
+//time: O(build graph) + O(traverse graph) + O(merge) = O(V+E) + O(V+E) +O(V) = O(n^2+n) + O(n^2+n) + O(n) = O(n^2)
+//space: O(n^2)
+class Solution {
+public:
+    map<vector<int>, vector<vector<int>>> graph;
+    set<vector<int>> visited;
+    map<int, vector<vector<int>>> nodesInComp;
+    
+    bool overlap(vector<int>& a, vector<int>& b){
+        return a[0] <= b[1] && b[0] <= a[1];
+    }
+    
+    void buildGraph(vector<vector<int>>& intervals){
+        for(vector<int>& interval1 : intervals){
+            for(vector<int>& interval2: intervals){
+                if(overlap(interval1, interval2)){
+                    graph[interval1].push_back(interval2);
+                    graph[interval2].push_back(interval1);
+                }
+            }
+        }
+    };
+    
+    void markComponentDFS(vector<int>& start, int compNumber){
+        stack<vector<int>> stk;
+        
+        stk.push(start);
+        
+        while(!stk.empty()){
+            vector<int> node = stk.top(); stk.pop();
+            if(visited.find(node) == visited.end()){
+                visited.insert(node);
+                
+                nodesInComp[compNumber].push_back(node);
+                
+                for(vector<int>& child : graph[node]){
+                    stk.push(child);
+                }
+            }
+        }
+    };
+    
+    void buildComponents(vector<vector<int>>& intervals){
+        int compNumber = 0;
+        for(vector<int>& interval : intervals){
+            if(visited.find(interval) == visited.end()){
+                markComponentDFS(interval, compNumber);
+                compNumber++;
+            }
+        }
+    };
+    
+    vector<int> mergeNodes(vector<vector<int>> nodes){
+        int minStart = nodes[0][0];
+        int maxEnd = nodes[0][1];
+        
+        for(vector<int>& node : nodes){
+            minStart = min(minStart, node[0]);
+            maxEnd = max(maxEnd, node[1]);
+        }
+        
+        return vector<int>{minStart, maxEnd};
+    };
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        buildGraph(intervals);
+        buildComponents(intervals);
+        
+        vector<vector<int>> merged;
+        for(int comp = 0; comp < nodesInComp.size(); comp++){
+            merged.push_back(mergeNodes(nodesInComp[comp]));
+        }
+        
+        return merged;
     }
 };
