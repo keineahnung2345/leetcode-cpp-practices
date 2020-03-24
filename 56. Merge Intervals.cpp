c5703e1e8214a6b21ed4479662774a946805254b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 10:10:05 +0800

    sorting

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -23,3 +23,28 @@ public:
         return intervals;
     }
 };
+
+//the above method removes merged intervals, this method add unmerged intervals into ans
+//Runtime: 32 ms, faster than 29.99% of C++ online submissions for Merge Intervals.
+//Memory Usage: 10 MB, less than 100.00% of C++ online submissions for Merge Intervals.
+//time: O(NlogN), space: O(1) if it's in-place sorting or O(N)
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() < 2) return intervals;
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> ans;
+        
+        for(vector<int>& interval : intervals){
+            if(ans.empty() || ans[ans.size()-1][1] < interval[0]){
+                ans.push_back(interval);
+            }else{
+                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], interval[1]);
+            }
+        }
+        
+        return ans;
+    }
+};
