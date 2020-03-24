d72a586bddba6ef948208b65eb3b509141e74880
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 17:33:50 +0800

    Bit manipulation + Hashmap

--- 187. Repeated DNA Sequences.cpp
+++ 187. Repeated DNA Sequences.cpp
@@ -113,3 +113,47 @@ public:
         return ans;
     }
 };
+
+//Bit manipulation + Hashmap
+//https://leetcode.com/problems/repeated-dna-sequences/discuss/53867/Clean-Java-solution-(hashmap-%2B-bits-manipulation)
+//Runtime: 184 ms, faster than 5.84% of C++ online submissions for Repeated DNA Sequences.
+//Memory Usage: 16.3 MB, less than 100.00% of C++ online submissions for Repeated DNA Sequences.
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        set<int> words; //substrings that appear once
+        set<int> doubleWords; //appear twice
+        map<char, int> bitRepresentation;
+        
+        //we use 2 bit to represent a char
+        bitRepresentation['A'] = 0;
+        bitRepresentation['C'] = 1;
+        bitRepresentation['G'] = 2;
+        bitRepresentation['T'] = 3;
+        
+        vector<string> ans;
+        
+        // cout << s.size() << " " << (int)s.size() - 10 + 1 << endl;
+        
+        /*
+        (int)s.size():
+        Need to convert it from size_t to int!!
+        Because size_t is unsigned, and (size_t)0-10+1 will be 18446744073709551607 which is larger than 0!
+        */
+        for(int i = 0; i < (int)s.size() - 10 + 1; i++){
+            //convert s.substr(i, 10) into its bit representation
+            int value = 0;
+            for(int j = i; j < i+10; j++){
+                value = (value << 2) + bitRepresentation[s[j]];
+            }
+            
+            //if the substring appear once but not twice
+            //a.k.a, it's the second time the substring appears
+            if(!words.insert(value).second && doubleWords.insert(value).second){
+                ans.push_back(s.substr(i, 10));
+            }
+        }
+        
+        return ans;
+    }
+};
