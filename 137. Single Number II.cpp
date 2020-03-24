eb4b05a6e323d9e73c2843216d01f74e7dd2fcdd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-24 14:55:17 +0800

    137. Single Number II.cpp

---
+++ 137. Single Number II.cpp
@@ -0,0 +1,25 @@
+//Runtime: 16 ms, faster than 23.66% of C++ online submissions for Single Number II.
+//Memory Usage: 7.7 MB, less than 100.00% of C++ online submissions for Single Number II.
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        
+        for(int i = 31; i >= 0; i--){
+            int mask = 1 << i;
+            int set = 0;
+            
+            for(int num : nums){
+                if(num & mask){
+                    set++;
+                }
+            }
+            
+            if(set%3 == 1){
+                ans += mask;
+            }
+        }
+        
+        return ans;
+    }
+};
