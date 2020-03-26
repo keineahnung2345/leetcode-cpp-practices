c88de82385f98b96cbba67e907bfaf6ccdaf5980
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-27 00:38:06 +0800

    topo bfs

--- 207. Course Schedule.cpp
+++ 207. Course Schedule.cpp
@@ -62,3 +62,41 @@ public:
         return !hasCycle;
     }
 };
+
+//topological sort, bfs
+//https://leetcode.com/problems/course-schedule/discuss/58516/Easy-BFS-Topological-sort-Java
+//Runtime: 24 ms, faster than 64.54% of C++ online submissions for Course Schedule.
+//Memory Usage: 10.8 MB, less than 100.00% of C++ online submissions for Course Schedule.
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> incomingEdgeCount(numCourses, 0);
+        vector<vector<int>> edges(numCourses);
+        
+        for(vector<int>& pre : prerequisites){
+            //pre[1] points to pre[0]
+            incomingEdgeCount[pre[0]]++;
+            edges[pre[1]].push_back(pre[0]);
+        }
+        
+        queue<int> noIncomingEdges;
+        for(int i = 0; i < numCourses; i++){
+            if(incomingEdgeCount[i] == 0){
+                noIncomingEdges.push(i);
+            }
+        }
+        
+        int remainingEdgeCount = prerequisites.size();
+        while(!noIncomingEdges.empty()){
+            int cur = noIncomingEdges.front(); noIncomingEdges.pop();
+            for(int nei : edges[cur]){
+                remainingEdgeCount--;
+                if(--incomingEdgeCount[nei] == 0){
+                    noIncomingEdges.push(nei);
+                }
+            }
+        }
+        //if there are redundant edges, there is a cycle
+        return remainingEdgeCount == 0;
+    }
+};
