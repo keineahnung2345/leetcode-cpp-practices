4c9a9c23e36fc9e773f831508870330aad5cbfed
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-27 17:04:07 +0800

    segment tree

--- 307. Range Sum Query - Mutable Medium.cpp
+++ 307. Range Sum Query - Mutable Medium.cpp
@@ -95,3 +95,110 @@ public:
         return sum;
     }
 };
+
+//Approach 3: Segment Tree
+//Runtime: 44 ms, faster than 50.75% of C++ online submissions for Range Sum Query - Mutable.
+//Memory Usage: 16.7 MB, less than 100.00% of C++ online submissions for Range Sum Query - Mutable.
+//Build segment tree - time O(n), space O(n)
+//Update segment tree - time O(logn), space O(1)
+//Range Sum Query - time O(logn), space O(1)
+class NumArray {
+public:
+    vector<int> tree;
+    int n;
+    
+    NumArray(vector<int>& nums) {
+        if(nums.size() > 0){
+            n = nums.size();
+            buildTree(nums);
+        }
+    }
+    
+    void buildTree(vector<int>& nums){
+        tree = vector<int>(n*2);
+        //tree[n ... 2n-1] = nums[0 ... n-1]
+        for(int i = n, j = 0; i < n*2; i++, j++){
+            tree[i] = nums[j];
+        }
+        /*
+        tree[n-1] = tree[2n-2] + tree[2n-1]
+        if n = 8
+        tree[7] = tree[14] + tree[15]
+        tree[6] = tree[12] + tree[13]
+        tree[5] = tree[10] + tree[11]
+        tree[4] = tree[8] + tree[9]
+        
+        tree[3] = tree[6] + tree[7]
+        tree[2] = tree[4] + tree[5]
+        
+        tree[1] = tree[2] + tree[3]
+        
+        if n = 3
+        tree[2] = tree[4] + tree[5]
+        tree[1] = tree[2] + tree[3] //total sum of leaves
+        */
+        for(int i = n-1; i > 0; i--){
+            tree[i] = tree[i*2] + tree[i*2+1];
+        }
+    };
+    
+    void update(int i, int val) {
+        //recall that the leaf node takes tree[n ... 2n-1]
+        i += n;
+        tree[i] = val;
+        while(i > 0){
+            int left = i;
+            int right = i;
+            if(i % 2 == 0){
+                //it's left ndoe
+                right = i+1;
+            }else{
+                //it's right node
+                left = i-1;
+            }
+            tree[i/2] = tree[left] + tree[right];
+            i /= 2;
+        }
+    }
+    
+    int sumRange(int i, int j){
+        //recall that the leaf node takes tree[n ... 2n-1]
+        i += n;
+        j += n;
+        
+        int sum = 0;
+        
+        while(i <= j){
+            // cout << "[" << i << ", " << j << "]" << endl;
+            if(i % 2 == 1){
+                /*
+                if i is right child of its parent,
+                then we cannot use its parent's value
+                */
+                // cout << "l: " << i << endl;
+                sum += tree[i];
+                i++;
+            }
+            if(j % 2 == 0){
+                /*
+                if r is left child of its parent,
+                then we cannot use its parent's value
+                */
+                // cout << "r: " << j << endl;
+                sum += tree[j];
+                j--;
+            }
+            i /= 2;
+            j /= 2;
+        }
+        
+        return sum;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(i,val);
+ * int param_2 = obj->sumRange(i,j);
+ */
