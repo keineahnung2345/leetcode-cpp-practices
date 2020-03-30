af02023a4231992b44c88b360a495c59f33a0de7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-30 18:37:42 +0800

    use 4-D vector instead of unordered_map to speed up

--- 1397. Find All Good Strings.cpp
+++ 1397. Find All Good Strings.cpp
@@ -1,4 +1,4 @@
-//KMP & DP memo
+//KMP + DFS + Memo
 //https://leetcode.com/problems/find-all-good-strings/discuss/554806/O(N-*-len(evil))-Solution-with-memorized-DP-and-KMP-algorithm
 //TLE
 //53 / 53 test cases passed, but took too long.
@@ -75,6 +75,90 @@ public:
         this->evil = evil;
         this->m = evil.size();
         this->mod = 1e9+7;
+        computeLPSArray();
+        
+        return count(0, true, true, 0);
+    }
+};
+
+//KMP + DFS + Memo
+//use 4-D vector instead of unordered_map to speed up
+//https://leetcode.com/problems/find-all-good-strings/discuss/555591/JavaC%2B%2B-Memoization-DFS-and-KMP-with-Picture-Clean-code
+//Runtime: 108 ms, faster than 43.06% of C++ online submissions for Find All Good Strings.
+//Memory Usage: 18.5 MB, less than 100.00% of C++ online submissions for Find All Good Strings.
+class Solution {
+public:
+    int n;
+    int m;
+    int mod;
+    string s1, s2, evil;
+    vector<int> lps;
+    vector<vector<vector<vector<int>>>> memo;
+    
+    void computeLPSArray(){
+        int n = evil.size();
+        lps = vector<int>(n, 0);
+        
+        for(int i = 1, len = 0; i < n; ){
+            if(evil[i] == evil[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }else if(len > 0){
+                len = lps[len-1];
+            }else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    };
+    
+    int count(int idx, bool pre1, bool pre2, int preE){
+        //we just construct evil
+        if(preE == m) return 0;
+        //we just construct one valid string
+        if(idx == n) return 1;
+        
+        if(memo[idx][preE][pre1][pre2] != -1){
+            return memo[idx][preE][pre1][pre2];
+        }
+        
+        int total = 0;
+        //the first and last possible char of current position
+        char first = pre1 ? s1[idx] : 'a';
+        char last = pre2 ? s2[idx] : 'z';
+        
+        for(int i = first-'a'; i <= last-'a'; i++){
+            char c = i+'a';
+            
+            bool _pre1 = pre1 && (first == c);
+            bool _pre2 = pre2 && (last == c);
+            
+            int _preE = preE;
+            while(_preE!=0 && c != evil[_preE]){
+                _preE = lps[_preE-1];
+            }
+            if(c == evil[_preE]){
+                _preE++;
+            }
+            
+            total += count(idx+1, _pre1, _pre2, _preE);
+            total %= mod;
+        }
+        
+        memo[idx][preE][pre1][pre2] = total;
+        
+        return total;
+    };
+    
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+        this->n = n;
+        this->s1 = s1;
+        this->s2 = s2;
+        this->evil = evil;
+        this->m = evil.size();
+        this->mod = 1e9+7;
+        this->memo = vector(n+1, vector(evil.size()+1, vector(2, vector(2, -1))));
         computeLPSArray();
         
         return count(0, true, true, 0);
