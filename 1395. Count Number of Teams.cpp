cec137659843aebd75a17d2b876a4c85f1cd42c3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-03-31 12:05:16 +0800

    O(N^2) sol

--- 1395. Count Number of Teams.cpp
+++ 1395. Count Number of Teams.cpp
@@ -44,3 +44,34 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/count-number-of-teams/discuss/554795/C%2B%2BJava-O(n-*-n)
+//Runtime: 8 ms, faster than 87.37% of C++ online submissions for Count Number of Teams.
+//Memory Usage: 7.8 MB, less than 100.00% of C++ online submissions for Count Number of Teams.
+//time: O(N^2), space: O(1)
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int N = rating.size();
+        int ans = 0;
+        
+        for(int i = 1; i < N-1; i++){
+            vector<int> smaller(2), larger(2);
+            
+            for(int j = 0; j < N; j++){
+                //whether j is at the left of i
+                int left = (j < i);
+                if(rating[i] < rating[j]){
+                    smaller[left]++;
+                }
+                if(rating[i] > rating[j]){
+                    larger[left]++;
+                }
+            }
+            
+            ans += smaller[0]*larger[1] + smaller[1]*larger[0];
+        }
+        
+        return ans;
+    }
+};
