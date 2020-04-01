2b240988670d0607756e0bc6e2d2b01cbb38414d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-01 11:21:32 +0800

    add comments

--- 1027. Longest Arithmetic Sequence.cpp
+++ 1027. Longest Arithmetic Sequence.cpp
@@ -11,6 +11,12 @@ public:
             for(int j = i+1; j < A.size(); j++){
                 int d = A[j]-A[i];
                 //sequence length of d and ends at j
+                /*
+                when dp[d][i] not exist, 
+                dp[d][j] is first of such sequence, 
+                so its length is 2.
+                otherwise, we can concat j to the previous sequence ends at i
+                */
                 dp[d][j] = (dp[d].find(i) != dp[d].end()) ? dp[d][i]+1 : 2;
                 // cout << "j: " << j << ", [" << d << "][" << i << "]: ";
                 // if(dp[d].find(i) != dp[d].end()){
