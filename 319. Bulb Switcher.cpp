3f6090ed5be41c90b56fb603273b37a90ea72fa0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-01 22:22:02 +0800

    math sol

--- 319. Bulb Switcher.cpp
+++ 319. Bulb Switcher.cpp
@@ -166,3 +166,20 @@ public:
         return ans;
     }
 };
+
+//Math
+//https://leetcode.com/problems/bulb-switcher/discuss/77104/Math-solution..
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        /*
+        we want to find how many i in the range [1...n] whose
+        divisor count is odd.
+        A number's divisor count is odd iff it's square.
+        So we want to find out how many i in the range [1...n] is square.
+        And the count of square root in the range [1...sqrt(n)] is same as the count of square in the range[1...n],
+        so we can just give sqrt(n) as our answer
+        */
+        return sqrt(n);
+    }
+};
