4981e81f4932114f1b97d62cba427c435a66083c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-01 18:36:57 +0800

    count divisor

--- 319. Bulb Switcher.cpp
+++ 319. Bulb Switcher.cpp
@@ -23,3 +23,44 @@ public:
         return accumulate(state.begin(), state.end(), 0);
     }
 };
+
+//count divisor
+//TLE
+//30 / 35 test cases passed.
+class Solution {
+public:
+    vector<int> divisorCount;
+    
+    int getDivisorCount(int n){
+        if(divisorCount[n] == 0){
+            int count = 0; //1
+            double sqrt_n = sqrt(n);
+            
+            for(int i = 1; i <= sqrt_n; i++){
+                if(n % i == 0){
+                    if(i == sqrt_n) count++;
+                    else count += 2;
+                }
+            }
+            
+            divisorCount[n] = count;
+        }
+        return divisorCount[n];
+    };
+    
+    int bulbSwitch(int n) {
+        //sum of divisor count from 1 to n
+        divisorCount = vector<int>(n+1, 0);
+        int ans = 0;
+        
+        for(int i = 1; i <= n; i++){
+            int count = getDivisorCount(i);
+            // cout << i << " " << count << endl;
+            if(count % 2 == 1){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
