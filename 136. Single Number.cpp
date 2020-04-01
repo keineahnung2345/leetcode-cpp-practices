71d9830d4a6e7f8dad5d6a65fd4ef9d521105421
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-01 17:37:20 +0800

    fix comment

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -104,10 +104,10 @@ Approach 4: Bit Manipulation
 Concept
 
 If we take XOR of zero and some bit, it will return that bit
-a \oplus 0 = aa⊕0=a
+a⊕0=a
 If we take XOR of two same bits, it will return 0
-a \oplus a = 0a⊕a=0
-a \oplus b \oplus a = (a \oplus a) \oplus b = 0 \oplus b = ba⊕b⊕a=(a⊕a)⊕b=0⊕b=b
+a⊕a=0
+a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
 So we can XOR all bits together to find the unique number.
 **/
 
