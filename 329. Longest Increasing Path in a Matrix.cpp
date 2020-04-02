ca75aa2c61da50998e33a3ddba53dd5deab2cf50
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-02 12:39:08 +0800

    bfs, topological sort

--- 329. Longest Increasing Path in a Matrix.cpp
+++ 329. Longest Increasing Path in a Matrix.cpp
@@ -101,3 +101,67 @@ public:
         return ans;
     }
 };
+
+//BFS, Topological Sort
+//https://leetcode.com/problems/longest-increasing-path-in-a-matrix/discuss/288520/BFS-Implemented-Topological-Sort
+//Runtime: 168 ms, faster than 18.07% of C++ online submissions for Longest Increasing Path in a Matrix.
+//Memory Usage: 27.9 MB, less than 9.09% of C++ online submissions for Longest Increasing Path in a Matrix.
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        if(m == 0) return 0;
+        int n = matrix[0].size();
+        if(n == 0) return 0;
+        
+        vector<vector<int>> dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        
+        vector<vector<int>> inDegree(m, vector(n, 0));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                for(vector<int>& dir : dirs){
+                    int ni = i + dir[0];
+                    int nj = j + dir[1];
+                    if(ni >= 0 && ni < m && nj >= 0 && nj < n && matrix[i][j] < matrix[ni][nj]){
+                        //we create an edge from smaller to larger
+                        inDegree[ni][nj]++;
+                    }
+                }
+            }
+        }
+        
+        queue<vector<int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(inDegree[i][j] == 0){
+                    q.push({i, j});
+                }
+            }
+        }
+        
+        int ans = 0;
+        
+        while(!q.empty()){
+            int levelCount = q.size();
+            //visite current level
+            while(levelCount-- > 0){
+                vector<int> cur = q.front(); q.pop();
+                int ci = cur[0], cj = cur[1];
+
+                for(vector<int>& dir : dirs){
+                    int ni = ci+dir[0];
+                    int nj = cj+dir[1];
+
+                    if(ni >= 0 && ni < m && nj >= 0 && nj < n && matrix[ni][nj] > matrix[ci][cj] && --inDegree[ni][nj] == 0){
+                        //if there's an edge from (ci,cj) to (ni,nj)
+                        q.push({ni, nj});
+                    }
+                }
+            }
+            ans++;
+        }
+        
+        return ans;
+    }
+};
