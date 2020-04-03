31bfc28b4ecd39cdc79cbe03f41bd46357495f84
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-03 12:10:06 +0800

    Balanced Tree, c++ map::upper_bound, map::lower_bound

--- 729. My Calendar I.cpp
+++ 729. My Calendar I.cpp
@@ -31,3 +31,37 @@ public:
  * bool param_1 = obj->book(start,end);
  */
  
+//Approach #2: Balanced Tree, c++ map::upper_bound, map::lower_bound
+//time: O(NlogN), N operations, each search for O(logN), insert for O(1)
+//space: O(N)
+class MyCalendar {
+public:
+    map<int, int> calendar;
+    
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        //floorKey
+        auto it = calendar.upper_bound(start);
+        int prev = (it != calendar.begin()) ? (--it)->first : -1;
+        //ceilingKey
+        it = calendar.lower_bound(start);
+        int next = (it != calendar.end()) ? it->first : -1;
+        
+        if((prev == -1 || calendar[prev] <= start) &&
+          (next == -1 || end <= next)){
+            //it's valid iff calendar[prev] <= start <= end <= next
+            calendar[start] = end;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
