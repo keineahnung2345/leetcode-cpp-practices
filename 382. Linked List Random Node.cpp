e733c9aaa48bb42a9a09b9c25d598ff61870aa99
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-03 10:56:09 +0800

    Reservoir sampling

--- 382. Linked List Random Node.cpp
+++ 382. Linked List Random Node.cpp
@@ -33,3 +33,45 @@ public:
  * int param_1 = obj->getRandom();
  */
  
+//Reservoir sampling
+//https://leetcode.com/problems/linked-list-random-node/discuss/85662/Java-Solution-with-cases-explain
+//Runtime: 40 ms, faster than 60.00% of C++ online submissions for Linked List Random Node.
+//Memory Usage: 14 MB, less than 100.00% of C++ online submissions for Linked List Random Node.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *head;
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode *cur = head, *candidate = head;
+        int count = 0;
+        while(cur != nullptr){
+            if(rand()%(count+1) == count){
+                //select the (k+1) th element with prob = 1/(k+1)
+                candidate = cur;
+            }
+            cur = cur->next;
+            count++;
+        }
+        return candidate->val;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
