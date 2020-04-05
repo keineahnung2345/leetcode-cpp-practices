8bacb2d554be42bfb5b8f4865ce26ee98832daf5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-05 16:44:48 +0800

    DP

--- 1406. Stone Game III.cpp
+++ 1406. Stone Game III.cpp
@@ -1,3 +1,4 @@
+//Recursion + memorization
 //Runtime: 528 ms, faster than 33.33% of C++ online submissions for Stone Game III.
 //Memory Usage: 146.5 MB, less than 100.00% of C++ online submissions for Stone Game III.
 class Solution {
@@ -27,5 +28,30 @@ public:
         if(score == 0) return "Tie";
         if(score > 0) return "Alice";
         return "Bob";
+    }
+};
+
+//DP
+//https://leetcode.com/problems/stone-game-iii/discuss/564260/JavaC%2B%2BPython-DP-O(1)-Space
+//Runtime: 468 ms, faster than 66.67% of C++ online submissions for Stone Game III.
+//Memory Usage: 132.6 MB, less than 100.00% of C++ online submissions for Stone Game III.
+//time: O(N^2), space: O(N)
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> dp(n, INT_MIN);
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int k = 0, take = 0; k <= 2 && i+k < n; k++){
+                take += stoneValue[i+k];
+                dp[i] = max(dp[i], take - ((i+k+1<n)?dp[i+k+1]:0));
+            }
+            // cout << i << " " << dp[i] << endl;
+        }
+        
+        if(dp[0] > 0) return "Alice";
+        if(dp[0] < 0) return "Bob";
+        return "Tie";
     }
 };
