fd26a2f69a22e5c43816c0294b9ae535a6ea4f45
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-05 12:05:46 +0800

    Speed up with count_if

--- 1399. Count Largest Group.cpp
+++ 1399. Count Largest Group.cpp
@@ -34,3 +34,36 @@ public:
         return ans;
     }
 };
+
+//Speed up with count_if
+//https://leetcode.com/problems/count-largest-group/discuss/563295/C%2B%2BJava-Count
+//Runtime: 4 ms, faster than 87.50% of C++ online submissions for Count Largest Group.
+//Memory Usage: 6.2 MB, less than 100.00% of C++ online submissions for Count Largest Group.
+class Solution {
+public:
+    int digitSum(int n){
+        int sum = 0;
+        
+        while(n){
+            sum += n%10;
+            n/=10;
+        }
+        
+        return sum;
+    };
+    
+    int countLargestGroup(int n) {
+        //n's range: [1...10000], 9999's digit sum is 36, which is largest
+        vector<int> count(37);
+        int maxCount = 0;
+        
+        for(int i = 1; i <= n; i++){
+            int dsum = digitSum(i);
+            count[dsum]++;
+            maxCount = max(maxCount, count[dsum]);
+        }
+        
+        return count_if(count.begin(), count.end(), 
+            [&maxCount](int n){return n == maxCount;});
+    }
+};
