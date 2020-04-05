72fbb5520a850db82f07cb840a6cd233ef2bc1f5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-05 12:53:25 +0800

    priority queue

--- 1403. Minimum Subsequence in Non-Increasing Order.cpp
+++ 1403. Minimum Subsequence in Non-Increasing Order.cpp
@@ -20,3 +20,25 @@ public:
         return ans;
     }
 };
+
+//priority queue
+//Runtime: 32 ms, faster than 33.33% of C++ online submissions for Minimum Subsequence in Non-Increasing Order.
+//Memory Usage: 11 MB, less than 100.00% of C++ online submissions for Minimum Subsequence in Non-Increasing Order.
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int curSum = 0;
+        vector<int> ans;
+        
+        priority_queue<int, vector<int>, less<int>> pq(nums.begin(), nums.end());
+        
+        while(curSum <= sum/2.0){
+            int cur = pq.top(); pq.pop();
+            ans.push_back(cur);
+            curSum += cur;
+        }
+        
+        return ans;
+    }
+};
