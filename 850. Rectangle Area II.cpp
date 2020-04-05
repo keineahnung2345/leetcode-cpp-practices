fbb235e656ae64f7bc09b0c8c76c37e6d2f62a49
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-05 21:03:39 +0800

    Segment Tree

--- 850. Rectangle Area II.cpp
+++ 850. Rectangle Area II.cpp
@@ -189,3 +189,105 @@ public:
         return ans;
     }
 };
+
+//Approach #4: Segment Tree
+//Runtime: 24 ms, faster than 26.22% of C++ online submissions for Rectangle Area II.
+//Memory Usage: 20.8 MB, less than 25.00% of C++ online submissions for Rectangle Area II.
+//time: O(N* logN), space: O(N)
+class Node {
+public:
+    int start, end;
+    vector<int> X;
+    Node *left, *right;
+    int count;
+    long total;
+    
+    Node(int start, int end, vector<int> X){
+        this->start = start;
+        this->end = end;
+        this->X = X;
+        left = nullptr;
+        right = nullptr;
+        count = 0;
+        total = 0;
+    }
+    
+    int getRangeMid(){
+        return start + (end-start)/2;
+    }
+    
+    Node* getLeft(){
+        if(left == nullptr) left = new Node(start, getRangeMid(), X);
+        return left;
+    }
+    
+    Node* getRight(){
+        if(right == nullptr) right = new Node(getRangeMid(), end, X);
+        return right;
+    }
+    
+    long update(int i, int j, int val){
+        if(i >= j) return 0;
+        if(start == i && end == j){
+            count += val;
+        }else{
+            getLeft()->update(i, min(getRangeMid(), j), val);
+            getRight()->update(max(getRangeMid(), i), j, val);
+        }
+        
+        if(count > 0) total = X[end] - X[start];
+        else total = getLeft()->total + getRight()->total;
+        
+        return total;
+    }
+};
+
+class Solution {
+public:
+    int rectangleArea(vector<vector<int>>& rectangles) {
+        int OPEN = 1, CLOSE = -1;
+        int n = rectangles.size();
+        vector<vector<int>> events(n*2);
+        set<int> xvals_set;
+        
+        int i = 0;
+        for(vector<int>& rec : rectangles){
+            //horizontal line, open or close, x1, x2
+            events[i++] = {rec[1], OPEN, rec[0], rec[2]};
+            events[i++] = {rec[3], CLOSE, rec[0], rec[2]};
+            xvals_set.insert(rec[0]);
+            xvals_set.insert(rec[2]);
+        }
+        //sort by y
+        sort(events.begin(), events.end(),
+            [](vector<int>& a, vector<int>& b){
+                return a[0] < b[0];
+            });
+        
+        vector<int> xvals(xvals_set.begin(), xvals_set.end());
+        sort(xvals.begin(), xvals.end());
+        
+        map<int, int> rxvals;
+        for(int i = 0; i < xvals.size(); i++){
+            rxvals[xvals[i]] = i;
+        }
+        
+        int last_y = events[0][0];
+        long ans = 0;
+        long cur_x_sum = 0;
+        Node active(0, xvals.size()-1, xvals);
+        
+        for(vector<int>& event : events){
+            int y = event[0];
+            int type = event[1];
+            int x1 = event[2], x2 = event[3];
+            
+            ans += cur_x_sum * (y - last_y);
+            cur_x_sum = active.update(rxvals[x1], rxvals[x2], type);
+            last_y = y;
+        }
+        
+        ans %= (int)1e9+7;
+        return ans;
+    }
+};
