e9d9e15d4eb16f7462818902ce93aa65619ec32e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-05 12:47:19 +0800

    clean solution

--- 1402. Reducing Dishes.cpp
+++ 1402. Reducing Dishes.cpp
@@ -41,3 +41,34 @@ public:
         return ans;
     }
 };
+
+//clean
+//https://leetcode.com/problems/reducing-dishes/discuss/563384/JavaC%2B%2BPython-Easy-and-Concise
+//Runtime: 8 ms, faster than 42.86% of C++ online submissions for Reducing Dishes.
+//Memory Usage: 6.5 MB, less than 100.00% of C++ online submissions for Reducing Dishes.
+//time: O(NlogN), space: O(1)
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        //descending order
+        sort(satisfaction.rbegin(), satisfaction.rend());
+        
+        int ans = 0, sum = 0, ltc = 0;
+        
+        /*
+        because we will add (original sum + satisfaction[i]) to ans,
+        we don't want it to be negative, 
+        so we jump out if satisfaction[i] + sum <= 0
+        */
+        for(int i = 0; i < satisfaction.size() && satisfaction[i] + sum > 0; i++){
+            //cumulative sum : satisfaction[0...i]
+            sum += satisfaction[i];
+            //we add (original sum + satisfaction[i]) to ans
+            //[0...i-1]'s cooking time is increased by 1
+            //, and newly add satisfaction[i]
+            ans += sum;
+        }
+        
+        return ans;
+    }
+};
