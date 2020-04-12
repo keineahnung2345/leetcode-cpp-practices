d235a464607010f02283dd0be04d08d62654001d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-12 19:16:24 +0800

    update pq solution

--- 1046. Last Stone Weight.cpp
+++ 1046. Last Stone Weight.cpp
@@ -23,16 +23,16 @@ public:
 //https://leetcode.com/problems/last-stone-weight/discuss/294956/JavaC%2B%2BPython-Priority-Queue
 //Runtime: 0 ms, faster than 100.00% of C++ online submissions for Last Stone Weight.
 //Memory Usage: 8.4 MB, less than 100.00% of C++ online submissions for Last Stone Weight.
-
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         std::priority_queue<int> pq(stones.begin(), stones.end());
-        while(pq.size() > 1){
-            int x = pq.top(); pq.pop();
+        while(pq.size() >= 2){
+            //y is always >= x
             int y = pq.top(); pq.pop();
+            int x = pq.top(); pq.pop();
             
-            if(x != y) pq.push(abs(x-y));
+            if(x != y) pq.push(y-x);
         }
         
         //note its pq.top() not pq[0]!
