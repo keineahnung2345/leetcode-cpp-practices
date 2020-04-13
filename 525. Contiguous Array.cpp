f68b554e9b6e68f603a91b13266c9d94517d4626
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-13 22:58:13 +0800

    Approach #2 Using Extra Array

--- 525. Contiguous Array.cpp
+++ 525. Contiguous Array.cpp
@@ -24,3 +24,28 @@ public:
         return ans;
     }
 };
+
+//Approach #2 Using Extra Array [Accepted]
+//Runtime: 96 ms, faster than 90.38% of C++ online submissions for Contiguous Array.
+//Memory Usage: 20 MB, less than 41.67% of C++ online submissions for Contiguous Array.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        vector<int> arr(2*nums.size()+1, -2);
+        
+        arr[nums.size()] = -1;
+        int ans = 0, count = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            count += (nums[i] == 0 ? -1 : 1);
+            if(arr[count + nums.size()] >= -1){
+                ans = max(ans, i-arr[count+nums.size()]);
+            }else{
+                arr[count+nums.size()] = i;
+            }
+        }
+        
+        return ans;
+    }
+};
