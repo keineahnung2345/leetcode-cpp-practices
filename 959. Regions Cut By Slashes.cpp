8e12f951e98a15aaba8ec0362e94a91f924a28a8
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-17 21:07:22 +0800

    add comment

--- 959. Regions Cut By Slashes.cpp
+++ 959. Regions Cut By Slashes.cpp
@@ -11,13 +11,16 @@ public:
     }
     
     int find(int x){
-        if(parent[x] != x){
+        if(parent[x] != x){ //initial state
             parent[x] = find(parent[x]);
         }
         return parent[x];
     }
     
     void unite(int x, int y){
+        // not "parent[x] = find(parent[y]);"
+        // not "parent[y] = find(parent[x]);"
+        // combine two component's head together
         parent[find(x)] = find(y);
     }
 };
