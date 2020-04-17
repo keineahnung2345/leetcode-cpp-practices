9d9e63419a5cae1596035c1003f971e74b65c312
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-17 21:09:58 +0800

    union find

--- 200. Number of Islands.cpp
+++ 200. Number of Islands.cpp
@@ -52,3 +52,77 @@ public:
         return ans;
     }
 };
+
+//Union Find
+//Runtime: 24 ms, faster than 16.98% of C++ online submissions for Number of Islands.
+//Memory Usage: 9.1 MB, less than 100.00% of C++ online submissions for Number of Islands.
+class DSU{
+public:
+    vector<int> parent;
+    
+    DSU(int n){
+        parent= vector<int>(n);
+        iota(parent.begin(), parent.end(), 0);
+    };
+    
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    };
+    
+    void unite(int x, int y){
+        // parent[x] = find(parent[y]);
+        // parent[y] = find(parent[x]);
+        parent[find(x)] = find(y);
+    };
+};
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        if(m == 0) return 0;
+        int n = grid[0].size();
+        
+        vector<vector<int>> dirs = {
+            {1,0},
+            {-1,0},
+            {0,1},
+            {0,-1}
+        };
+        
+        DSU dsu(m*n);
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                // cout << "cur: " << i << " " << j << endl;
+                if(grid[i][j] != '1') continue;
+                for(vector<int>& dir : dirs){
+                    int newI = i + dir[0];
+                    int newJ = j + dir[1];
+                    if(newI >= 0 && newI < m && newJ >= 0 && newJ < n && grid[newI][newJ] == '1'){
+                        // cout << newI << " " << newJ << endl;
+                        dsu.unite(i*n+j, newI*n+newJ);
+                        // cout << "(" << i << ", " << j << ", " << dsu.parent[i*n+j] << "), (" << newI << ", " << newJ << ", " << dsu.parent[newI*n+newJ] << ")" << endl;
+                    }
+                    
+                }
+            }
+        }
+        
+        set<int> uniqParent;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    // cout << i << " " << j << " " <<　dsu.find(i*n+j) << endl;
+                    uniqParent.insert(dsu.find(i*n+j));
+                }
+            }
+        }
+        
+        return uniqParent.size();
+    }
+};
