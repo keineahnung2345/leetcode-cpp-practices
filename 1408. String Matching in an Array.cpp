85a47de3c6b434ef35f23d84d4a592f880b224a3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-18 10:14:04 +0800

    Suffix tree

--- 1408. String Matching in an Array.cpp
+++ 1408. String Matching in an Array.cpp
@@ -18,3 +18,73 @@ public:
         return vector<string>(ans.begin(), ans.end());
     }
 };
+
+class Node{
+public:
+    vector<Node*> children;
+    
+    Node(){
+        children = vector<Node*>(26, nullptr);
+    }
+};
+
+class SuffixTree{
+public:
+    Node* root = new Node();
+    
+    void add(string& word){
+        Node* cur = root;
+        for(char c: word){
+            if(cur->children[c-'a'] == nullptr){
+                cur->children[c-'a'] = new Node();
+            }
+            cur = cur->children[c-'a'];
+        }
+    };
+    
+    bool get(string& word){
+        Node* cur = root;
+        for(char c : word){
+            if(cur->children[c-'a'] == nullptr){
+                return false;
+            }
+            cur = cur->children[c-'a'];
+        }
+        return true;
+    };
+};
+
+//Suffix tree
+//https://leetcode.com/problems/string-matching-in-an-array/discuss/575147/Clean-Python-3-suffix-trie-O(NlogN-%2B-N-*-S2)
+//Time: O(NlogN + N * S^2), where S is the max length of all words.
+//NlogN for sorting and N * S^2 for build suffix trie.
+//Space: O(N * S^2) for suffix trie
+//Runtime: 28 ms, faster than 29.06% of C++ online submissions for String Matching in an Array.
+//Memory Usage: 29.1 MB, less than 100.00% of C++ online submissions for String Matching in an Array.
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        SuffixTree* st = new SuffixTree();
+        vector<string> ans;
+        
+        //sort by their length, descending
+        //because the longer word cannot be a suffix of a shorter word
+        sort(words.begin(), words.end(), [](string& a, string& b){
+            return a.size() > b.size();
+        });
+        
+        for(string& word : words){
+            // cout << word << endl;
+            if(st->get(word)){
+                ans.push_back(word);
+            }
+            //add suffix of word into the suffix tree
+            for(int i = 0; i < word.size(); i++){
+                string suffix = word.substr(i, word.size()-i);
+                st->add(suffix);
+            }
+        }
+        
+        return ans;
+    }
+};
