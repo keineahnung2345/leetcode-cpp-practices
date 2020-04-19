03345f681f93d9f04cc044f90a5a9367b6ac388a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-19 19:18:41 +0800

    DP without extra space!

--- 64. Minimum Path Sum.cpp
+++ 64. Minimum Path Sum.cpp
@@ -82,3 +82,29 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+//DP without extra space!
+//https://leetcode.com/problems/minimum-path-sum/discuss/23457/C%2B%2B-DP
+//Runtime: 8 ms, faster than 84.87% of C++ online submissions for Minimum Path Sum.
+//Memory Usage: 8.1 MB, less than 100.00% of C++ online submissions for Minimum Path Sum.
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        if(m == 0) return 0;
+        int n = grid[0].size();
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                //INT_MAX means invalid
+                int top = (i-1>=0) ? grid[i-1][j] : INT_MAX;
+                int left = (j-1>=0) ? grid[i][j-1] : INT_MAX;
+                grid[i][j] += (min(top, left) == INT_MAX)?0:min(top, left);
+                // cout << grid[i][j] << " ";
+            }
+            // cout << endl;
+        }
+        
+        return grid[m-1][n-1];
+    }
+};
