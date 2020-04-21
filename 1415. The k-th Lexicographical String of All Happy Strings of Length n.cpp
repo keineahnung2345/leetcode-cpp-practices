294818e86cd207ad6caa2e732de74137cf9e0690
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-21 21:43:21 +0800

    math

--- 1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
+++ 1415. The k-th Lexicographical String of All Happy Strings of Length n.cpp
@@ -47,3 +47,49 @@ public:
         return ans;
     }
 };
+
+//Math
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for The k-th Lexicographical String of All Happy Strings of Length n.
+//Memory Usage: 5.8 MB, less than 100.00% of C++ online submissions for The k-th Lexicographical String of All Happy Strings of Length n.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        //there are total 3*pow(2, n-1) happy strings
+        int prem = 1 << (n-1);
+        if(k > 3 * prem) return "";
+        
+        //k falls in (1) [1, prem] -> 'a' (2) [prem+1, prem*2] -> 'b' (3) [prem*2+1, prem*3] -> 'c'
+        //(k-1)/prem: which interval(0,1 or 2) current char is in
+        string ans(1, 'a' + (k-1)/prem);
+        
+        // cout << k << " " << prem << " " << (char)('a'+(k-1)/prem) << endl;
+        
+        // while(prem > 1){
+        while(--n > 0){
+            //we have consumed one position
+            /*
+            (k-1)/prem: which interval current char is in
+            prem: each interval's length
+            previous intervals have taken "(k - 1) / prem * prem" spaces,
+            so we need to remove them
+            
+            for 1st iteration, possible intervals are (0,1,2) (we can choose from a, b or c)
+            for later iteration, possible intervals are (0,1) (we can only choose two character different from last character)
+            */
+            k -= (k - 1) / prem * prem;
+            prem >>= 1;
+            /*
+            n = 3
+            ["aba", "abc", "aca", "acb", "bab", "bac", "bca", "bcb", "cab", "cac", "cba", "cbc"]
+            
+            for interval 0, choose from a or b
+            for interval 1, choose from b or c
+            */
+            // cout << k << " " << prem << " " << (k-1)/prem << endl;
+            ans += (k - 1) / prem == 0 ? 'a' + (ans.back() == 'a') : 'b' + (ans.back() != 'c');
+        }
+        
+        return ans;
+    }
+};
