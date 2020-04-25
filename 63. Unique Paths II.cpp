67682a46a751cdd4cab5fb4bf2e8c91e2f7dd62e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-25 22:53:45 +0800

    DP, O(N) space

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -64,3 +64,26 @@ public:
         return dp[m][n];
     }
 };
+
+//DP, O(N) space
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        
+        vector<long long int> row(n+1, 0LL);
+        
+        row[1] = 1;
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 && j == 1) continue;
+                row[j] = (obstacleGrid[i-1][j-1] == 1) ? 0 : (row[j] + row[j-1]);
+            }
+        }
+        
+        return row[n];
+    }
+};
