f6dce3180964f28bf14f63f8a6f01ba208b9a76c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-25 20:58:53 +0800

    DP, O(n) space

--- 62. Unique Paths.cpp
+++ 62. Unique Paths.cpp
@@ -47,3 +47,28 @@ public:
         return dp[m][n];
     }
 };
+
+//DP, O(n) space
+//https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //base case: top boundary and left boundary
+        // vector<vector<int>> dp(m, vector(n, 1));
+        vector<int> row(n, 1);
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                // dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                /*
+                row[j] represnets for the value in last row,
+                just like dp[i-1][j]
+                */
+                row[j] = row[j] + row[j-1];
+            }
+        }
+        
+        // return dp[m-1][n-1];
+        return row[n-1];
+    }
+};
