3d7aa3c392dbad719ddee930fac22e582d9503d6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-25 23:53:45 +0800

    add comments

--- 123. Best Time to Buy and Sell Stock III.cpp
+++ 123. Best Time to Buy and Sell Stock III.cpp
@@ -6,14 +6,25 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         //the cash in hand when we have bought 1 or 2 stocks
+        /*
+        we need to initialize hold1 and hold2 to INT_MIN, 
+        because they are required to compared with "-price"
+        */
         int hold1 = INT_MIN, hold2 = INT_MIN;
         //the cash in hand when we have sold 1 or 2 stocks
+        /*
+        the minimum of release1 and release2 is 0, 
+        no need to be INT_MIN
+        */
         int release1 = 0, release2 = 0;
         
         for(int price : prices){
             //their values depend on the value in previous iteration, so their order is required to be like this
+            //release2 is always >= release1
             release2 = max(release2, hold2+price);
+            //hold2 is always >= hold1
             hold2 = max(hold2, release1-price);
+            //release1 is always >= 0
             release1 = max(release1, price+hold1);
             hold1 = max(hold1, -price);
         }
