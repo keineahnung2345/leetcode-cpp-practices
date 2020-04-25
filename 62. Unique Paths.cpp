6f3e097d7d0ea57e1dc3520803fc9cf77ac5b236
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-25 20:52:36 +0800

    dp, cleaner

--- 62. Unique Paths.cpp
+++ 62. Unique Paths.cpp
@@ -26,3 +26,24 @@ public:
         return grid[m-1][n-1];
     }
 };
+
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Paths.
+//Memory Usage: 6.5 MB, less than 100.00% of C++ online submissions for Unique Paths.
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+        
+        //base case
+        dp[1][1] = 1;
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1 && j == 1) continue;
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
