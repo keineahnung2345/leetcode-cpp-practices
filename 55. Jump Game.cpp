29e67afa985f05ecf450dff53db2b8b184da26a7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-25 21:17:38 +0800

    TLE, Graph

--- 55. Jump Game.cpp
+++ 55. Jump Game.cpp
@@ -1,5 +1,4 @@
 //TLE
-
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -33,6 +32,42 @@ public:
         }
         
         return dp[0];
+    }
+};
+
+//TLE
+//74 / 75 test cases passed.
+//Graph
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        map<int, vector<int>> graph;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = max(0, i-nums[i]); j <= min(n-1, i+nums[i]); j++){
+                graph[i].push_back(j);
+            }
+        }
+        
+        queue<int> q;
+        q.push(0);
+        
+        vector<bool> visited(n, false);
+        visited[0] = true;
+        
+        while(!q.empty()){
+            int cur = q.front(); q.pop();
+            
+            for(int next : graph[cur]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    q.push(next);
+                }
+            }
+        }
+        
+        return visited[n-1];
     }
 };
 
