8a3d548fc71e99a1d7385251768b30b28eacaf89
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-26 00:02:20 +0800

    DP, consider next 2 houses

--- 198. House Robber.cpp
+++ 198. House Robber.cpp
@@ -13,7 +13,7 @@ public:
 };
 **/
 
-//DP
+//DP, consider 2nd and 3rd next houses
 //Runtime: 4 ms, faster than 100.00% of C++ online submissions for House Robber.
 //Memory Usage: 8.6 MB, less than 100.00% of C++ online submissions for House Robber.
 
@@ -32,5 +32,30 @@ public:
             money[i] += max(money[i+2], money[i+3]);
         }
         return max(money[0], money[1]);
+    }
+};
+
+//DP, consider next 2 houses
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for House Robber.
+//Memory Usage: 6.4 MB, less than 100.00% of C++ online submissions for House Robber.
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n);
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        for(int i = 2; i < n; i++){
+            //if we rob the ith house, we can add dp[i-2] because there is no alear
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+        
+        return dp[n-1];
     }
 };
