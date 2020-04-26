2caeae2c91ceb307897ac43c60dfbf8c8b728349
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-26 09:40:27 +0800

    DP, forward

--- 213. House Robber II.cpp
+++ 213. House Robber II.cpp
@@ -27,3 +27,40 @@ public:
         // return max(dpA[0], dpB[1]);
     }
 };
+
+//DP, forward
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for House Robber II.
+//Memory Usage: 6.3 MB, less than 100.00% of C++ online submissions for House Robber II.
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        
+        vector<int> dpNoLast(n-1);
+        vector<int> dpNoFirst(n); //padding 0 first
+        
+        dpNoLast[0] = nums[0];
+        dpNoLast[1] = max(nums[0], nums[1]);
+        
+        for(int i = 2; i < dpNoLast.size(); i++){
+            //if we rob the ith house, we can add dp[i-2] because there is no alear
+            dpNoLast[i] = max(dpNoLast[i-1], dpNoLast[i-2]+nums[i]);
+        }
+        
+        //start from index 1
+        //index 0 is just used for padding
+        dpNoFirst[1] = nums[1];
+        dpNoFirst[2] = max(nums[1], nums[2]);
+        
+        for(int i = 3; i < dpNoFirst.size(); i++){
+            //if we rob the ith house, we can add dp[i-2] because there is no alear
+            dpNoFirst[i] = max(dpNoFirst[i-1], dpNoFirst[i-2]+nums[i]);
+        }
+        
+        return max(dpNoLast.back(), dpNoFirst.back());
+    }
+};
