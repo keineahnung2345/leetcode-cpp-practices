84c6d9328ff08f583dadd41363b7d0b12107b2d1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-27 23:18:11 +0800

    DP, O(n) space

--- 221. Maximal Square.cpp
+++ 221. Maximal Square.cpp
@@ -69,3 +69,43 @@ public:
         return maxslen * maxslen;
     }
 };
+
+//DP, O(n) space
+//Runtime: 20 ms, faster than 77.64% of C++ online submissions for Maximal Square.
+//Memory Usage: 8.6 MB, less than 100.00% of C++ online submissions for Maximal Square.
+//time: O(mn), space: O(n)
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        if(m == 0) return 0;
+        int n = matrix[0].size();
+        
+        int maxslen = 0;
+        vector<int> dp(n+1, 0);
+        
+        int prev = 0; //top-left
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                /*
+                dp[j] of previous row
+                it will be used as 'prev' in next j,
+                at that time, it means the dp value of top-left corner
+                */
+                int tmp = dp[j];
+                if(matrix[i-1][j-1] == '0'){
+                    dp[j] = 0;
+                }else{
+                    //check top, left and top-left
+                    dp[j] = min({dp[j], dp[j-1], prev}) + 1;
+                    maxslen = max(maxslen, dp[j]);
+                }
+                
+                prev = tmp;
+            }
+        }
+        
+        return maxslen * maxslen;
+    }
+};
