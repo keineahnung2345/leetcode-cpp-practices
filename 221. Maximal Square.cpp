ec7746aebe3377dec467aa52ccf3a998583ee975
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-27 23:11:12 +0800

    Dynamic Programming

--- 221. Maximal Square.cpp
+++ 221. Maximal Square.cpp
@@ -42,3 +42,30 @@ public:
         return maxslen * maxslen;
     }
 };
+
+//Approach #2 (Dynamic Programming) [Accepted]
+//Runtime: 24 ms, faster than 33.17% of C++ online submissions for Maximal Square.
+//Memory Usage: 8.7 MB, less than 100.00% of C++ online submissions for Maximal Square.
+//time: O(mn), space: O(mn)
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        if(m == 0) return 0;
+        int n = matrix[0].size();
+        
+        int maxslen = 0;
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(matrix[i-1][j-1] == '0') continue;
+                //check top, left and top-left
+                dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+                maxslen = max(maxslen, dp[i][j]);
+            }
+        }
+        
+        return maxslen * maxslen;
+    }
+};
