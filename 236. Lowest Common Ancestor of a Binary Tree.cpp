c9efb0f2b3c8f0551f2808e558a11f68297f3cc7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-29 22:36:21 +0800

    Recursive Approach

--- 236. Lowest Common Ancestor of a Binary Tree.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -71,3 +71,48 @@ public:
         return p;
     }
 };
+
+//Approach 1: Recursive Approach
+//Runtime: 20 ms, faster than 79.55% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+//Memory Usage: 14.3 MB, less than 100.00% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+//time: O(N), space: O(N)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *p, *q, *ans;
+    
+    bool recurseTree(TreeNode* cur){
+        if(cur == nullptr) return false;
+        
+        int mid = (int)(cur == p || cur == q);
+        int left = (int)recurseTree(cur->left);
+        int right = (int)recurseTree(cur->right);
+        
+        /*
+        mid & left : current node is lca
+        mid & right: current node is lca
+        left& right: their parent, which is current node, is lca
+        */
+        if(mid + left + right >= 2){
+            ans = cur;
+        }
+        
+        //if itself or one of its children is p or q's ancestor
+        return mid + left + right > 0;
+    };
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        this->p = p;
+        this->q = q;
+        recurseTree(root);
+        return ans;
+    }
+};
