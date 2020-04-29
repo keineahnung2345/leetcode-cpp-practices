dbdc733d031198d48394531e72c4fcee1565e4e5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-29 20:23:34 +0800

    236. Lowest Common Ancestor of a Binary Tree.cpp

---
+++ 236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,73 @@
+//Runtime: 40 ms, faster than 12.32% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+//Memory Usage: 15.9 MB, less than 100.00% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int pLevel = -1, qLevel = -1;
+        
+        queue<TreeNode*> bfsq;
+        TreeNode* cur;
+        int levelCount = 0;
+        int level = 0;
+        map<TreeNode*, TreeNode*> parents;
+        
+        bfsq.push(root);
+        parents[root] = nullptr;
+        levelCount++;
+        
+        while(!bfsq.empty()){
+            // cout << level << " " << levelCount << endl;
+            while(levelCount-- > 0){
+                cur = bfsq.front(); bfsq.pop();
+                if(cur == p){
+                    pLevel = level;
+                }
+                if(cur == q){
+                    qLevel = level;
+                }
+                if(cur->left){
+                    bfsq.push(cur->left);
+                    parents[cur->left] = cur;
+                }
+                if(cur->right){
+                    bfsq.push(cur->right);
+                    parents[cur->right] = cur;
+                }
+            }
+            level++;
+            levelCount = bfsq.size();
+        }
+        
+        //lift the lower one to the same level
+        while(qLevel > pLevel){
+            qLevel--;
+            q = parents[q];
+        }
+        
+        while(pLevel > qLevel){
+            pLevel--;
+            p = parents[p];
+        }
+        
+        //now p and q are at same level
+        if(p == q) return p;
+        
+        while(p != q){
+            p = parents[p];
+            q = parents[q];
+        }
+        
+        // cout << pLevel << " " << qLevel << endl;
+        
+        return p;
+    }
+};
