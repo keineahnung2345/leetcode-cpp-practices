81b75d3308312042235c74744ba93d8e3e64eb5a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-30 23:19:39 +0800

    DP+Sliding window

--- 1423. Maximum Points You Can Obtain from Cards.cpp
+++ 1423. Maximum Points You Can Obtain from Cards.cpp
@@ -28,3 +28,26 @@ public:
         return cur[n][k];
     }
 };
+
+//DP+Sliding window
+//Runtime: 148 ms, faster than 76.21% of C++ online submissions for Maximum Points You Can Obtain from Cards.
+//Memory Usage: 42.5 MB, less than 100.00% of C++ online submissions for Maximum Points You Can Obtain from Cards.
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        //the remaining cards must be continuous
+        int remainCount = n - k;
+        int remainSum = accumulate(cardPoints.begin(), cardPoints.begin()+n-k, 0);
+        int minRemainSum = remainSum;
+        int totalSum = remainSum;
+        
+        for(int i = n-k; i < n; i++){
+            remainSum += (cardPoints[i] - cardPoints[i-(n-k)]);
+            minRemainSum = min(minRemainSum, remainSum);
+            totalSum += cardPoints[i];
+        }
+        
+        return totalSum - minRemainSum;
+    }
+};
