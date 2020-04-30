5dd6590e721f5aa68da733a3b8546b96084d913a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-30 23:02:59 +0800

    One Pass

--- 1422. Maximum Score After Splitting a String.cpp
+++ 1422. Maximum Score After Splitting a String.cpp
@@ -29,3 +29,32 @@ public:
         return ans;
     }
 };
+
+//One Pass
+//https://leetcode.com/problems/maximum-score-after-splitting-a-string/discuss/597716/Java-5-Liner-(One-Pass)
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Maximum Score After Splitting a String.
+//Memory Usage: 6.5 MB, less than 100.00% of C++ online submissions for Maximum Score After Splitting a String.
+class Solution {
+public:
+    int maxScore(string s) {
+        int zeros = 0, ones = 0, ans = INT_MIN;
+        int n = s.size();
+        /*
+        ans 
+        = max(lZero+rOne)
+        = max(lZero+totalOne-lOne)
+        = max(lZero-lOne) + totalOne
+        */
+        for(int i = 0; i < n; i++){
+            if(s[i] == '0') zeros++;
+            else ones++;
+            /*
+            there should be at least one char in the right part,
+            so don't update ans in the last iteration
+            */
+            if(i != n-1) ans = max(ans, zeros-ones);
+        }
+        
+        return ans + ones;
+    }
+};
