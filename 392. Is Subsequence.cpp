fcb96083154ea2ee120e4c6c6bdb9825aac760d6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-01 17:39:33 +0800

    skip redundant comparison

--- 392. Is Subsequence.cpp
+++ 392. Is Subsequence.cpp
@@ -60,10 +60,50 @@ public:
         
         vector<int> ans(n);
         for(int i = 0; i < ss.size(); i++){
-            ans[i] == (cursors[i] == ss[i].size());
+            ans[i] = (cursors[i] == ss[i].size());
         }
         
         return ans;
     }
 };
 
+//Follow-up, compare vector<string> ss(S1,S2,...Sk) with string t
+//https://leetcode.com/problems/is-subsequence/discuss/87272/3-lines-C/92233
+//skip redundant comparison
+//O(len(t) + sum(len(s[i])))
+class Solution {
+public:
+    bool isSubsequence(vector<string> ss, string t) {
+        int n = ss.size();
+        vector<int> cursors(n, 0);
+        vector<vector<int>> waitingList(26);
+        
+        for(int i = 0; i < n; i++){
+            /*
+            for the char (ss[i][0]-'a'), 
+            maintain the index of string in ss whose first char is that
+            */
+            waitingList[ss[i][0]-'a'].push_back(i);
+        }
+        
+        for(char c : t){
+            vector<int> old = waitingList[c-'a'];
+            waitingList[c-'a'].clear();
+            
+            //only visit the string in ss who are waiting for c
+            for(int i : old){
+                //ss[i] matches, move its cursor forward
+                cursors[i] += 1;
+                //update waitingList concurrently
+                waitingList[cursors[i]-'a'].push_back(i);
+            }
+        }
+        
+        vector<int> ans(n);
+        for(int i = 0; i < ss.size(); i++){
+            ans[i] = (cursors[i] == ss[i].size());
+        }
+        
+        return ans;
+    }
+};
