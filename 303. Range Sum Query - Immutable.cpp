e59f5c4fc16a5548d7163896d8954ad93ee0a865
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-01 11:52:03 +0800

    add comments

--- 303. Range Sum Query - Immutable.cpp
+++ 303. Range Sum Query - Immutable.cpp
@@ -39,13 +39,15 @@ public:
     NumArray(vector<int>& nums) {
         int N = nums.size();
         this->acc_sum = vector<int>(N+1);
+        //for padding
         this->acc_sum[0] = 0;
-        for(int i = 1; i < N+1; i++){
+        for(int i = 1; i <= N; i++){
             this->acc_sum[i] = this->acc_sum[i-1] + nums[i-1];
         }
     }
     
     int sumRange(int i, int j) {
+        //convert i and j from 0-based to 1-based
         return this->acc_sum[j+1] - this->acc_sum[i];
     }
 };
