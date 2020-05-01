d5cbd5f2cc65e8320afe8c279b7d45469b656eac
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-01 20:18:57 +0800

    binary seach, different stop condition

--- 278. First Bad Version.cpp
+++ 278. First Bad Version.cpp
@@ -53,11 +53,43 @@ public:
                 /*
                 when right is equal to left+1, 
                 mid is also left,
-                we should jump out because we have found 
+                we should jump out (left < right = mid breaks),  
+                because we have found 
                 the first bad version which is mid, 
                 and mid is equal to left
                 */
                 right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+};
+
+//binary seach, different stop condition
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for First Bad Version.
+//Memory Usage: 5.9 MB, less than 100.00% of C++ online submissions for First Bad Version.
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            // cout << left << ", " << mid << ", " << right << endl;
+            if(isBadVersion(mid)){
+                /*
+                when right is equal to left+1, 
+                mid is also left,
+                we should jump out (left = mid <= right = mid-1 breaks), 
+                because we have found 
+                the first bad version which is mid, 
+                and mid is equal to left
+                */
+                right = mid-1;
             }else{
                 left = mid+1;
             }
