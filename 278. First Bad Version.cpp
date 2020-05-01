35dcf30e558b2dd99d76f4f3a0741cbc2e3d9b46
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-01 18:40:15 +0800

    278. First Bad Version.cpp

---
+++ 278. First Bad Version.cpp
@@ -0,0 +1,36 @@
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for First Bad Version.
+//Memory Usage: 6.3 MB, less than 100.00% of C++ online submissions for First Bad Version.
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        //n is 1-based
+        if(n == 1) return 1;
+        //first -1 to avoid overflow
+        int left = 1, right = n;
+        int mid = left + (right-left)/2;
+        //-1: meaningless, 0: false, 1: true
+        unordered_map<int, bool> cache;
+        // cout << n+1 << " space allocated." << endl;
+        
+        do{
+            // cout << left << ", " << mid << ", " << right << endl;
+            cache[mid] = isBadVersion(mid);
+            if(cache[mid]){
+                //search left part
+                //7->4, 6->3
+                right = mid-1;
+            }else{
+                //search right part
+                left = mid+1;
+
+            }
+            mid = left + (right-left)/2;
+            // if(mid == 1) break; //mid[1-1] is meaningless
+        }while((left <= right) && !(cache.find(mid-1)!= cache.end() && cache.find(mid)!= cache.end() && !cache[mid-1] && cache[mid]));
+        
+        return mid;
+    }
+};
