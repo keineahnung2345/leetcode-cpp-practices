da87af3bdc276097a1ea0540f0c7fbbcb270d616
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-01 11:44:35 +0800

    deque, clean

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -69,3 +69,36 @@ public:
         return ans;
     }
 };
+
+//deque, clean
+//https://leetcode.com/problems/sliding-window-maximum/discuss/65884/Java-O(n)-solution-using-deque-with-explanation
+//Runtime: 36 ms, faster than 99.52% of C++ online submissions for Sliding Window Maximum.
+//Memory Usage: 16.4 MB, less than 21.31% of C++ online submissions for Sliding Window Maximum.
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n-k+1);
+        deque<pair<int, int>> window; //decreasing
+        
+        for(int i = 0; i < nums.size(); i++){
+            //window range: i-k+1, ... , i
+            
+            //keep it decreasing
+            while((window.size() > 0) && (nums[i] > window.back().first)){
+                window.pop_back();
+            }
+            window.push_back(make_pair(nums[i], i));
+            
+            //discard the element outside window range
+            if(window.front().second == i-k){
+                window.pop_front();
+            }
+            
+            //start to construct ans vector when we first see k elements
+            if(i >= k-1) ans[i-(k-1)] = window.front().first;
+        }
+        
+        return ans;
+    }
+};
