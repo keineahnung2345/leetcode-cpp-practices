17af354654b45575e788ff96f2b2a969603d3d33
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-03 16:26:58 +0800

    add comment

--- 383. Ransom Note.cpp
+++ 383. Ransom Note.cpp
@@ -18,6 +18,11 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> count(26);
         
+        /*
+        need to visit all chars in magazine first,
+        then to visit chars in ransomNote,
+        because we may use chars in magazine in any order
+        */
         for(char c : magazine){
             count[c-'a'] += 1;
         }
