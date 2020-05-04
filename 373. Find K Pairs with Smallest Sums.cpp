e8c745c8fa72f03ad486c9ebc9f0e9e6fa02f0d3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-04 18:48:50 +0800

    pq alternative

--- 373. Find K Pairs with Smallest Sums.cpp
+++ 373. Find K Pairs with Smallest Sums.cpp
@@ -12,6 +12,14 @@ class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         priority_queue<vector<int>, vector<vector<int>>, pairSumGreater> pq;
+        /*
+        or
+        auto comp = [&nums1, &nums2](const vector<int>& p1, const vector<int>& p2){
+            return p1[0]+p1[1] > p2[0]+p2[1];
+        };
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
+        */
         for(int i = 0; i < nums1.size(); i++){
             for(int j = 0; j< nums2.size(); j++){
                 pq.push({nums1[i], nums2[j]});
