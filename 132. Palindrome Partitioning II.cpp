3d9d4ed2e926b1c02a89c7f152f845ea84589563
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-07 20:18:17 +0800

    DP, start from center

--- 132. Palindrome Partitioning II.cpp
+++ 132. Palindrome Partitioning II.cpp
@@ -112,3 +112,50 @@ public:
         return ans-1;
     }
 };
+
+//DP, start from center
+//https://leetcode.com/problems/palindrome-partitioning-ii/discuss/42198/My-solution-does-not-need-a-table-for-palindrome-is-it-right-It-uses-only-O(n)-space./112629
+//Runtime: 12 ms, faster than 88.70% of C++ online submissions for Palindrome Partitioning II.
+//Memory Usage: 6.3 MB, less than 100.00% of C++ online submissions for Palindrome Partitioning II.
+class Solution {
+public:
+    int minCut(string s) {
+        int n = s.size();
+        if(n <= 1) return 0;
+        
+        vector<int> dp(n);
+        iota(dp.begin(), dp.end(), 0);
+        
+        int w, choice;
+        //start from center = 1, because dp[0] is always 0
+        for(int c = 1; c < n; c++){
+            w = 0;
+            //odd
+            //c-w: left, c+w: right
+            while(c-w >= 0 && c+w < n && s[c-w] == s[c+w]){
+                //c-w-1: left-1
+                choice = (c-w-1 >= 0) ? dp[c-w-1]+1 : 0;
+                //update dp[end], not dp[center]!
+                dp[c+w] = min(dp[c+w], choice);
+                w++;
+            }
+            
+            //even
+            w = 0;
+            
+            /*
+            since we start from c = 1,
+            we need to take care of s[0],
+            so we set left = c-w-1 and right = c+w,
+            not left = c-w and right = c+w+1
+            */
+            while(c-w-1 >= 0 && c+w < n && s[c-w-1] == s[c+w]){
+                choice = (c-w-1-1 >= 0) ? dp[c-w-1-1]+1 : 0;
+                dp[c+w] = min(dp[c+w], choice);
+                w++;
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
