970e519dabb0c0040a86d2145f7ccc8f01dfe2a6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-08 18:13:50 +0800

    cross product

--- 1232. Check If It Is a Straight Line.cpp
+++ 1232. Check If It Is a Straight Line.cpp
@@ -19,3 +19,28 @@ public:
         return true;
     }
 };
+
+//cross prodcut
+//Runtime: 20 ms, faster than 14.27% of C++ online submissions for Check If It Is a Straight Line.
+//Memory Usage: 10.2 MB, less than 100.00% of C++ online submissions for Check If It Is a Straight Line.
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates) {
+        int n = coordinates.size();
+        
+        if(n == 2) return true;
+        
+        //the vector from coordinates[0] to coordinates[1]
+        vector<int> v1 = {coordinates[1][0] - coordinates[0][0], coordinates[1][1] - coordinates[0][1]};
+        vector<int> v2;
+        
+        for(int i = 2; i < n; i++){
+            v2 = {coordinates[i][0] - coordinates[0][0], coordinates[i][1] - coordinates[0][1]};
+            //cross product
+            //this is equivalent to comparing their slope: v2[1]/v2[0] == v1[1]/v1[0]
+            if(v1[0]*v2[1] - v1[1]*v2[0] != 0) return false;
+        }
+        
+        return true;
+    }
+};
