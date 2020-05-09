5fd8f6cc6b2ba64002133e206bf7cd737a64f4af
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-09 12:19:07 +0800

    cleaner sol

--- 228. Summary Ranges.cpp
+++ 228. Summary Ranges.cpp
@@ -31,3 +31,30 @@ public:
         return ans;
     }
 };
+
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Summary Ranges.
+//Memory Usage: 6.8 MB, less than 100.00% of C++ online submissions for Summary Ranges.
+//https://leetcode.com/problems/summary-ranges/discuss/63219/Accepted-JAVA-solution-easy-to-understand
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return vector<string>();
+        int start;
+        vector<string> ans;
+        for(int i = 0; i < n; i++){
+            start = nums[i];
+            while(i+1 < n && nums[i] != INT_MAX && nums[i+1] == nums[i]+1){
+                i++;
+            }
+            //now i is the index of the end of the interval
+            
+            if(start == nums[i])
+                ans.push_back(to_string(start));
+            else
+                ans.push_back(to_string(start) + "->" + to_string(nums[i]));
+        }
+        
+        return ans;
+    }
+};
