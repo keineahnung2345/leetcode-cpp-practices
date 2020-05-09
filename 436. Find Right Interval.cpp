f5db85c86932b0d8ece7c91a58877a5ed5685bca
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-09 12:39:41 +0800

    map::lower_bound

--- 436. Find Right Interval.cpp
+++ 436. Find Right Interval.cpp
@@ -1,3 +1,4 @@
+//lower_bound
 //Runtime: 236 ms, faster than 19.59% of C++ online submissions for Find Right Interval.
 //Memory Usage: 29.9 MB, less than 20.00% of C++ online submissions for Find Right Interval.
 class Solution {
@@ -29,6 +30,34 @@ public:
                 ans[i] = -1;
             }else{
                 ans[i] = (*it)[1];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//map::lower_bound
+//https://leetcode.com/problems/find-right-interval/discuss/91819/C%2B%2B-map-solution
+//Runtime: 160 ms, faster than 40.67% of C++ online submissions for Find Right Interval.
+//Memory Usage: 27.8 MB, less than 20.00% of C++ online submissions for Find Right Interval.
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        map<int, int> si; //(start point, index)
+        for(int i = 0; i < intervals.size(); i++){
+            si[intervals[i][0]] = i;
+        }
+        
+        vector<int> ans(n);
+        
+        for(int i = 0; i < n; i++){
+            auto it = si.lower_bound(intervals[i][1]);
+            if(it == si.end()){
+                ans[i] = -1;
+            }else{
+                ans[i] = it->second;
             }
         }
         
