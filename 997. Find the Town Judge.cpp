3dbc86da9859db8360888892d27c39db4c6d2f24
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-10 16:34:53 +0800

    worse sol

--- 997. Find the Town Judge.cpp
+++ 997. Find the Town Judge.cpp
@@ -43,6 +43,7 @@ trust[i][0] != trust[i][1]
 1 <= trust[i][0], trust[i][1] <= N
 **/
 
+//better
 //Runtime: 196 ms, faster than 49.32% of C++ online submissions for Find the Town Judge.
 //Memory Usage: 50.5 MB, less than 100.00% of C++ online submissions for Find the Town Judge.
 
@@ -69,5 +70,38 @@ public:
         }
         
         return -1;
+    }
+};
+
+//Runtime: 356 ms, faster than 23.27% of C++ online submissions for Find the Town Judge.
+//Memory Usage: 60.9 MB, less than 12.50% of C++ online submissions for Find the Town Judge.
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        if(trust.size() == 0) return (N == 1) ? 1 : -1;
+        //index 0 for padding
+        vector<int> indegrees(N+1, 0);
+        vector<int> outdegrees(N+1, 0);
+        //assume judge candidate not work!
+        // int judge = trust[0][1]; //candidate judge
+        
+        for(vector<int>& v : trust){
+            //judge doesn't trust anyone
+            // if(v[0] == judge) return -1;
+            outdegrees[v[0]]++;
+            indegrees[v[1]]++;
+        }
+        
+        set<int> cands;
+        
+        for(int i = 1; i <= N; i++){
+            //there is a person trusted by N-1 person and not judge
+            if(indegrees[i] == N-1 && outdegrees[i] == 0){
+                cands.insert(i);
+                if(cands.size() > 1) return -1;
+            }
+        }
+        
+        return cands.empty() ? -1 : *cands.begin();
     }
 };
