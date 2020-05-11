9bbb4675951a3c8a89f573b8fa01c480f835c88e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-11 19:19:22 +0800

    Binary Search and Two Pointers

--- 658. Find K Closest Elements.cpp
+++ 658. Find K Closest Elements.cpp
@@ -62,6 +62,51 @@ public:
     }
 };
 
+//Approach 2: Binary Search and Two Pointers
+//similar as above, cleaner
+//Runtime: 116 ms, faster than 38.21% of C++ online submissions for Find K Closest Elements.
+//Memory Usage: 31.2 MB, less than 16.67% of C++ online submissions for Find K Closest Elements.
+//time: O(logN+K), space: O(K)
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        //find element >= x
+        auto it = lower_bound(arr.begin(), arr.end(), x);
+        
+        if(it == arr.begin()){
+            return vector<int>(arr.begin(), arr.begin()+k);
+        }else if(it == arr.end()){
+            return vector<int>(arr.end() - k, arr.end());
+        }
+        
+        int n = arr.size();
+        int idx = it - arr.begin();
+        int left, right;
+        /*
+        search range suggested by solution is [idx-(k-1), idx+(k-1)],
+        but idx will not necessary be included in final subarray, 
+        so here expand the search range
+        */
+        left = max(0, idx - k); //max(0, idx - (k-1));
+        right = min(n-1, idx + k); //min(n-1, idx + (k-1));
+        //[left, right]: both inclusive
+        
+        // cout << left << ", " << right << " ";
+        
+        while(right - left + 1 > k){
+            if(x - arr[left] <= arr[right] - x){
+                right--;
+            }else if(arr[right] - x < x - arr[left]){
+                left++;
+            }
+            // cout << left << ", " << right << " ";
+        }
+        
+        return vector<int>(arr.begin()+left , arr.begin()+right+1);
+    }
+};
+
+
 //Approach 1: sort
 //Runtime: 284 ms, faster than 7.52% of C++ online submissions for Find K Closest Elements.
 //Memory Usage: 31.2 MB, less than 16.67% of C++ online submissions for Find K Closest Elements.
