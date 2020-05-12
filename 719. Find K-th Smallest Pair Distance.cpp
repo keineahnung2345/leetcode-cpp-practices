be592454f7cf586b48e2944b93860ed3c5ff9d10
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-12 22:50:25 +0800

    Heap [Time Limit Exceeded]

--- 719. Find K-th Smallest Pair Distance.cpp
+++ 719. Find K-th Smallest Pair Distance.cpp
@@ -56,3 +56,43 @@ public:
         return pq.top();
     }
 };
+
+//Approach #1: Heap [Time Limit Exceeded]
+//TLE
+//16 / 19 test cases passed.
+//time: push: O(NlogN) + pop: O(klogN) -> O((N+k)logN) 
+//space: O(N)
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        
+        auto comp = [&nums](const vector<int>& a, const vector<int>& b){
+            //use > because we want the smaller to be popped earlier
+            return nums[a[1]] - nums[a[0]] > nums[b[1]] - nums[b[0]];
+            };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
+        
+        int n = nums.size();
+        for(int i = 0; i+1 < n; i++){
+            pq.push(vector<int>{i, i+1});
+            // cout << i << ", " << i+1 << endl;
+        }
+        
+        vector<int> cur;
+        k--;
+        //pop first k-1 pairs
+        while(k-- > 0){
+            cur = pq.top(); pq.pop();
+            // cout << cur[0] << ", " << cur[1] << endl;
+            if(cur[1]+1 < n){
+                pq.push({cur[0], cur[1]+1});
+            }
+        }
+        //the kth pair
+        cur = pq.top();
+        // cout << "final: " << cur[0] << ", " << cur[1] << endl;
+        
+        return nums[cur[1]] - nums[cur[0]];
+    }
+};
