81e95fa32b96411693d4886da6e552490864c9e6
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-13 23:49:58 +0800

    binary search

--- 786. K-th Smallest Prime Fraction.cpp
+++ 786. K-th Smallest Prime Fraction.cpp
@@ -35,6 +35,8 @@ public:
 //https://leetcode.com/problems/k-th-smallest-prime-fraction/discuss/115819/Summary-of-solutions-for-problems-%22reducible%22-to-LeetCode-378
 //TLE
 //53 / 62 test cases passed.
+//time: O((n + K) * logn), with n = A.length
+//space: O(n)
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
@@ -80,5 +82,56 @@ public:
         
         //second coord is the index of reversed A!
         return {A[cur[0]], A[A.size()-1-cur[1]]};
+    }
+};
+
+//binary search, can only be used when there is no duplicate elements
+//https://leetcode.com/problems/k-th-smallest-prime-fraction/discuss/115819/Summary-of-solutions-for-problems-%22reducible%22-to-LeetCode-378
+//Runtime: 16 ms, faster than 77.92% of C++ online submissions for K-th Smallest Prime Fraction.
+//Memory Usage: 9.9 MB, less than 100.00% of C++ online submissions for K-th Smallest Prime Fraction.
+//time: O(n * log(MAX^2)), where MAX is the maximum element in A //?
+//space: O(1)
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+        //the range of prime fraction
+        double left = 0, right = 1;
+        double mid;
+        //numerator and denominator of candidate answer
+        int numerator = 0, denominator = 1;
+        int n = A.size();
+        
+        while(left < right){
+            mid = (left + right)/2.0;
+            
+            //count of fractions <= mid
+            int count = 0;
+            numerator = 0; //make sure "A[n-1-j] * numerator" will be smaller than any number initially!
+            for(int i = 0, j = n -1; j >= 0 && i< n; i++){
+                // cout << i << ", " << j << ", " << (double)A[i]/A[n-1-j] << endl;
+                // while(j >= 0 && (double)A[i]/A[n-1-j] > mid) j--;
+                while(j >= 0 && A[i] > mid*A[n-1-j]) j--;
+                //now A[i]/A[j] <= mid
+                count += (j+1);
+                //if((double)A[i]/A[j] > (double)numerator/denominator){
+                if(j >= 0 && A[i]*denominator > A[n-1-j] * numerator){
+                    numerator = A[i];
+                    denominator = A[n-1-j];
+                }
+                // cout << numerator << "/" << denominator << endl;
+            }
+            
+            // cout << "count: " << count << endl;
+            
+            if(count == K){
+                return {numerator, denominator};
+            }else if(count < K){
+                left = mid;
+            }else if(count > K){
+                right = mid;
+            }
+        }
+        
+        return {0, 0};
     }
 };
