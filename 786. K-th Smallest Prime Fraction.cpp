cf72057fa357179b829ba2e3954aeb125acfb8ec
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-13 23:03:41 +0800

    heap, mapped to "Kth Smallest Element in a Sorted Matrix"

--- 786. K-th Smallest Prime Fraction.cpp
+++ 786. K-th Smallest Prime Fraction.cpp
@@ -30,3 +30,55 @@ public:
         return {A[(int)pq.top()[1]], A[(int)pq.top()[2]]};
     }
 };
+
+//heap, mapped to "Kth Smallest Element in a Sorted Matrix"
+//https://leetcode.com/problems/k-th-smallest-prime-fraction/discuss/115819/Summary-of-solutions-for-problems-%22reducible%22-to-LeetCode-378
+//TLE
+//53 / 62 test cases passed.
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& A, int K) {
+        auto comp = [&A](vector<int>& a, vector<int>& b){
+            //first coord represents for nominator, accessing A
+            //second coord represents for denominator, accessing reversed A
+            
+            // return A[a[0]]/A[A.size()-1-a[1]] > A[b[0]]/A[A.size()-1-b[1]];
+            return A[a[0]]*A[A.size()-1-b[1]] > A[b[0]]*A[A.size()-1-a[1]];
+        };
+        
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> pq(comp);
+        int n = A.size();
+        
+        for(int i = 0; i < n; i++){
+            /*
+            For A = {1,2,3,5}, we build a matrix:
+               5   3   2   1
+            1 1/5 1/3 1/2 1/1
+            2 2/5 2/3 2/2 2/1
+            3 3/5 3/3 3/2 3/1
+            5 5/5 5/3 5/2 5/1
+            
+            Which uses rows to represent nominator,
+            cols(in reverse order) to represent denominator,
+            so that every row is sorted, ascending
+            also every col is sorted, ascending
+            
+            (coord for nominator, coord for denominator)
+            */
+            pq.push({i, 0});
+        }
+        
+        vector<int> cur;
+        while(K-- > 0){
+            cur = pq.top(); pq.pop();
+            // cout << "pop: " << cur[0] << ", " << cur[1] << endl;
+            if(cur[1]+1 < n){
+                // cout << "push: " << cur[0] << ", " << cur[1]+1 << endl;
+                pq.push({cur[0], cur[1]+1});
+            }
+        }
+        
+        //second coord is the index of reversed A!
+        return {A[cur[0]], A[A.size()-1-cur[1]]};
+    }
+};
