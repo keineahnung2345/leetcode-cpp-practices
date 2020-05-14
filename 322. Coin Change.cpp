d528f0cf56cb1a8819d22530c76d32e643673a48
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-14 22:06:03 +0800

    DP

--- 322. Coin Change.cpp
+++ 322. Coin Change.cpp
@@ -36,6 +36,41 @@ public:
     }
 };
 
+//DP
+//Runtime: 124 ms, faster than 29.29% of C++ online submissions for Coin Change.
+//Memory Usage: 14.1 MB, less than 27.45% of C++ online submissions for Coin Change.
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
+        //padding ahead
+        vector<int> count(amount+1, INT_MAX);
+        
+        //base case
+        for(int coin : coins){
+            //to ensure it won't exceed the array's size
+            if(coin <= amount) count[coin] = 1;
+        }
+        
+        // for(int i = 1; i <= amount; i++){
+        //     cout << i << ", " << count[i] << " | ";
+        // }
+        // cout << endl;
+        
+        for(int i = 1; i <= amount; i++){
+            for(int coin : coins){
+                if(i < coin) continue;
+                if(count[i-coin] == INT_MAX) continue;
+                count[i] = min(count[i], count[i-coin]+1);
+            }
+            // cout << i << ", " << count[i] << endl;
+        }
+        // cout << "===========" << endl;
+        
+        return count[amount] == INT_MAX ? -1 : count[amount];
+    }
+};
+
 //recursion
 //Runtime: 1128 ms, faster than 5.02% of C++ online submissions for Coin Change.
 //Memory Usage: 66.5 MB, less than 5.88% of C++ online submissions for Coin Change.
