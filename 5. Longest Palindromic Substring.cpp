6c0e824d67d71053629c5998b836e63a0e2ff484
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-16 09:48:12 +0800

    O(n) space

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -70,3 +70,33 @@ public:
         return ans;
     }
 };
+
+//same as above, space complexity O(n)
+//Runtime: 652 ms, faster than 10.41% of C++ online submissions for Longest Palindromic Substring.
+//Memory Usage: 14.5 MB, less than 40.00% of C++ online submissions for Longest Palindromic Substring.
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        string rs(s.rbegin(), s.rend());
+        string ans = "";
+        
+        vector<vector<int>> dp(2, vector(n+1, 0));
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i-1] == rs[j-1]){
+                    dp[i%2][j] = dp[(i+1)%2][j-1] + 1;
+                }else{
+                    dp[i%2][j] = 0;
+                }
+                
+                if(dp[i%2][j] > ans.size() && i+j == dp[i%2][j]+n){
+                    ans = s.substr(i-dp[i%2][j], dp[i%2][j]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
