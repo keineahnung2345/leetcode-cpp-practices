39b37c00481b71d6a680494f85f008f3682f8658
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-16 09:51:25 +0800

    DP, O(n) space

--- 5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring.cpp
@@ -1,6 +1,8 @@
+//Approach 3: Dynamic Programming
 //https://ithelp.ithome.com.tw/articles/10215365
 //Runtime: 924 ms, faster than 6.44% of C++ online submissions for Longest Palindromic Substring.
 //Memory Usage: 24.3 MB, less than 21.38% of C++ online submissions for Longest Palindromic Substring.
+//time: O(N^2), space: O(N^2)
 class Solution {
 public:
     string longestPalindrome(string s) {
@@ -21,6 +23,38 @@ public:
                 }
                 
                 if(dp[i][j] && j-i+1 > ans.size()){
+                    ans = s.substr(i, j-i+1);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//same as above, space complexity O(n)
+//Runtime: 904 ms, faster than 7.04% of C++ online submissions for Longest Palindromic Substring.
+//Memory Usage: 14.6 MB, less than 40.00% of C++ online submissions for Longest Palindromic Substring.
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(2, vector(n, false));
+        string ans;
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                //susbstring: s[i...j]
+                //length 1
+                if(i == j){
+                    dp[i%2][j] = true;
+                }else if(i+1 == j){
+                    dp[i%2][j] = (s[i] == s[j]);
+                }else{
+                    dp[i%2][j] = (s[i] == s[j]) && dp[(i+1)%2][j-1];
+                }
+                
+                if(dp[i%2][j] && j-i+1 > ans.size()){
                     ans = s.substr(i, j-i+1);
                 }
             }
