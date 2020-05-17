51c3656ac70c7aa98413dd7d53bbda8486dec100
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-17 18:06:07 +0800

    use built-in __gcd(), change the order of loop

--- 1447. Simplified Fractions.cpp
+++ 1447. Simplified Fractions.cpp
@@ -30,3 +30,27 @@ public:
         return ans;
     }
 };
+
+//use built-in __gcd(), change the order of loop
+//Runtime: 108 ms, faster than 66.67% of C++ online submissions for Simplified Fractions.
+//Memory Usage: 21.8 MB, less than 100.00% of C++ online submissions for Simplified Fractions.
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        if(n == 1) return vector<string>();
+        
+        vector<string> ans;
+        
+        for(int denom = 2; denom <= n; denom++){
+            for(int nom = 1; nom < denom; nom++){
+                // cout << nom << "/" << denom <<": " << gcd(denom, nom) << endl;
+                if(__gcd(denom, nom) != 1){
+                    continue;
+                }
+                ans.push_back(to_string(nom) + "/" + to_string(denom));
+            }
+        }
+        
+        return ans;
+    }
+};
