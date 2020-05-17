ee1987e13a26108738bb6554567a3dd7b2402e17
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-17 18:42:33 +0800

    1450. Number of Students Doing Homework at a Given Time.cpp

---
+++ 1450. Number of Students Doing Homework at a Given Time.cpp
@@ -0,0 +1,17 @@
+//Runtime: 8 ms, faster than 20.00% of C++ online submissions for Number of Students Doing Homework at a Given Time.
+//Memory Usage: 11 MB, less than 100.00% of C++ online submissions for Number of Students Doing Homework at a Given Time.
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        int n = startTime.size();
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(startTime[i] <= queryTime && queryTime <= endTime[i]){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
