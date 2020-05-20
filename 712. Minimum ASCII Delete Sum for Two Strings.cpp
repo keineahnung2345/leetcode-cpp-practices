919142286d3575b66bc268ee1306094763ddc1cf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-20 19:18:33 +0800

    DP

--- 712. Minimum ASCII Delete Sum for Two Strings.cpp
+++ 712. Minimum ASCII Delete Sum for Two Strings.cpp
@@ -41,3 +41,37 @@ public:
         return dp[0][0];
     }
 };
+
+//DP
+//Runtime: 44 ms, faster than 67.45% of C++ online submissions for Minimum ASCII Delete Sum for Two Strings.
+//Memory Usage: 15.1 MB, less than 77.78% of C++ online submissions for Minimum ASCII Delete Sum for Two Strings.
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+        
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 0;
+                }else if(j == 0){
+                    dp[i][j] = dp[i-1][j] + s1[i-1];
+                }else if(i == 0){
+                    dp[i][j] = dp[i][j-1] + s2[j-1];
+                }else if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = min(
+                        dp[i-1][j] + s1[i-1],
+                        dp[i][j-1] + s2[j-1]
+                    );
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
