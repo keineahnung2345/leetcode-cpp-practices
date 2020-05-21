38523552577a11cf91e70e6f1ec9a6273840d0dc
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-21 23:18:12 +0800

    LCS, DP, O(N) space

--- 1035. Uncrossed Lines.cpp
+++ 1035. Uncrossed Lines.cpp
@@ -1,6 +1,7 @@
-//LCS
+//LCS, DP, O(N^2) space
 //Runtime: 40 ms, faster than 14.05% of C++ online submissions for Uncrossed Lines.
 //Memory Usage: 13.2 MB, less than 9.09% of C++ online submissions for Uncrossed Lines.
+//time: O(N^2), space: O(N^2)
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& A, vector<int>& B) {
@@ -20,5 +21,35 @@ public:
         }
         
         return dp[m][n];
+    }
+};
+
+//LCS, DP, O(N) space
+//Runtime: 12 ms, faster than 78.56% of C++ online submissions for Uncrossed Lines.
+//Memory Usage: 9.8 MB, less than 77.27% of C++ online submissions for Uncrossed Lines.
+//time: O(N^2), space: O(N)
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int m = A.size();
+        int n = B.size();
+        
+        vector<int> dp(n+1, 0);
+        
+        for(int i = 1; i <= m; i++){
+            int dp_is1_js1 = dp[0]; //will be used when j = 1
+            // cout << dp[0] << " "; //0
+            for(int j = 1; j <= n; j++){
+                int dp_is1_j = dp[j];
+                if(A[i-1] == B[j-1]){
+                    dp[j] = dp_is1_js1 + 1;
+                }else{
+                    dp[j] = max(dp[j-1], dp[j]);
+                }
+                dp_is1_js1 = dp_is1_j;
+            }
+        }
+        
+        return dp[n];
     }
 };
