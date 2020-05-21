dc52b25da6dcf5f712fb091509721c286ff2e533
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-21 23:00:19 +0800

    DP LCS

--- 583. Delete Operation for Two Strings.cpp
+++ 583. Delete Operation for Two Strings.cpp
@@ -1,3 +1,4 @@
+//DP, O(N^2) space
 //Runtime: 28 ms, faster than 53.48% of C++ online submissions for Delete Operation for Two Strings.
 //Memory Usage: 12 MB, less than 55.56% of C++ online submissions for Delete Operation for Two Strings.
 class Solution {
@@ -62,5 +63,37 @@ public:
         }
         
         return dp[n];
+    }
+};
+
+//Approach #3 Using Longest Common Subsequence- Dynamic Programming [Accepted]
+//https://leetcode.com/articles/delete-operation-for-two-strings/
+//time: O(m*n), space: O(m*n)
+class Solution {
+public:
+    int LCS(string word1, string word2){
+        int m = word1.size();
+        int n = word2.size();
+        
+        vector<vector<int>> dp(m+1, vector(n+1, 0));
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    };
+    
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        
+        return m + n - 2 * LCS(word1, word2);
     }
 };
