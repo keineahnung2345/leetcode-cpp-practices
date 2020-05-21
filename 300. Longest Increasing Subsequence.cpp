c494475697b786b88cadede2a4f44bbe21c0416e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-21 22:12:02 +0800

    DP + binary search

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -25,3 +25,44 @@ public:
         return ans;
     }
 };
+
+//DP + binary search
+//Runtime: 4 ms, faster than 89.41% of C++ online submissions for Longest Increasing Subsequence.
+//Memory Usage: 7.9 MB, less than 100.00% of C++ online submissions for Longest Increasing Subsequence.
+//https://algorithmsandme.com/longest-increasing-subsequence-in-onlogn/
+//https://github.com/keineahnung2345/fucking-algorithm/blob/note/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E8%AE%BE%E8%AE%A1%EF%BC%9A%E6%9C%80%E9%95%BF%E9%80%92%E5%A2%9E%E5%AD%90%E5%BA%8F%E5%88%97.md
+//time: O(NlogN), space: O(N)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        
+        vector<int> top;
+        
+        for(int poker : nums){
+            int left = 0, right = top.size()-1;
+            while(left <= right){
+                int mid = left + (right-left)/2;
+                if(poker < top[mid]){
+                    right = mid - 1;
+                }else if(poker == top[mid]){
+                    right = mid - 1;
+                }else if(poker > top[mid]){
+                    left = mid+1;
+                }
+            }
+            
+            if(left == top.size()){
+                //poker is larger than all cards in top
+                top.push_back(poker);
+            }else{
+                //overlay current poker to the specific pile
+                top[left] = poker;
+            }
+            
+        }
+        
+        return top.size();
+    }
+};
