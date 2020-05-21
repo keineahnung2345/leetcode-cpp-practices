310bb83465b10df0d196f111892a53d9b6bab4ed
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-21 22:30:57 +0800

    Approach 1: Brute Force, Recursion

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -66,3 +66,32 @@ public:
         return top.size();
     }
 };
+
+//Approach 1: Brute Force, Recursion
+//TLE
+//21 / 24 test cases passed.
+//time: O(2^N), because for each element, there are 2 cases: taken or nottaken
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums, int prev = -1, int cur = 0) {
+        /*
+        nums[cur] is the element we consider to append,
+        it cannot be out of the range of nums
+        */
+        if(cur == nums.size()) return 0;
+        int taken = 0;
+        //we may append current element into sequence
+        /*
+        initial state: prev is -1, and cur = 0,
+        that means we are considering the 0th element,
+        and the 0th element is always ok to be appended to the empty sequence
+        */
+        if(prev < 0 || nums[cur] > nums[prev]){
+            taken = 1 + lengthOfLIS(nums, cur, cur+1);
+        }
+        //skip current element
+        int nottaken = lengthOfLIS(nums, prev, cur+1);
+        // cout << prev << ", " << cur << " : " << taken << ", " << nottaken << endl;
+        return max(taken, nottaken);
+    }
+};
