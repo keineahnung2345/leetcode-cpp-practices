6901af6a97fb3e3495f391c42a6449e536a6e733
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-23 21:35:21 +0800

    DP

--- 1278. Palindrome Partitioning III.cpp
+++ 1278. Palindrome Partitioning III.cpp
@@ -1,3 +1,4 @@
+//recursion(dfs)
 //Runtime: 16 ms, faster than 72.66% of C++ online submissions for Palindrome Partitioning III.
 //Memory Usage: 6.6 MB, less than 100.00% of C++ online submissions for Palindrome Partitioning III.
 //https://leetcode.com/problems/palindrome-partitioning-iii/discuss/441427/Python3-Top-down-DFS-with-Memoization
@@ -70,5 +71,51 @@ public:
         memo = vector<vector<int>>(n, vector(k+1, INT_MAX));
         
         return dfs(s, 0, k);
+    }
+};
+
+//DP
+//https://leetcode.com/problems/palindrome-partitioning-iii/discuss/442083/Simple-C%2B%2B-Dp-O(N2K)-Beats-100-with-Explanation
+//Runtime: 20 ms, faster than 63.10% of C++ online submissions for Palindrome Partitioning III.
+//Memory Usage: 6.9 MB, less than 100.00% of C++ online submissions for Palindrome Partitioning III.
+class Solution {
+public:
+    int palindromePartition(string s, int K) {
+        int n = s.size();
+        //cost of s[i...j]
+        vector<vector<int>> cost(n, vector(n, 0));
+        //(#partitions, end index)
+        vector<vector<int>> dp(K+1, vector(n, n));
+        
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                //cost of s[i...j]
+                if(j == i){
+                    cost[i][j] = 0;
+                }else if(j == i+1){
+                    cost[i][j] = (s[i] != s[j]);
+                }else if(s[i] == s[j]){
+                    cost[i][j] = cost[i+1][j-1];
+                }else{
+                    cost[i][j] = cost[i+1][j-1]+1;
+                }
+            }
+        }
+        
+        for(int k = 1; k <= K; k++){
+            for(int j = 0; j < n; j++){
+                if(k == 1){
+                    //don't partition s[0...j]
+                    dp[k][j] = cost[0][j];
+                }else{
+                    for(int i = 0; i < j; i++){
+                        //partition to [0...i] and [i+1...j]
+                        dp[k][j] = min(dp[k][j], dp[k-1][i]+cost[i+1][j]);
+                    }
+                }
+            }
+        }
+        
+        return dp[K][n-1];
     }
 };
