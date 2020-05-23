7519cd1224e6ad58384bf1fe93e5d0d98122c7e2
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-23 17:29:12 +0800

    segment tree

--- 673. Number of Longest Increasing Subsequence.cpp
+++ 673. Number of Longest Increasing Subsequence.cpp
@@ -52,3 +52,129 @@ public:
         return ans;
     }
 };
+
+//
+class NodeValue{
+public:
+    int length;
+    int count;
+    NodeValue(int l, int c) : length(l), count(c) {}
+};
+
+class Node{
+public:
+    int range_left, range_right;
+    Node *left, *right;
+    NodeValue* val;
+    
+    Node(int rl, int rr){
+        range_left = rl;
+        range_right = rr;
+        left = nullptr;
+        right = nullptr;
+        //length 0, count 1
+        val = new NodeValue(0, 1);
+    }
+    
+    int getRangeMid(){
+        return range_left + (range_right - range_left)/2;
+    }
+    
+    Node* getLeft(){
+        if(left == nullptr) left = new Node(range_left, getRangeMid());
+        return left;
+    }
+    
+    Node* getRight(){
+        if(right == nullptr) right = new Node(getRangeMid()+1, range_right);
+        return right;
+    }
+};
+
+//Approach 2: Segment Tree
+//https://leetcode.com/articles/number-of-longest-increasing-subsequence/
+//Runtime: 72 ms, faster than 52.97% of C++ online submissions for Number of Longest Increasing Subsequence.
+//Memory Usage: 60.9 MB, less than 11.11% of C++ online submissions for Number of Longest Increasing Subsequence.
+//time: O(NlogN), space: O(N)
+//segment tree
+class Tree{
+private:
+    NodeValue* merge(NodeValue* v1, NodeValue* v2){
+        if(v1->length == v2->length){
+            //we cannot accumulate the count of length 0 sequence
+            if(v1->length == 0) return new NodeValue(0, 1);
+            return new NodeValue(v1->length, v1->count + v2->count);
+        }
+        return (v1->length > v2->length) ? v1 : v2;
+    }
+    
+    void insert(Node* node, int key, NodeValue* value){
+        if(node->range_left == node->range_right){
+            //leaf node
+            node->val = merge(node->val, value);
+            return;
+        }
+        
+        if(key <= node->getRangeMid()){
+            //the queried key falls in left subtree
+            insert(node->getLeft(), key, value);
+        }else{
+            //the queried key falls in right subtree
+            insert(node->getRight(), key, value);
+        }
+        
+        node->val = merge(node->getLeft()->val, node->getRight()->val);
+    }
+    
+    NodeValue* query(Node* node, int key){
+        if(key >= node->range_right){
+            /*
+            we are querying [minv, key],
+            and current node's range is completely included in the query range
+            */
+            return node->val;
+        }else if(key < node->range_left){
+            //query range is outside the node's range
+            return new NodeValue(0, 1);
+        }
+        
+        return merge(query(node->getLeft(), key), query(node->getRight(), key));
+    }
+
+public:
+    Node* root;
+    
+    Tree(int rl, int rr){
+        root = new Node(rl, rr);
+    };
+    
+    //used outside
+    void insert(int key, NodeValue* value){
+        insert(root, key, value);
+    };
+    
+    NodeValue* query(int key){
+        return query(root, key);
+    };
+};
+
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int minv = INT_MAX, maxv = INT_MIN;
+        
+        for(int num : nums){
+            minv = min(minv, num);
+            maxv = max(maxv, num);
+        }
+        
+        Tree* tree = new Tree(minv, maxv);
+        for(int num : nums){
+            NodeValue* v = tree->query(num-1);
+            tree->insert(num, new NodeValue(v->length+1, v->count));
+        }
+        
+        return tree->root->val->count;
+    }
+};
