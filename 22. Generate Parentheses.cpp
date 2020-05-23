7595e69f26ac0537bdf11a68a2e5c579465c15a0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-23 12:13:59 +0800

    backtrack

--- 22. Generate Parentheses.cpp
+++ 22. Generate Parentheses.cpp
@@ -36,7 +36,7 @@ public:
 //Approach 1: Brute Force, recursion
 //Runtime: 20 ms, faster than 11.14% of C++ online submissions for Generate Parentheses.
 //Memory Usage: 6.9 MB, less than 100.00% of C++ online submissions for Generate Parentheses.
-//time: O(2^n*n), space: O(2^n*n)
+//time: O(2^(2n)*n), space: O(2^(2n)*n)
 class Solution {
 public:
     bool valid(string& comb){
@@ -74,6 +74,12 @@ public:
 //Approach 1: Brute Force, recursion
 //Runtime: 16 ms, faster than 15.92% of C++ online submissions for Generate Parentheses.
 //Memory Usage: 12.4 MB, less than 92.56% of C++ online submissions for Generate Parentheses.
+/*
+time: O(2^(2n)*n), space: O(2^(2n)*n)
+we may choose either ( or ) for a comb of size 2*n,
+so there are total 2^(2n) possibilities.
+And for each possibility, we need to spend O(n) time to check for its validity.
+*/
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
@@ -92,5 +98,38 @@ public:
         }
         
         return ans;
+    }
+};
+
+//backtrack
+//Runtime: 4 ms, faster than 89.84% of C++ online submissions for Generate Parentheses.
+//Memory Usage: 11.6 MB, less than 92.56% of C++ online submissions for Generate Parentheses.
+//time: O(4^n/sqrt(n)), space: O(4^n/sqrt(n))
+class Solution {
+public:
+    void backtrack(string& comb, vector<string>& combs, int open, int close, int n){
+        if(comb.size() == n*2){
+            combs.push_back(comb);
+            return;
+        }
+        
+        if(open < n){
+            comb += '(';
+            backtrack(comb, combs, open+1, close, n);
+            comb.pop_back();
+        }
+        
+        if(close < open){
+            comb += ')';
+            backtrack(comb, combs, open, close+1, n);
+            comb.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string comb = "";
+        vector<string> combs;
+        backtrack(comb, combs, 0, 0, n);
+        return combs;
     }
 };
