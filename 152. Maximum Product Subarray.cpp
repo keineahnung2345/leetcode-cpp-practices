dde39d42c2d8fc1be98e2048a5e9d94dfba40f6b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-24 10:15:34 +0800

    O(n) DP

--- 152. Maximum Product Subarray.cpp
+++ 152. Maximum Product Subarray.cpp
@@ -24,3 +24,39 @@ public:
         return ans;
     }
 };
+
+//O(n) DP
+//https://leetcode.com/problems/maximum-product-subarray/discuss/48230/Possibly-simplest-solution-with-O(n)-time-complexity
+//Runtime: 8 ms, faster than 40.36% of C++ online submissions for Maximum Product Subarray.
+//Memory Usage: 11.8 MB, less than 5.00% of C++ online submissions for Maximum Product Subarray.
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int ans = nums[0];
+        int curmax = nums[0], curmin = nums[0];
+        
+        for(int i = 1; i < n; i++){
+            /*
+            multiplied by a negative makes big number smaller, 
+            small number bigger
+            so we redefine the extremums by swapping them
+            */
+            if(nums[i] < 0){
+                swap(curmax, curmin);
+            }
+            
+            /*
+            note the first item of max() is nums[i], not curmax!!
+            it either extends the previous found subarray or
+            create a new subarray
+            */
+            curmax = max(nums[i], curmax*nums[i]);
+            curmin = min(nums[i], curmin*nums[i]);
+            
+            ans = max(curmax, ans);
+        }
+        
+        return ans;
+    }
+};
