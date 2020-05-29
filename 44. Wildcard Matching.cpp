281613fe4062453e0c2d2486eefc5a6cc93841ee
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-29 22:36:51 +0800

    greedy

--- 44. Wildcard Matching.cpp
+++ 44. Wildcard Matching.cpp
@@ -86,3 +86,45 @@ public:
         return dp[0][0];
     }
 };
+
+//greedy
+//https://leetcode.com/problems/wildcard-matching/discuss/17810/Linear-runtime-and-constant-space-solution
+//Runtime: 8 ms, faster than 95.12% of C++ online submissions for Wildcard Matching.
+//Memory Usage: 6.6 MB, less than 100.00% of C++ online submissions for Wildcard Matching.
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0;
+        int m = s.size(), n = p.size();
+        int last_match = -1, starj = -1;
+        
+        while(i < m){
+            // cout << s[i] << ", " << ((j < n) ? p[j] : ' ') << endl;
+            // cout << i << ", " << j << ", starj: " << starj << ", last_match: " << last_match << endl;
+            if(j < n && (p[j] == s[i] || p[j] == '?')){
+                //match one
+                i++;
+                j++;
+            }else if(j < n && p[j] == '*'){
+                //greedily match one
+                starj = j;
+                last_match = i;
+                j++;
+                //why not i++?
+            }else if(starj != -1){
+                //not match, fallback to use previous found '*'
+                last_match++; //the previous star now matches s[last_match+1]
+                i = last_match; //?
+                j = starj+1; //now start from the next char of previous '*'
+            }else{
+                //current char not match, and cannot fallback
+                // cout << endl;
+                return false;
+            }
+        }
+        // cout << endl;
+        
+        //if the remaining char in p are all '*', we think it's a match
+        return p.substr(j) == string(p.size()-j, '*');
+    }
+};
