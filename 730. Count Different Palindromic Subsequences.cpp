e1a234123bc7091e8802089d57a3a90c6ef874aa
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-05-30 12:47:20 +0800

    O(N^2) DP

--- 730. Count Different Palindromic Subsequences.cpp
+++ 730. Count Different Palindromic Subsequences.cpp
@@ -2,6 +2,7 @@
 //https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109507/Java-96ms-DP-Solution-with-Detailed-Explanation
 //Runtime: 164 ms, faster than 60.90% of C++ online submissions for Count Different Palindromic Subsequences.
 //Memory Usage: 36.4 MB, less than 100.00% of C++ online submissions for Count Different Palindromic Subsequences.
+//time: O(N^3), space: O(N^2)
 class Solution {
 public:
     int countPalindromicSubsequences(string S) {
@@ -121,6 +122,78 @@ public:
                 knowning the reason why negative number appears,
                 it's naturally to add the number MOD back!
                 */
+                dp[i][j] = (dp[i][j] < 0) ? dp[i][j] + MOD: dp[i][j] % MOD;
+            }
+        }
+        
+        return dp[0][n-1];
+    }
+};
+
+//DP, O(N^2)
+//optimize the previous method, finding lo and hi in O(1) time
+//https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109507/Java-96ms-DP-Solution-with-Detailed-Explanation/111365
+//Runtime: 120 ms, faster than 84.48% of C++ online submissions for Count Different Palindromic Subsequences.
+//Memory Usage: 36.7 MB, less than 100.00% of C++ online submissions for Count Different Palindromic Subsequences.
+//time: O(N^2), space: O(N^2)
+class Solution {
+public:
+    int countPalindromicSubsequences(string S) {
+        int n = S.size();
+        const int MOD = 1e9+7;
+        vector<vector<int>> dp(n, vector(n, 0));
+        
+        //record the next right/left position of the same char
+        vector<int> nextRight(n), nextLeft(n);
+        //helper
+        vector<int> char2pos(4, -1);
+        
+        for(int i = 0; i < n; i++){
+            /*
+            initial value of char2pos are all -1,
+            meaning that we haven't found S[i] in S[0...i-1]
+            */
+            nextLeft[i] = char2pos[S[i]-'a'];
+            /*
+            record the position of S[i], 
+            this will be used in following iterations
+            */
+            char2pos[S[i]-'a'] = i;
+        }
+        
+        fill(char2pos.begin(), char2pos.end(), n);
+        for(int i = n-1; i >= 0; i--){
+            nextRight[i] = char2pos[S[i]-'a'];
+            char2pos[S[i]-'a'] = i;
+        }
+        
+        for(int dist = 0; dist < n; dist++){
+            for(int i = 0; i + dist < n; i++){
+                int j = i+dist;
+                if(i == j){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if(S[i] != S[j]){
+                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1];
+                }else{
+                    //the position of S[i] in S[i+1...]
+                    int lo = nextRight[i];
+                    //the position of S[j] in S[...j-1]
+                    int hi = nextLeft[j];
+                    /*
+                    note that lo and hi could be out of the range of 
+                    S[i+1...j-1], but this can be handled by lo > hi
+                   */
+                    if(lo > hi){
+                        dp[i][j] = dp[i+1][j-1]*2+2;
+                    }else if(lo == hi){
+                        dp[i][j] = dp[i+1][j-1]*2+1;
+                    }else if(lo < hi){
+                        dp[i][j] = dp[i+1][j-1]*2 - dp[lo+1][hi-1];
+                    }
+                }
                 dp[i][j] = (dp[i][j] < 0) ? dp[i][j] + MOD: dp[i][j] % MOD;
             }
         }
