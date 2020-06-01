d56e7c5195050f3426a6b9f9f7f36f96a8696cf4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-01 18:12:05 +0800

    iterative, bfs

--- 226. Invert Binary Tree.cpp
+++ 226. Invert Binary Tree.cpp
@@ -22,3 +22,33 @@ public:
         return root;
     }
 };
+
+//iterative, bfs
+//Runtime: 4 ms, faster than 39.78% of C++ online submissions for Invert Binary Tree.
+//Memory Usage: 8.6 MB, less than 100.00% of C++ online submissions for Invert Binary Tree.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* cur = q.front(); q.pop();
+            if(cur == nullptr) continue;
+            swap(cur->left, cur->right);
+            q.push(cur->left);
+            q.push(cur->right);
+        }
+        return root;
+    }
+};
