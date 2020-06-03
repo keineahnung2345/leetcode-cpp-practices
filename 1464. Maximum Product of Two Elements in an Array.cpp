f2948831a31771eba166487123be1b4ecca6e1ef
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-03 23:12:08 +0800

    nth_element

--- 1464. Maximum Product of Two Elements in an Array.cpp
+++ 1464. Maximum Product of Two Elements in an Array.cpp
@@ -9,3 +9,16 @@ public:
         return (nums[0]-1) * (nums[1]-1);
     }
 };
+
+//nth_element
+//Runtime: 8 ms, faster than 96.88% of C++ online submissions for Maximum Product of Two Elements in an Array.
+//Memory Usage: 10.1 MB, less than 100.00% of C++ online submissions for Maximum Product of Two Elements in an Array.
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        nth_element(nums.begin(), nums.begin()+2, nums.end(),
+                   greater<int>());
+        
+        return (nums[0]-1) * (nums[1]-1);
+    }
+};
