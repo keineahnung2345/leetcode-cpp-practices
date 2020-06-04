0c778173d1a961bd8e99aaa8e53d49c5df78469f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-04 18:12:07 +0800

    one variable

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -11,3 +11,18 @@ public:
         }
     }
 };
+
+//one variable
+//Runtime: 56 ms, faster than 53.92% of C++ online submissions for Reverse String.
+//Memory Usage: 23.3 MB, less than 6.10% of C++ online submissions for Reverse String.
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0, n = s.size();
+        
+        while(i < n/2){
+            swap(s[i], s[n-1-i]);
+            i++;
+        }
+    }
+};
