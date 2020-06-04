c6a71399d789aadd16804c19dff09bbdf20068d9
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-04 20:47:17 +0800

    recursion + memorization

--- 494. Target Sum.cpp
+++ 494. Target Sum.cpp
@@ -18,3 +18,43 @@ public:
         return findTargetSumWays(nums, S, 0);
     }
 };
+
+//recursion + memorization
+//Runtime: 20 ms, faster than 75.18% of C++ online submissions for Target Sum.
+//Memory Usage: 38.2 MB, less than 23.08% of C++ online submissions for Target Sum.
+//time: O(sum*N), space: O(sum*N)
+class Solution {
+public:
+    vector<vector<int>> memo;
+    
+    int findTargetSumWays(vector<int>& nums, int S, int start) {
+        /*
+        change the condition to S > 1000, so that 
+        memo[start][S+1000] is always valid
+        */
+        if(start == nums.size() || S > 1000) return S == 0;
+        
+        if(memo[start][S+1000] != INT_MAX) return memo[start][S+1000];
+        
+        // cout << S << endl;
+        //nums[i] >= 0 for all i
+        //sum of nums <= 1000
+        //add this determination to avoid overflow
+        memo[start][S+1000] = findTargetSumWays(nums, S <= 1000 ? S+nums[start] : INT_MAX, start+1) + 
+            findTargetSumWays(nums, S-nums[start], start+1);
+        
+        return memo[start][S+1000];
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int S) {
+        /*
+        sum of nums <= 1000,
+        so the possible range after adding + or - is [-1000, 1000],
+        we map [-1000,1000] to [0,2000] as the index of memo
+        */
+        memo = vector<vector<int>>(nums.size(), vector(2001, INT_MAX));
+        findTargetSumWays(nums, S, 0);
+        //if S > 1000, it's impossible for us to make up S by nums
+        return S <= 1000 ? memo[0][S+1000] : 0;
+    }
+};
