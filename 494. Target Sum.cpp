45c015e5b4166a1b24469e547b1bea440bef0e86
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-04 20:16:51 +0800

    recursion

--- 494. Target Sum.cpp
+++ 494. Target Sum.cpp
@@ -1,26 +1,20 @@
-//recursion + memorization
-//139 / 139 test cases passed, but took too long.
+//recursion
+//Runtime: 1452 ms, faster than 10.35% of C++ online submissions for Target Sum.
+//Memory Usage: 9.2 MB, less than 38.46% of C++ online submissions for Target Sum.
+//time: O(2^N), space: O(N)
 class Solution {
 public:
-    vector<int> memo;
-    
     int findTargetSumWays(vector<int>& nums, int S, int start) {
         if(start == nums.size() || S == INT_MAX) return S == 0;
         
         //nums[i] >= 0 for all i
         //sum of nums <= 1000
         //add this determination to avoid overflow
-        memo[start] = findTargetSumWays(nums, S <= 1000 ? S+nums[start] : INT_MAX, start+1) + 
+        return findTargetSumWays(nums, S <= 1000 ? S+nums[start] : INT_MAX, start+1) + 
             findTargetSumWays(nums, S-nums[start], start+1);
-        
-        return memo[start];
     }
     
     int findTargetSumWays(vector<int>& nums, int S) {
-        memo = vector<int>(nums.size(), 0);
-        
-        findTargetSumWays(nums, S, 0);
-        
-        return memo[0];
+        return findTargetSumWays(nums, S, 0);
     }
 };
