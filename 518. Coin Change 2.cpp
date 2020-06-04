c748f76aa8835fe55455b7760f3512b8c952d578
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-05 00:14:15 +0800

    O(N^2) space DP

--- 518. Coin Change 2.cpp
+++ 518. Coin Change 2.cpp
@@ -39,6 +39,7 @@ public:
 //O(N) space DP
 //Runtime: 4 ms, faster than 99.34% of C++ online submissions for Coin Change 2.
 //Memory Usage: 7.8 MB, less than 100.00% of C++ online submissions for Coin Change 2.
+//time: O(N^2), space: O(N)
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
@@ -55,5 +56,40 @@ public:
         }
         
         return dp[amount];
+    }
+};
+
+//unbounded knapsack problem
+//https://leetcode.com/problems/coin-change-2/discuss/99212/Knapsack-problem-Java-solution-with-thinking-process-O(nm)-Time-and-O(m)-Space
+//O(N^2) space DP
+//Runtime: 36 ms, faster than 35.46% of C++ online submissions for Coin Change 2.
+//Memory Usage: 26 MB, less than 14.29% of C++ online submissions for Coin Change 2.
+//time: O(N^2), space: O(N^2)
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(5001, 0));
+        
+        //use 0 coin to make up amount = 0
+        dp[0][0] = 1;
+        
+        //i: use coins[0...i-1]
+        for(int i = 1; i <= n; i++){
+            /*
+            dp[i][0] = 1;
+            for(int sum = 1; sum <= amount; sum++){
+            */
+            for(int sum = 0; sum <= amount; sum++){
+                /*
+                dp[i-1][sum]: not choose current coin
+                dp[i][sum-coins[i-1]]: choose current coin(again)
+                */
+                dp[i][sum] = dp[i-1][sum] + (sum >= coins[i-1] ? dp[i][sum-coins[i-1]] : 0);
+            }
+        }
+        
+        //use coins[0...n-1] to make up amount
+        return dp[n][amount];
     }
 };
