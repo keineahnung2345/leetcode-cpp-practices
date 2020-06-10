de1791ce002ecc0236b11bae516534382a612fe8
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-10 18:13:57 +0800

    binary search

--- 35. Search Insert Position.cpp
+++ 35. Search Insert Position.cpp
@@ -12,3 +12,29 @@ public:
         return nums.size();
     }
 };
+
+//binary search
+//Runtime: 4 ms, faster than 99.20% of C++ online submissions for Search Insert Position.
+//Memory Usage: 9.7 MB, less than 57.82% of C++ online submissions for Search Insert Position.
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n-1;
+        int mid;
+        
+        //find lower bound
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        
+        return l;
+    }
+};
