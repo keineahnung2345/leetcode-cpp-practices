642dba897d6bc2cc674e444030ab1f81791451d4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-10 19:03:08 +0800

    optimized backtracking

--- 526. Beautiful Arrangement.cpp
+++ 526. Beautiful Arrangement.cpp
@@ -34,3 +34,37 @@ public:
         return count;
     }
 };
+
+//optimized backtracking
+//Runtime: 360 ms, faster than 37.06% of C++ online submissions for Beautiful Arrangement.
+//Memory Usage: 6.1 MB, less than 61.13% of C++ online submissions for Beautiful Arrangement.
+class Solution {
+public:
+    int N;
+    
+    int backtrack(int cur, bitset<15>& used){
+        if(cur > N){
+            return 1;
+        }else{
+            int count = 0;
+            
+            for(int i = 0; i < N; i++){
+                //i+1: the value to be inserted
+                //cur: the value's index in perm(1-based)
+                if(!used[i] && ((i+1) % cur == 0 || cur % (i+1) == 0)){
+                    used[i] = 1;
+                    count += backtrack(cur+1, used);
+                    used[i] = 0;
+                }
+            }
+            
+            return count;
+        }
+    }
+    
+    int countArrangement(int N) {
+        this->N = N;
+        bitset<15> used;
+        return backtrack(1, used);
+    }
+};
