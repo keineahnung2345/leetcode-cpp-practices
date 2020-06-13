75078432e11632276045803907276632dd241da4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-13 12:01:08 +0800

    dfs + memorization

--- 1473. Paint House III.cpp
+++ 1473. Paint House III.cpp
@@ -56,3 +56,82 @@ public:
         return (ans == INT_MAX) ? -1 : ans;
     }
 };
+
+//dfs + memorization
+//TLE
+//25 / 60 test cases passed.
+//https://leetcode.com/problems/paint-house-iii/discuss/674313/Simple-Python-explanation-and-why-I-prefer-top-down-DP-to-bottom-up
+class Solution {
+public:
+    /*
+    m == houses.length == cost.length
+    n == cost[i].length
+    1 <= m <= 100
+    1 <= n <= 20
+    1 <= target <= m
+    0 <= houses[i] <= n
+    1 <= cost[i][j] <= 10^4
+    max cost is 10^4 * 100 = 1e6
+    */
+    int MAX = 1e6+1;
+    vector<int> houses;
+    vector<vector<int>> cost;
+    int m, n, target;
+    vector<vector<vector<int>>> memo;
+    
+    int dfs(int start, int t, int prevColor){
+        // cout << "(" << start << ",  " << t << ", " << prevColor << ")" << endl;
+        if(t < 0 || t > m-start){
+            /*
+            t > m-start: 
+            we only have (m-1)-(start-1) = m-start houses remained,
+            but we need more neighborhoods
+            */
+            // cout << "(" << start << ",  " << t << ", " << prevColor << "): " << MAX << endl;
+            return MAX;
+        }else if(start == m){
+            // cout << "(" << start << ",  " << t << ", " << prevColor << "): " << ((t == 0) ? 0 : MAX) << endl;
+            return (t == 0) ? 0 : MAX;
+        }else if(memo[start][t][prevColor+1] < MAX){
+            return memo[start][t][prevColor+1];
+        }else if(houses[start] != 0){
+            memo[start][t][prevColor+1] = dfs(start+1, t - (houses[start] != prevColor), houses[start]);
+            // cout << "(" << start << ",  " << t << ", " << prevColor << "): " << memo[start][t][prevColor+1] << endl;
+            return memo[start][t][prevColor+1];
+        }else{
+            /*
+            note that color = 0 means unpainted,
+            and color's range is [1,n]!
+            */
+            for(int color = 1; color <= n; color++){
+                int tmp = dfs(start+1, t - (color != prevColor), color);
+                if(tmp < MAX){
+                    memo[start][t][prevColor+1] = min(memo[start][t][prevColor+1], cost[start][color-1] + tmp);
+                }
+                // memo[start][t][prevColor] = min(memo[start][t][prevColor], cost[start][color-1] + dfs(start+1, t - (color != prevColor), color));
+            }
+            // cout << "(" << start << ",  " << t << ", " << prevColor << "): " << memo[start][t][prevColor+1] << endl;
+            return memo[start][t][prevColor+1];
+        }
+    };
+    
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        this->houses = houses;
+        this->cost = cost;
+        this->m = m;
+        this->n = n;
+        this->target = target;
+        
+        //t: [0, target]
+        //prevColor: [-1, n] -> [0, n+1]
+        memo = vector<vector<vector<int>>>(m, vector<vector<int>>(target+1, vector<int>(n+2, MAX)));
+        
+        /*
+        set 0th color as -1,
+        because when we paint 0th house,
+        we will consume one neighborhood anyway
+        */
+        int ans = dfs(0, target, -1);
+        return (ans >= MAX) ? -1 : ans;
+    }
+};
