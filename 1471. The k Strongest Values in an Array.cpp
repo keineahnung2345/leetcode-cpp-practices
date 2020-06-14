1d98b61387304e1ff4bc69d6ccae0d04dee531a3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-14 10:08:08 +0800

    heap

--- 1471. The k Strongest Values in an Array.cpp
+++ 1471. The k Strongest Values in an Array.cpp
@@ -60,7 +60,7 @@ public:
     vector<int> getStrongest(vector<int>& arr, int k) {
         int n = arr.size();
         if(n == 0) return arr;
-        //O(n)
+        //quick select, O(n)
         nth_element(arr.begin(), arr.begin() + (n-1)/2, arr.end());
         int median = arr[(n-1)/2];
         //O(nlogk)
@@ -70,5 +70,39 @@ public:
             });
         arr.resize(k);
         return arr;
+    }
+};
+
+//heap
+//https://leetcode.com/problems/the-k-strongest-values-in-an-array/discuss/674384/C%2B%2BJavaPython-Two-Pointers-%2B-3-Bonuses
+//Runtime: 992 ms, faster than 47.27% of C++ online submissions for The k Strongest Values in an Array.
+//Memory Usage: 85.8 MB, less than 100.00% of C++ online submissions for The k Strongest Values in an Array.
+//time: O(n+klogn)
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        int n = arr.size();
+        if(n == 0) return arr;
+        //O(n)
+        nth_element(arr.begin(), arr.begin() + (n-1)/2, arr.end());
+        int median = arr[(n-1)/2];
+        
+        //the comp function is the opposite from previous method!
+        //since we want the larger to be popped earlier
+        auto comp = [&median](const int& a, const int& b){
+            return abs(a-median) == abs(b-median) ? a < b : abs(a-median) < abs(b-median);
+        };
+        
+        //build heap: O(n)
+        //https://www.geeksforgeeks.org/time-complexity-of-building-a-heap/
+        priority_queue<int, vector<int>, decltype(comp)> pq(arr.begin(), arr.end(), comp);
+        
+        //O(klogn)
+        vector<int> ans;
+        while(k-- > 0){
+            ans.push_back(pq.top()); pq.pop();
+        }
+        
+        return ans;
     }
 };
