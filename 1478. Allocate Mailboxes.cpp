cd2f90192d0011d88b25cbc955075f7b96f9fd90
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-14 22:59:02 +0800

    recursion + memorization + cache cost

--- 1478. Allocate Mailboxes.cpp
+++ 1478. Allocate Mailboxes.cpp
@@ -49,14 +49,6 @@ public:
 //Memory Usage: 8.4 MB, less than 100.00% of C++ online submissions for Allocate Mailboxes.
 class Solution {
 public:
-    /*
-    1 <= houses.length <= 100
-    1 <= houses[i] <= 10^4
-    1 <= k <= houses.length
-    
-    one mailbox's cost: at most 100*1e4
-    k mailboxes' cost: at most 100*100*1e4
-    */
     int MAX = 1e8;
     vector<vector<int>> memo;
     
@@ -68,10 +60,6 @@ public:
         int ans = MAX;
         
         for(int end = start; end < houses.size(); end++){
-            /*
-            put one mailbox in [start, end] and 
-            remaining k-1 mailboxes in [end+1, n-1]
-            */
             int median = houses[(start+end)/2];
             int cost = 0;
             for(int i = start; i <= end; i++){
@@ -84,10 +72,54 @@ public:
     };
     
     int minDistance(vector<int>& houses, int k) {
-        //this is important because we need to calculate median!
         sort(houses.begin(), houses.end());
         //k = 0 for padding
         memo = vector<vector<int>>(k+1, vector<int>(houses.size()));
+        return dfs(houses, k, 0);
+    }
+};
+
+//recursion + memorization + cache cost
+//https://leetcode.com/problems/allocate-mailboxes/discuss/685620/JavaC%2B%2BPython-Top-down-DP-Prove-median-mailbox-O(n3)
+//Runtime: 284 ms, faster than 66.67% of C++ online submissions for Allocate Mailboxes.
+//Memory Usage: 9.2 MB, less than 66.67% of C++ online submissions for Allocate Mailboxes.
+//time: O(n^3 + k*n^2), O(n^3) for calculating cost, O(k*n^2) for k*n states of dp, each takes O(n)
+//space: O(n^2)
+class Solution {
+public:
+    int MAX = 1e8;
+    vector<vector<int>> cost;
+    vector<vector<int>> memo;
+    
+    int dfs(vector<int>& houses, int k, int start){
+        if(k == 0 && start == houses.size()) return 0;
+        if(k == 0 || start == houses.size()) return MAX;
+        if(memo[k][start] != 0) return memo[k][start];
+        
+        int ans = MAX;
+        
+        for(int end = start; end < houses.size(); end++){
+            ans = min(ans, cost[start][end] + dfs(houses, k-1, end+1));
+        }
+        
+        return memo[k][start] = ans;
+    };
+    
+    int minDistance(vector<int>& houses, int k) {
+        int n = houses.size();
+        sort(houses.begin(), houses.end());
+        memo = vector<vector<int>>(k+1, vector<int>(n));
+        
+        //precalculate cost of [i, j]
+        cost = vector<vector<int>>(n, vector<int>(n));
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                for(int k = i; k <= j; k++){
+                    cost[i][j] += abs(houses[(i+j)/2] - houses[k]);
+                }
+            }
+        }
+        
         return dfs(houses, k, 0);
     }
 };
