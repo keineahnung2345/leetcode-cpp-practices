0e2ba6f9dcfe30eb06845527a6dd2e9972364b42
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-14 10:28:28 +0800

    two stacks

--- 1472. Design Browser History.cpp
+++ 1472. Design Browser History.cpp
@@ -60,3 +60,48 @@ public:
  * string param_3 = obj->forward(steps);
  */
  
+//two stacks
+//https://leetcode.com/problems/design-browser-history/discuss/674300/Two-Stacks-vs.-List
+//Runtime: 752 ms, faster than 5.01% of C++ online submissions for Design Browser History.
+//Memory Usage: 120 MB, less than 100.00% of C++ online submissions for Design Browser History.
+class BrowserHistory {
+public:
+    stack<string> fw, bw;
+    string cur;
+    
+    BrowserHistory(string homepage) {
+        cur = homepage;
+    }
+    
+    void visit(string url) {
+        fw = stack<string>(); //there is no stack::clear()!
+        bw.push(cur);
+        cur = url;
+    }
+    
+    string back(int steps) {
+        while(!bw.empty() && steps-- > 0){
+            fw.push(cur);
+            cur = bw.top(); bw.pop();
+        }
+        
+        return cur;
+    }
+    
+    string forward(int steps) {
+        while(!fw.empty() && steps-- > 0){
+            bw.push(cur);
+            cur = fw.top(); fw.pop();
+        }
+        
+        return cur;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
