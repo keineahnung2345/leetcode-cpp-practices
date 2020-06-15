aeb64e8555a228638a0eb023d9c5f9fa674f528d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-15 21:49:12 +0800

    dp + bit manipulation

--- 1483. Kth Ancestor of a Tree Node.cpp
+++ 1483. Kth Ancestor of a Tree Node.cpp
@@ -103,3 +103,62 @@ public:
  * TreeAncestor* obj = new TreeAncestor(n, parent);
  * int param_1 = obj->getKthAncestor(node,k);
  */
+
+//dp + bit manipulation
+//https://leetcode.com/problems/kth-ancestor-of-a-tree-node/discuss/686362/JavaC%2B%2BPython-Binary-Lifting
+//fails when set "maxJump = ceil(log(n));", don't know why?
+//Runtime: 764 ms, faster than 78.89% of C++ online submissions for Kth Ancestor of a Tree Node.
+//Memory Usage: 111.6 MB, less than 100.00% of C++ online submissions for Kth Ancestor of a Tree Node.
+class TreeAncestor {
+public:
+    int maxJump;
+    vector<vector<int>> jump;
+    
+    TreeAncestor(int n, vector<int>& parent) {
+        /*
+        for a tree of n nodes,
+        we can jump at most "maxJump" times
+        */
+        // maxJump = ceil(log(n));
+        maxJump = 20;
+        //from 0 to maxJump
+        jump = vector<vector<int>>(maxJump+1, vector<int>(n));
+        
+        //jump 2^0 steps upward
+        jump[0] = parent;
+        
+        for(int j = 1; j <= maxJump; j++){
+            for(int node = 0; node < n; node++){
+                //from node, jump 2^(j-1) steps upward
+                int halfway = jump[j-1][node];
+                jump[j][node] = (halfway == -1) ? -1 : jump[j-1][halfway];
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        // cout << "k: " << k << ", j: ";
+        for(int j = 0; j <= maxJump; j++){
+            if(k & (1 << j)){
+                /*
+                view "k" as a bitset,
+                and we are decompose "k" into the powers of 2
+                when "k"'s "j"th bit is set,
+                that means one of the component of k is 2^j,
+                so we jump up 2^j steps upward
+                */
+                // cout << j << " ";
+                node = jump[j][node];
+                if(node == -1) break;
+            }
+        }
+        // cout << endl;
+        return node;
+    }
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
