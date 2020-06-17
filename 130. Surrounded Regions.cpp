4eafd6c0f4215c26469c5cc78134710b7d870879
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-17 22:49:03 +0800

    union find

--- 130. Surrounded Regions.cpp
+++ 130. Surrounded Regions.cpp
@@ -126,3 +126,99 @@ public:
         }
     }
 };
+
+//union find
+//https://leetcode.com/problems/surrounded-regions/discuss/41617/Solve-it-using-Union-Find
+//Runtime: 36 ms, faster than 41.79% of C++ online submissions for Surrounded Regions.
+//Memory Usage: 10.4 MB, less than 63.29% of C++ online submissions for Surrounded Regions.
+class DSU{
+public:
+    vector<int> parent;
+    
+    DSU(int N){
+        parent = vector<int>(N);
+        iota(parent.begin(), parent.end(), 0);
+    };
+    
+    //wrong implementation
+    // int find(int x){
+    //     if(parent[x] == x){
+    //         return x;
+    //     }
+    //     return parent[x] = find(parent[x]);
+    // };
+    
+    int find(int x){
+        if(parent[x] != x){ //initial state
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    
+    void unite(int x, int y){
+        parent[find(x)] = find(y);
+    };
+    
+    bool isConnected(int x, int y){
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
+public:
+    int m, n;
+    
+    int node(int i, int j){
+        return i*n+j;
+    };
+    
+    void solve(vector<vector<char>>& board) {
+        m = board.size();
+        if(m == 0) return;
+        n = board[0].size();
+        if(n == 0) return;
+        
+        vector<vector<int>> dirs = {
+            {0,1},
+            {0,-1},
+            {1,0},
+            {-1,0}
+        };
+        
+        //[0, m*n-1] for each cell, m*n for dummy cell
+        DSU dsu(m*n+1);
+        int dummyNode = m*n;
+        
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(board[i][j] != 'O') continue;
+                if(i == 0 || i == m-1 || j == 0 || j == n-1){
+                    dsu.unite(node(i, j), dummyNode);
+                }else{
+                    for(vector<int>& dir : dirs){
+                        int ni = i + dir[0];
+                        int nj = j + dir[1];
+                        if(ni >= 0 && ni < m && nj >= 0 && nj < n && board[ni][nj] == 'O'){
+                            dsu.unite(node(ni, nj), node(i, j));
+                        }
+                    }
+                    //another implementation
+                    // if(i > 0 && board[i-1][j] == 'O')  dsu.unite(node(i,j), node(i-1,j));
+                    // if(i < m-1 && board[i+1][j] == 'O')  dsu.unite(node(i,j), node(i+1,j));
+                    // if(j > 0 && board[i][j-1] == 'O')  dsu.unite(node(i,j), node(i, j-1));
+                    // if(j < n-1 && board[i][j+1] == 'O')  dsu.unite(node(i,j), node(i, j+1));
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(dsu.isConnected(node(i, j), dummyNode)){
+                    board[i][j] = 'O';
+                }else{
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
