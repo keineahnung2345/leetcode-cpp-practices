c3efe136700a64398e07cbaf29144491125340ef
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-17 21:02:35 +0800

    Dijkstra's Algorithm, priority queue

--- 787. Cheapest Flights Within K Stops.cpp
+++ 787. Cheapest Flights Within K Stops.cpp
@@ -41,3 +41,41 @@ public:
         return dist[dst] == MAX ? -1 : dist[dst];
     }
 };
+
+//Single-Source Shortest Path：Dijkstra's Algorithm, priority queue
+//https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/115541/JavaPython-Priority-Queue-Solution
+//Runtime: 96 ms, faster than 45.17% of C++ online submissions for Cheapest Flights Within K Stops.
+//Memory Usage: 17.2 MB, less than 22.17% of C++ online submissions for Cheapest Flights Within K Stops.
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        vector<vector<pair<int, int>>> edges(n);
+        for(vector<int>& flight : flights){
+            //src -> (dst1, price1), (dst2, price2), ...
+            edges[flight[0]].emplace_back(flight[1], flight[2]);
+        }
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        vector<int> cur;
+        int curCost, curSrc, curK;
+        
+        //(cost from source, current node, max distance to destination allowed)
+        pq.push({0, src, K+1});
+        
+        //visit nodes in increasing price order
+        while(!pq.empty()){
+            cur = pq.top(); pq.pop();
+            curCost = cur[0]; curSrc = cur[1]; curK = cur[2];
+            if(curSrc == dst){
+                return curCost;
+            }
+            if(curK > 0){
+                for(pair<int, int>& nei : edges[curSrc]){
+                    pq.push({curCost+nei.second, nei.first, curK-1});
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
