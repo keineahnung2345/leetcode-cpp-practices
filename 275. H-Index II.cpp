9d3bee0878835d35a3ddc2cab628f23a7992ede5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-18 21:25:56 +0800

    add ref

--- 275. H-Index II.cpp
+++ 275. H-Index II.cpp
@@ -1,5 +1,8 @@
+//binary search
+//https://leetcode.com/problems/h-index-ii/discuss/71063/Standard-binary-search
 //Runtime: 32 ms, faster than 88.51% of C++ online submissions for H-Index II.
 //Memory Usage: 18.7 MB, less than 47.45% of C++ online submissions for H-Index II.
+//time: O(logN), space: O(1)
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
