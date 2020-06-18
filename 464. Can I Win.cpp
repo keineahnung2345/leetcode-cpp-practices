258807f53f065e84800a0f56bab7bff81a454a46
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-18 21:48:40 +0800

    brute force

--- 464. Can I Win.cpp
+++ 464. Can I Win.cpp
@@ -1,37 +1,57 @@
+//brute force
+//https://leetcode.com/problems/can-i-win/discuss/155321/From-Brute-Force-to-Top-down-DP
 //TLE
 //5 / 54 test cases passed.
 class Solution {
 public:
     bool canWin(vector<bool>& choosable, int target){
-        if(std::all_of(choosable.begin(), choosable.end(), [](const bool& b){return b == false;})){
+        if(none_of(choosable.begin(), choosable.end(),
+            [](const bool b){return b;})){
             return false;
         }
         
-        auto it = std::find_if(choosable.rbegin(), choosable.rend(), 
-                    [](const bool b){return b == true;});
-        int maxChoosable = distance(it, choosable.rend()-1)+1;
+        if(target <= 0){
+            return false;
+        }
+        
+        int maxChoosable;
+        for(int i = choosable.size()-1; i >= 1; i--){
+            if(choosable[i]){
+                maxChoosable = i;
+                break;
+            }
+        }
         
         if(target <= maxChoosable){
             //we can go to the number >= target
             return true;
-        }else{
-            //once we can make the next player lose, we win
-            for(int i = 0; i < choosable.size(); i++){
-                if(choosable[i]){
-                    bool nextCanWin;
-                    choosable[i] = false;
-                    nextCanWin = canWin(choosable, target-(i+1));
-                    choosable[i] = true;
-                    if(!nextCanWin) return true;
-                }
-            }
-            //the next player will win anyway
-            return false;
         }
+        
+        //once we can make the next player lose, we win
+        for(int i = 1; i < choosable.size(); i++){
+            if(choosable[i]){
+                bool nextCanWin;
+                choosable[i] = false;
+                nextCanWin = canWin(choosable, target-i);
+                choosable[i] = true;
+                if(!nextCanWin) return true;
+            }
+        }
+        //the next player will win anyway
+        return false;
     }
     
     bool canIWin(int maxChoosableInteger, int desiredTotal) {
-        vector<bool> choosable(maxChoosableInteger, true);
+        if(desiredTotal <= maxChoosableInteger){
+            return true;
+        }
+        
+        if((1+maxChoosableInteger)*maxChoosableInteger/2 < desiredTotal){
+            return false;
+        }
+        
+        //0 for padding
+        vector<bool> choosable(maxChoosableInteger+1, true);
         return canWin(choosable, desiredTotal);
     }
 };
