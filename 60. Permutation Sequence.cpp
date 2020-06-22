382bd5e72687bb2db30ef8091035ff1456f5f380
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-22 23:28:42 +0800

    math

--- 60. Permutation Sequence.cpp
+++ 60. Permutation Sequence.cpp
@@ -83,3 +83,37 @@ public:
         return ans;
     }
 };
+
+//math
+//https://leetcode.com/problems/permutation-sequence/discuss/22507/%22Explain-like-I'm-five%22-Java-Solution-in-O(n)
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Permutation Sequence.
+//Memory Usage: 5.9 MB, less than 86.08% of C++ online submissions for Permutation Sequence.
+//time: O(N)
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string ans = "";
+        
+        vector<int> nums(n);
+        iota(nums.begin(), nums.end(), 1);
+        
+        int fact = 1;
+        for(int i = 1; i <= n; ++i){
+            fact *= i;
+        }
+        
+        --k; //1-based -> 0-based
+        for(int i = 0; i < n; ++i){
+            //there could be (n-1)! permutations if we exclude one char
+            fact /= (n-i);
+            //the kth permutation belongs to what group
+            int groupid = k/fact;
+            ans += (nums[groupid] + '0');
+            nums.erase(nums.begin()+groupid);
+            //there are "groupid" groups before the visited group
+            k -= fact * groupid;
+        }
+        
+        return ans;
+    }
+};
