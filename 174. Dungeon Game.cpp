c45ecfe9d1d0af8ed87255aca09e28023dad0c78
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-25 12:18:03 +0800

    DP

--- 174. Dungeon Game.cpp
+++ 174. Dungeon Game.cpp
@@ -79,3 +79,67 @@ public:
         return (minhp[m-1][n-1] <= 0) ? (-minhp[m-1][n-1]+1) : 1;
     }
 };
+
+//DP, top-bottom(starts from right-bottom corner)
+//https://leetcode.com/problems/dungeon-game/discuss/698271/Python-Short-DP-7-lines-O(mn)-top-down-explained
+//Runtime: 12 ms, faster than 83.36% of C++ online submissions for Dungeon Game.
+//Memory Usage: 9.2 MB, less than 13.19% of C++ online submissions for Dungeon Game.
+//time: O(mn), space: O(mn)
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        
+        //padding to the bottom and to the right
+        vector<vector<int>> minhp(m+1, vector<int>(n+1, INT_MAX));
+        
+        /*
+        (m,n-1) and (m-1, n) is the bottom neighbor and 
+        right neighbor of princess
+        */
+        minhp[m][n-1] = minhp[m-1][n] = 1;
+        
+        for(int i = m-1; i >= 0; --i){
+            for(int j = n-1; j >= 0; --j){
+                if(dungeon[i][j] - min(minhp[i+1][j], minhp[i][j+1]) >= 0){
+                    /*
+                    we need min(minhp[i+1][j], minhp[i][j+1]) hp
+                    so that we can survive if we go to (i+1,j) or (i,j+1).
+                    
+                    dungeon[i][j] - min(minhp[i+1][j], minhp[i][j+1]) means
+                    when we are at (i,j), we get dungeon[i][j] hp,
+                    and when we go to (i+1,j) or (i,j+1), 
+                    we will use min(minhp[i+1][j], minhp[i][j+1]) hp
+                    
+                    if this number >= 0, that means we can survive at (i,j) with
+                    minimum possible hp, which is 1
+                    */
+                    minhp[i][j] = 1;
+                }else{
+                    /*
+                    at (i,j), our hp is dungeon[i][j],
+                    (since the algorithm starts from bottom-right, 
+                    we don't consider the cells before (i,j)),
+                    when we go to (i+1,j) or (i,j+1),
+                    we remain dungeon[i][j] - min(minhp[i+1][j], minhp[i][j+1])
+                    
+                    minhp[i+1][j]: we need this hp to survive at (i+1,j)
+                    minhp[i][j+1]: we need this hp to survive at (i,j+1)
+                    
+                    min(minhp[i+1][j], minhp[i][j+1]) -dungeon[i][j]:
+                    we need this hp to survive at (i,j)
+                    */
+                    
+                    //signed integer overflow
+                    // minhp[i][j] = min(minhp[i+1][j]-dungeon[i][j],
+                    //                   minhp[i][j+1]-dungeon[i][j]);
+                    
+                    minhp[i][j] = min(minhp[i+1][j], minhp[i][j+1])-dungeon[i][j];
+                }
+            }
+        }
+        
+        return minhp[0][0];
+    }
+};
