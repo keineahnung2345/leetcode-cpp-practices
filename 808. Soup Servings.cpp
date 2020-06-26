ae9ccbee60f120e33dd7754543011ad4f23c8029
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-26 12:10:35 +0800

    conversion, using vector

--- 808. Soup Servings.cpp
+++ 808. Soup Servings.cpp
@@ -93,6 +93,29 @@ public:
     }
 };
 
+//conversion
+//Runtime: 8 ms, faster than 52.57% of C++ online submissions for Soup Servings.
+//Memory Usage: 14.5 MB, less than 13.63% of C++ online submissions for Soup Servings.
+class Solution {
+public:
+    vector<vector<double>> memo;
+    
+    double f(int NA, int NB){
+        if(NA <= 0 && NB <= 0) return 0.5;
+        if(NA <= 0) return 1.0;
+        if(NB <= 0) return 0.0;
+        if(memo[NA][NB] > 0) return memo[NA][NB];
+        return memo[NA][NB] = 0.25*(f(NA-4,NB)+f(NA-3,NB-1)+f(NA-2,NB-2)+f(NA-1,NB-3));
+    };
+    
+    double soupServings(int N) {
+        if(N > 4800) return 1.0;
+        memo = vector<vector<double>>(200, vector<double>(200));
+        //conversion from 25's multiple to the number of spoons
+        return f(ceil((double)N/25), ceil((double)N/25));
+    }
+};
+
 //conversion, using array
 //https://leetcode.com/problems/soup-servings/discuss/121711/C%2B%2BJavaPython-When-N-greater-4800-just-return-1
 //Runtime: 4 ms, faster than 69.96% of C++ online submissions for Soup Servings.
