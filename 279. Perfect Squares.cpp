6f0f646d27c65e13398ba7ee4fbd43b61cb89a84
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-28 09:59:15 +0800

    DP

--- 279. Perfect Squares.cpp
+++ 279. Perfect Squares.cpp
@@ -26,3 +26,28 @@ public:
         return dp[n];
     }
 };
+
+//DP
+//https://leetcode.com/problems/perfect-squares/discuss/71488/Summary-of-4-different-solutions-(BFS-DP-static-DP-and-mathematics)
+//Runtime: 320 ms, faster than 29.50% of C++ online submissions for Perfect Squares.
+//Memory Usage: 9 MB, less than 69.72% of C++ online submissions for Perfect Squares.
+class Solution {
+public:
+    int numSquares(int n) {
+        //padding ahead
+        vector<int> dp(n+1, 0);
+        
+        for(int i = 1; i <= n; ++i){
+            int count = INT_MAX;
+            /*
+            i = j*j + (i-j*j) for j <= sqrt(i)
+            */
+            for(int j = 1; j*j <= i; ++j){
+                count = min(count, dp[i-j*j]+1);
+            }
+            dp[i] = count;
+        }
+        
+        return dp[n];
+    }
+};
