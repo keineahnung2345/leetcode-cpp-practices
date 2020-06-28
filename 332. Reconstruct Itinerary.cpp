2aaa2ecd50b73dc57ba28dd023392a9f948ab305
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-28 22:16:55 +0800

    iterative

--- 332. Reconstruct Itinerary.cpp
+++ 332. Reconstruct Itinerary.cpp
@@ -1,4 +1,4 @@
-//Graph, Eulerian path
+//Graph, Eulerian path, not understand?
 //https://leetcode.com/problems/reconstruct-itinerary/discuss/78768/Short-Ruby-Python-Java-C%2B%2B
 //Runtime: 88 ms, faster than 19.85% of C++ online submissions for Reconstruct Itinerary.
 //Memory Usage: 15 MB, less than 54.64% of C++ online submissions for Reconstruct Itinerary.
@@ -32,6 +32,37 @@ public:
         */
         
         dfs("JFK");
+        
+        return route;
+    }
+};
+
+//iterative, not understand?
+//https://leetcode.com/problems/reconstruct-itinerary/discuss/78768/Short-Ruby-Python-Java-C%2B%2B
+//Runtime: 88 ms, faster than 19.85% of C++ online submissions for Reconstruct Itinerary.
+//Memory Usage: 14.6 MB, less than 71.60% of C++ online submissions for Reconstruct Itinerary.
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, multiset<string>> edges;
+        
+        for(vector<string>& ticket : tickets){
+            edges[ticket[0]].insert(ticket[1]);
+        }
+        
+        vector<string> route;
+        stack<string> stk;
+        stk.push("JFK");
+        
+        while(!stk.empty()){
+            while(edges.find(stk.top()) != edges.end() && !edges[stk.top()].empty()){
+                string next = *edges[stk.top()].begin();
+                edges[stk.top()].erase(edges[stk.top()].begin());
+                stk.push(next);
+            }
+            route.insert(route.begin(), stk.top());
+            stk.pop();
+        }
         
         return route;
     }
