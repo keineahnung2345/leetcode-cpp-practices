58d3463ac41e623b1223ace357dea4746e132442
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-28 18:17:16 +0800

    bottom-up DP

--- 576. Out of Boundary Paths.cpp
+++ 576. Out of Boundary Paths.cpp
@@ -30,3 +30,49 @@ public:
         return dfs(m, n, N, i, j);
     }
 };
+
+//Approach 3: Dynamic Programming
+//bottom-up DP
+//Runtime: 12 ms, faster than 69.88% of C++ online submissions for Out of Boundary Paths.
+//Memory Usage: 9.3 MB, less than 54.36% of C++ online submissions for Out of Boundary Paths.
+//time: O(mnN), space: O(mn)
+class Solution {
+public:
+    int findPaths(int m, int n, int N, int i, int j) {
+        int MOD = 1e9+7;
+        //how many possible ways to go to (i, j)
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        //take 0 moves
+        dp[i][j] = 1;
+        
+        //how many possible ways to cross the boundary
+        int ans = 0;
+        
+        //take N moves
+        while(N-- > 0){
+            vector<vector<int>> tmp(m, vector<int>(n, 0));
+            
+            for(int i = 0; i < m; ++i){
+                for(int j = 0; j < n; ++j){
+                    //cross boundary, update ans
+                    if(i == 0)   ans = (ans+dp[i][j])%MOD;
+                    if(i == m-1) ans = (ans+dp[i][j])%MOD;
+                    if(j == 0)   ans = (ans+dp[i][j])%MOD;
+                    if(j == n-1) ans = (ans+dp[i][j])%MOD;
+                    
+                    //update (i,j)'s count, from 4 directions
+                    int a = (i>0) ? dp[i-1][j] : 0;
+                    int b = (i<m-1) ? dp[i+1][j] : 0;
+                    int c = (j>0) ? dp[i][j-1] : 0;
+                    int d = (j<n-1) ? dp[i][j+1] : 0;
+                    
+                    tmp[i][j] = (((a+b)%MOD + c)%MOD + d)%MOD;
+                }
+            }
+            
+            swap(dp, tmp);
+        }
+        
+        return ans;
+    }
+};
