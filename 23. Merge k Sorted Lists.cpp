aed11ca6237cd8a295b1fe520c5dcada426005ad
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-29 21:08:24 +0800

    recursion + iteration

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -50,3 +50,50 @@ public:
         return head->next;
     }
 };
+
+//recursion
+//https://leetcode.com/problems/merge-two-sorted-lists/discuss/9715/Java-1-ms-4-lines-codes-using-recursion
+//Runtime: 8 ms, faster than 71.22% of C++ online submissions for Merge Two Sorted Lists.
+//Memory Usage: 14.3 MB, less than 94.06% of C++ online submissions for Merge Two Sorted Lists.
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
+
+//iterative
+//https://leetcode.com/problems/merge-two-sorted-lists/discuss/9735/Python-solutions-(iteratively-recursively-iteratively-in-place).
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Merge Two Sorted Lists.
+//Memory Usage: 14.5 MB, less than 55.22% of C++ online submissions for Merge Two Sorted Lists.
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(-1);
+        ListNode *cur = head;
+        
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val <= l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        
+        if(l1 != nullptr) cur->next = l1;
+        else if(l2 != nullptr) cur->next = l2;
+        
+        return head->next;
+    }
+};
