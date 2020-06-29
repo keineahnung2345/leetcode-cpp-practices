9a949cdbfb249e01d933624f29b162e6596af0d3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-06-29 21:03:29 +0800

    iterative

--- 21. Merge Two Sorted Lists.cpp
+++ 21. Merge Two Sorted Lists.cpp
@@ -75,3 +75,31 @@ public:
         }
     }
 };
+
+//iterative
+//https://leetcode.com/problems/merge-two-sorted-lists/discuss/9735/Python-solutions-(iteratively-recursively-iteratively-in-place).
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Merge Two Sorted Lists.
+//Memory Usage: 14.5 MB, less than 55.22% of C++ online submissions for Merge Two Sorted Lists.
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode(-1);
+        ListNode *cur = head;
+        
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val <= l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        
+        if(l1 != nullptr) cur->next = l1;
+        else if(l2 != nullptr) cur->next = l2;
+        
+        return head->next;
+    }
+};
