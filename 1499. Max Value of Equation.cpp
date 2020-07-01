8f57833352345a7ce0b2217dc43284abfda6ad16
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-01 22:20:24 +0800

    monotonic deque

--- 1499. Max Value of Equation.cpp
+++ 1499. Max Value of Equation.cpp
@@ -1,3 +1,5 @@
+//priority queue
+//https://leetcode.com/problems/max-value-of-equation/discuss/709231/Python-Stack-O(N)
 //Runtime: 896 ms, faster than 29.81% of C++ online submissions for Max Value of Equation.
 //Memory Usage: 112.3 MB, less than 100.00% of C++ online submissions for Max Value of Equation.
 class Solution {
@@ -38,6 +40,44 @@ public:
             }
             
             pq.push({y-x, x});
+        }
+        
+        return ans;
+    }
+};
+
+//monotonic deque
+//https://leetcode.com/problems/max-value-of-equation/discuss/709231/Python-Stack-O(N)
+//Runtime: 700 ms, faster than 52.36% of C++ online submissions for Max Value of Equation.
+//Memory Usage: 109.1 MB, less than 100.00% of C++ online submissions for Max Value of Equation.
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        //(y-x, x)
+        deque<vector<int>> deq;
+        
+        int ans = INT_MIN;
+        
+        for(vector<int>& point : points){
+            int x = point[0], y = point[1];
+            
+            while(!deq.empty() && x - deq.front()[1] > k){
+                deq.pop_front();
+            }
+            
+            if(!deq.empty()){
+                ans = max(ans, deq.front()[0] + (y + x));
+            }
+            
+            /*
+            keep the deque decreasing(fronter element has larger y-x)
+            the element before current point must have larger y-x
+            */
+            while(!deq.empty() && deq.back()[0] <= y - x){
+                deq.pop_back();
+            }
+            
+            deq.push_back({y-x, x});
         }
         
         return ans;
