90339d5277cab3dc4920a9b2913881fe36623871
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-02 18:09:04 +0800

    bfs another style

--- 107. Binary Tree Level Order Traversal II.cpp
+++ 107. Binary Tree Level Order Traversal II.cpp
@@ -63,3 +63,46 @@ public:
         return ans;
     }
 };
+
+//another style
+//Runtime: 32 ms, faster than 10.47% of C++ online submissions for Binary Tree Level Order Traversal II.
+//Memory Usage: 13.1 MB, less than 25.93% of C++ online submissions for Binary Tree Level Order Traversal II.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        if(root != nullptr){
+            TreeNode* cur;
+            queue<TreeNode*> q;
+            q.push(root);
+
+            while(!q.empty()){
+                int levelSize = q.size();
+                vector<int> level;
+                while(levelSize-- > 0){
+                    cur = q.front(); q.pop();
+
+                    level.push_back(cur->val);
+
+                    if(cur->left) q.push(cur->left);
+                    if(cur->right) q.push(cur->right);
+                }
+                ans.insert(ans.begin(), level);
+            }
+        }
+        
+        return ans;
+    }
+};
