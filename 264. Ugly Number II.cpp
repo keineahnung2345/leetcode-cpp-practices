b2de4bbf62e4ad82ceeff714996d0d52053f2eb8
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-04 21:12:49 +0800

    priority queue

--- 264. Ugly Number II.cpp
+++ 264. Ugly Number II.cpp
@@ -34,3 +34,53 @@ public:
         return dp[n-1];
     }
 };
+
+//priority queue
+//https://leetcode.com/problems/ugly-number-ii/discuss/69397/Sharing-very-simple-and-elegant-Python-solution-using-heap-with-explanation
+//Runtime: 904 ms, faster than 5.03% of C++ online submissions for Ugly Number II.
+//Memory Usage: 36.2 MB, less than 5.06% of C++ online submissions for Ugly Number II.
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        //val, last multiplied factor
+        //the smaller val should be popped first
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        
+        vector<vector<int>> primes = {{2,INT_MAX/2},
+                                      {3,INT_MAX/3},
+                                      {5, INT_MAX/5}};
+        
+        int val = 1, fact = 1;
+        int multiplier, upperLimit;
+        
+        pq.push({val, fact});
+        
+        for(int i = 0; i < n; ++i){
+            vector<int> t = pq.top(); pq.pop();
+            val = t[0];
+            fact = t[1];
+            // cout << "val: " << val << ", fact: " << fact << endl;
+            
+            for(int i = 0; i < primes.size(); ++i){
+                multiplier = primes[i][0];
+                upperLimit = primes[i][1];
+                /*
+                consider the number 6 = 2*3 = 3*2,
+                if can be formed of (val=2)*(multiplier=3) or
+                (val=3)*(multiplier=2),
+                so there is a duplicate.
+                To avoid the duplicate, we add a restriction that
+                current multiplier should be >= fact.
+                e.g. when val's last fact is 3, we won't multiply it by 2
+                
+                note: it works like the ascending permutation of prime numbers
+                */
+                if(multiplier >= fact && val < upperLimit){
+                    pq.push({val*multiplier, multiplier});
+                }
+            }
+        }
+        
+        return val;
+    }
+};
