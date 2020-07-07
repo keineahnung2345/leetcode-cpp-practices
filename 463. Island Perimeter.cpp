46066cc0ecef4910374cbe8deb32807205336da1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-07 18:17:06 +0800

    math

--- 463. Island Perimeter.cpp
+++ 463. Island Perimeter.cpp
@@ -34,3 +34,36 @@ public:
         return ans;
     }
 };
+
+//math
+//https://leetcode.com/problems/island-perimeter/discuss/95001/clear-and-easy-java-solution
+//Runtime: 228 ms, faster than 29.18% of C++ online submissions for Island Perimeter.
+//Memory Usage: 96 MB, less than 97.84% of C++ online submissions for Island Perimeter.
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int m = grid.size();
+        if(m == 0) return 0;
+        int n = grid[0].size();
+        if(n == 0) return 0;
+        
+        int islands = 0;
+        int sharedEdges = 0;
+            
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j]){
+                    ++islands;
+                    /*
+                    count shared edges, 
+                    note that we only look down and right
+                    */
+                    if(i+1 < m && grid[i+1][j]) ++sharedEdges;
+                    if(j+1 < n && grid[i][j+1]) ++sharedEdges;
+                }
+            }
+        }
+        
+        return islands*4 - sharedEdges*2;
+    }
+};
