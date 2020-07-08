e0bc5de81d3520301e33bae73741dbceeb41662e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-08 19:01:57 +0800

    monotonic deque

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -210,3 +210,46 @@ public:
     }
 };
 
+//monotonic deque
+//Runtime: 12 ms, faster than 71.71% of C++ online submissions for Best Time to Buy and Sell Stock II.
+//Memory Usage: 13.5 MB, less than 5.41% of C++ online submissions for Best Time to Buy and Sell Stock II.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        
+        //just for trigger when the scanning ends
+        prices.push_back(0);
+        //increasing deque
+        deque<int> deq;
+        
+        int profit = 0;
+        
+        for(int i = 0; i < n+1; ++i){
+            /*
+            start to process deque's content
+            when we meet a smaller element
+            */
+            /*
+            we will process all elements from deque in one pass,
+            so don't need a while loop here
+            */
+            if(!deq.empty() && prices[i] < prices[deq.back()]){
+                /*
+                only when there are >= 2 choosable prices,
+                we can start a transaction
+                */
+                if(deq.size() >= 2){
+                    profit += prices[deq.back()] - prices[deq.front()];
+                }
+                deq.clear();
+            }
+            
+            deq.push_back(i);
+        }
+        
+        return profit;
+    }
+};
+
