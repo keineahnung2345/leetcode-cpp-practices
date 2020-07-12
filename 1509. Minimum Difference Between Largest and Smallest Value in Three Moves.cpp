feb1f084a7617563440e70e747988352e478c8e5
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-12 18:36:57 +0800

    add comment

--- 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -1,3 +1,4 @@
+//https://www.geeksforgeeks.org/minimize-the-maximum-minimum-difference-after-one-removal-from-array/?ref=rp
 //Runtime: 324 ms, faster than 33.33% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
 //Memory Usage: 35.4 MB, less than 100.00% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
 class Solution {
@@ -13,6 +14,12 @@ public:
         
         int minDiff = INT_MAX;
         
+        /*
+        From the problem:
+        Return the minimum difference between the largest and smallest value of nums after perfoming at most 3 moves.
+        that means we can remove 3 elements,
+        and following are the 4 ways of removing 3 elements
+        */
         for(int i = 0; i <= 3; ++i){
             minDiff = min(minDiff, nums[n-1-i] - nums[3-i]);
         }
