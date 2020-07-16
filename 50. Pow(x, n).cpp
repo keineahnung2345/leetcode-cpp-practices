bfc12f7ce782db480b228fb647b507be785339ce
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-16 18:10:16 +0800

    50. Pow(x, n).cpp

---
+++ 50. Pow(x, n).cpp
@@ -0,0 +1,8 @@
+//Runtime: 4 ms, faster than 23.29% of C++ online submissions for Pow(x, n).
+//Memory Usage: 6.1 MB, less than 20.88% of C++ online submissions for Pow(x, n).
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x, n);
+    }
+};
