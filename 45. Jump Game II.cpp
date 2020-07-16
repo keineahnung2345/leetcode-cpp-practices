bbb0a8828e222285ff5976611a99f065f2cedf43
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-16 21:24:38 +0800

    monotonic deque

--- 45. Jump Game II.cpp
+++ 45. Jump Game II.cpp
@@ -19,3 +19,46 @@ public:
         return dp[n-1];
     }
 };
+
+//monotonic deque
+//https://leetcode.com/problems/jump-game-ii/discuss/230778/C%2B%2B-deque-based-beats-99
+//Runtime: 20 ms, faster than 87.98% of C++ online submissions for Jump Game II.
+//Memory Usage: 13.6 MB, less than 18.24% of C++ online submissions for Jump Game II.
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        
+        if(n <= 1) return 0;
+        
+        /*
+        pair: (steps required to jump to i, farthest position reachable from i)
+        later element requires more steps, but it can reach farther position
+        */
+        deque<pair<int, int>> dq;
+        
+        dq.push_back({0, nums[0]});
+        
+        for(int i = 1; i < n; ++i){
+            while(!dq.empty() && dq.front().second < i){
+                //cannot reach current position
+                dq.pop_front();
+            }
+            
+            if(i+nums[i] > dq.back().second){
+                /*
+                if the farthest position(p) can be reached from i is
+                same as j(j < i),
+                then we can discard i,
+                that's because the steps required by j 
+                to reach p is definitely <= 
+                the steps required by i
+                */
+                dq.push_back({dq.front().first+1, i+nums[i]});
+            }
+        }
+        
+        //dq.front() has minimum steps required to reach n-1
+        return dq.front().first+1;
+    }
+};
