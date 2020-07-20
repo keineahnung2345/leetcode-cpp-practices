00496c220dc6d2b51f4aff0d373a5a74be3364ef
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-20 18:06:37 +0800

    +speed up, vector<bool> -> vector<int>, build ans on the fly

--- 51. N-Queens.cpp
+++ 51. N-Queens.cpp
@@ -54,3 +54,42 @@ public:
         return ans;
     }
 };
+
+//+speed up, vector<bool> -> vector<int>, build ans on the fly
+//https://leetcode.com/problems/n-queens/discuss/19808/Accepted-4ms-c%2B%2B-solution-use-backtracking-and-bitmask-easy-understand
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for N-Queens.
+//Memory Usage: 7.2 MB, less than 91.99% of C++ online submissions for N-Queens.
+class Solution {
+public:
+    int n;
+    
+    void backtrack(vector<int>& used, int r, 
+                   vector<string>& cur, vector<vector<string>>& ans){
+        if(r == n){
+            ans.push_back(cur);
+        }else{
+            for(int c = 0; c < n; ++c){
+                if(!used[c] && !used[n+r-c+n-1] && !used[n+2*n-1+r+c]){
+                    cur[r][c] = 'Q';
+                    used[c] = used[n+r-c+n-1] = used[n+2*n-1+r+c] = true;
+                    
+                    backtrack(used, r+1, cur, ans);
+                    
+                    used[c] = used[n+r-c+n-1] = used[n+2*n-1+r+c] = false;
+                    cur[r][c] = '.';
+                }
+            }
+        }
+    };
+    
+    vector<vector<string>> solveNQueens(int n) {
+        this->n = n;
+        vector<string> cur(n, string(n, '.'));
+        vector<vector<string>> ans;
+        vector<int> used(n + 2*n-1 + 2*n-1, false);
+        
+        backtrack(used, 0, cur, ans);
+        
+        return ans;
+    }
+};
