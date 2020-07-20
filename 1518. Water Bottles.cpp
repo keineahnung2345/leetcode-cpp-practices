8e27643975a0e40e13027b0977c72b4069390443
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-20 20:45:52 +0800

    O(logN)

--- 1518. Water Bottles.cpp
+++ 1518. Water Bottles.cpp
@@ -17,3 +17,24 @@ public:
         return ans;
     }
 };
+
+//O(logN)
+//https://leetcode.com/problems/water-bottles/discuss/743148/JavaPython-3-O(logN)-and-O(1)-codes-w-brief-analysis.
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Water Bottles.
+//Memory Usage: 6 MB, less than 100.00% of C++ online submissions for Water Bottles.
+//time: O(logN), space: O(1)
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        //drink
+        int ans = numBottles;
+        
+        //while we can exchange for a full bottle
+        while(numBottles >= numExchange){
+            //exchange and drink
+            ans += numBottles/numExchange;
+            numBottles = numBottles/numExchange + numBottles%numExchange;
+        }
+        return ans;
+    }
+};
