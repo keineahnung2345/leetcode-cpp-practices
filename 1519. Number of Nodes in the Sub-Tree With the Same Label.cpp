438622108a57402a1f4025db7ad4a6fdcaeaa390
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-20 21:47:54 +0800

    DFS, remove "visited"

--- 1519. Number of Nodes in the Sub-Tree With the Same Label.cpp
+++ 1519. Number of Nodes in the Sub-Tree With the Same Label.cpp
@@ -130,6 +130,7 @@ public:
 //https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/discuss/743133/JavaPython-3-DFS-w-brief-explanation-and-analysis.
 //Runtime: 1084 ms, faster than 80.42% of C++ online submissions for Number of Nodes in the Sub-Tree With the Same Label.
 //Memory Usage: 289.1 MB, less than 100.00% of C++ online submissions for Number of Nodes in the Sub-Tree With the Same Label.
+//time: O(N), space: O(N)
 class Solution {
 public:
     vector<int> dfs(vector<vector<int>>& adjList, string& labels, vector<bool>& visited, int cur, vector<int>& ans){
@@ -166,6 +167,51 @@ public:
         
         visited[0] = true;
         dfs(adjList, labels, visited, 0, ans);
+        
+        return ans;
+    }
+};
+
+//DFS, remove "visited"
+//https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label/discuss/743133/JavaPython-3-DFS-w-brief-explanation-and-analysis.
+//Runtime: 1476 ms, faster than 49.89% of C++ online submissions for Number of Nodes in the Sub-Tree With the Same Label.
+//Memory Usage: 278.8 MB, less than 100.00% of C++ online submissions for Number of Nodes in the Sub-Tree With the Same Label.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    vector<int> dfs(vector<vector<int>>& adjList, string& labels, int parent, int cur, vector<int>& ans){
+        vector<int> counter(26, 0);
+        char c = labels[cur]-'a';
+        counter[c] = 1;
+        
+        for(int nei : adjList[cur]){
+            if(nei != parent){
+                vector<int> subcounter = dfs(adjList, labels, cur, nei, ans);
+                for(int i = 0; i < counter.size(); ++i){
+                    counter[i] += subcounter[i];
+                }
+            }
+        }
+        //since we are using dfs, now counter's coverage is the subtree rooted at cur
+        ans[cur] = counter[c];
+        
+        return counter;
+    };
+    
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>> adjList(n);
+        vector<vector<int>> counter(n, vector<int>(26, -1));
+        
+        //bi-directional
+        for(vector<int>& edge : edges){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        
+        vector<int> ans(n, 0);
+        int parent = -1;
+        
+        dfs(adjList, labels, -1, 0, ans);
         
         return ans;
     }
