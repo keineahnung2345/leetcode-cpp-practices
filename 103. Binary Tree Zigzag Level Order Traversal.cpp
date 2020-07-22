d79c9e2dfafcfb350afcff77630cab9e3ea94523
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-22 22:04:20 +0800

    dfs

--- 103. Binary Tree Zigzag Level Order Traversal.cpp
+++ 103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -57,3 +57,55 @@ public:
         return ans;
     }
 };
+
+//dfs
+//https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/33815/My-accepted-JAVA-solution
+//Runtime: 8 ms, faster than 36.00% of C++ online submissions for Binary Tree Zigzag Level Order Traversal.
+//Memory Usage: 12.8 MB, less than 9.80% of C++ online submissions for Binary Tree Zigzag Level Order Traversal.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* cur, int level, vector<vector<int>>& ans){
+        if(!cur) return;
+        
+        if(ans.size() == level){
+            //in level 0, we totally have 1 level
+            ans.push_back(vector<int>());
+        }
+        
+        /*
+        the only difference is that 
+        we build the ans from left to right or
+        from right to left
+        */
+        if(!(level&1)){
+            //level is even
+            //in current level, we go from left to right
+            ans[level].push_back(cur->val);
+        }else{
+            ans[level].insert(ans[level].begin(), cur->val);
+        }
+        
+        //we still visit the tree from left to right
+        dfs(cur->left, level+1, ans);
+        dfs(cur->right, level+1, ans);
+    };
+    
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        dfs(root, 0, ans);
+        
+        return ans;
+    }
+};
