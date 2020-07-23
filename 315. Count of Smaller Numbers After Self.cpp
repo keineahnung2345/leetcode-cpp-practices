8217a2a5221ef303350eca614ba36ea391a18b35
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-23 21:08:24 +0800

    Binary Indexed Tree

--- 315. Count of Smaller Numbers After Self.cpp
+++ 315. Count of Smaller Numbers After Self.cpp
@@ -351,3 +351,71 @@ public:
         return ans;
     }
 };
+
+//Binary Indexed Tree
+//https://leetcode.com/problems/count-of-smaller-numbers-after-self/discuss/76674/3-Ways-(Segment-Tree-Binary-Indexed-Tree-Merge-Sort)-clean-Java-code
+//Runtime: 16 ms, faster than 96.71% of C++ online submissions for Count of Smaller Numbers After Self.
+//Memory Usage: 10.1 MB, less than 97.84% of C++ online submissions for Count of Smaller Numbers After Self.
+class BIT{
+public:
+    int n;
+    vector<int> tree;
+    
+    BIT(int n){
+        //the index: 0 is meaningless
+        //the indices: [1,n] are meaningful
+        //we can retrieve count of values from the tree
+        this->n = n;
+        tree = vector<int>(n+1);
+    }
+    
+    void increase(int i){
+        while(i <= n){
+            ++tree[i];
+            //left child is covered by its parent
+            //go to the upper node to its right
+            i += i&-i;
+        }
+    }
+    
+    int get(int i){
+        //get count of numbers in [1, i]
+        int count = 0;
+        
+        while(i > 0){
+            count += tree[i];
+            //go to the upper node just to its left
+            i -= i&-i;
+        }
+        
+        return count;
+    }
+};
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        if(n == 0) return ans;
+        
+        /*
+        suppose nums's range is [a, b],
+        convert it to [1, b-a+1]
+        */
+        int minv = *min_element(nums.begin(), nums.end());
+        transform(nums.begin(), nums.end(), nums.begin(),
+                  [&minv](int num){return num - minv + 1;});
+        
+        int maxv = *max_element(nums.begin(), nums.end());
+        
+        BIT* bit = new BIT(maxv);
+        
+        for(int i = n-1; i >= 0; --i){
+            ans[i] = bit->get(nums[i]-1);
+            bit->increase(nums[i]);
+        }
+        
+        return ans;
+    }
+};
