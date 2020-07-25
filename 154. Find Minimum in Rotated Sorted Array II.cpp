a83060c0c059d6c9ed4f0c4106b3b4ce2e80ad14
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-25 21:44:14 +0800

    Binary search, find left boundary

--- 154. Find Minimum in Rotated Sorted Array II.cpp
+++ 154. Find Minimum in Rotated Sorted Array II.cpp
@@ -58,3 +58,61 @@ public:
         return (res == -1) ? nums[0] : res;
     }
 };
+
+//Binary search, find left boundary
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/discuss/48808/My-pretty-simple-code-to-solve-it
+//Runtime: 12 ms, faster than 71.51% of C++ online submissions for Find Minimum in Rotated Sorted Array II.
+//Memory Usage: 12.3 MB, less than 74.40% of C++ online submissions for Find Minimum in Rotated Sorted Array II.
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        /*
+        choose nums[n-1] as pivot, 
+        so the search range is [0, n-2]
+        */
+        int l = 0, r = n-1-1;
+        int mid;
+        
+        //find left boundary
+        while(l <= r){
+            mid = (l+r) >> 1;
+            // cout << "look at [" << l << ", " << r << "] - " << mid << ", pivot: " << nums[r+1] << endl;
+            
+            if(nums[mid] < nums[r+1]){
+                /*
+                nums[mid:r+1] is increasing,
+                but we want to find inflection point,
+                so next we look at the left part: nums[l:mid]
+                (nums[mid] is implicitly considered since it's our next pivot)
+                */
+                r = mid-1;
+            }else if(nums[mid] > nums[r+1]){
+                /*
+                for the range nums[mid:r+1],
+                the head is larger than the tail,
+                the inflection point must be in this range,
+                so next we look at the right part: nums[mid+1:r+1]
+                */
+                l = mid+1;
+            }else{
+                //nums[mid] == pivot
+                /*
+                since we need to find left boundary,
+                it's safe to shrink right boundary
+                */
+                
+                //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/discuss/48808/My-pretty-simple-code-to-solve-it/48840
+                if (r-1 >= 0 && nums[r-1] > nums[r]) {
+                    l = r;
+                    break;
+                }
+                --r;
+            }
+        }
+        // cout << "look at [" << l << ", " << r << "]" << endl;
+        // cout << "==============" << endl;
+        
+        return nums[l];
+    }
+};
