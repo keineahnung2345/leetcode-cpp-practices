8de9cc9b99cb36aeb92dc6ad039c2142da727201
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-28 20:44:50 +0800

    char state

--- 1529. Bulb Switcher IV.cpp
+++ 1529. Bulb Switcher IV.cpp
@@ -19,3 +19,28 @@ public:
         return count;
     }
 };
+
+//we don't need an array for state, because the initial state for all positions are the same
+//https://leetcode.com/problems/bulb-switcher-iv/discuss/755939/C%2B%2B-Python-Java%3A-Readable-easy-code-with-explanation-and-code-comments
+//Runtime: 48 ms, faster than 81.67% of C++ online submissions for Bulb Switcher IV.
+//Memory Usage: 9.4 MB, less than 100.00% of C++ online submissions for Bulb Switcher IV.
+class Solution {
+public:
+    int minFlips(string target) {
+        int n = target.size();
+        
+        char state = '0';
+        int count = 0;
+        bool flip = false;
+        
+        for(int i = 0; i < n; ++i){
+            if(state != target[i]){
+                ++count;
+                flip = !flip;
+                state = (state == '0') ? '1' : '0';
+            }
+        }
+        
+        return count;
+    }
+};
