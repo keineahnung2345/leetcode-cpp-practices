e8c5ac28d52c7a90c19ecbcf0076bb3bb4c42459
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-29 21:10:29 +0800

    dfs, use visited array

--- 1530. Number of Good Leaf Nodes Pairs.cpp
+++ 1530. Number of Good Leaf Nodes Pairs.cpp
@@ -1,3 +1,57 @@
+//only go up or bottom-right, this cannot work
+//WA
+//47 / 113 test cases passed.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> leaves;
+    unordered_map<TreeNode*, TreeNode*> parents;
+    int ans;
+    
+    void buildParents(TreeNode* node, TreeNode* parent){
+        parents[node] = parent;
+        if(!node->left && !node->right) leaves.push_back(node);
+        if(node->left) buildParents(node->left, node);
+        if(node->right) buildParents(node->right, node);
+    };
+    
+    void findGoodPairs(TreeNode* node, TreeNode* last, int distance){
+        //either go up or go bottom-right
+        if(distance < 0) return;
+        if(last && !node->left && !node->right) ++ans;
+        
+        if(parents[node] && parents[node] != last){
+            findGoodPairs(parents[node], node, distance-1);
+        }
+        if(node->right && node->right != last){
+            findGoodPairs(node->right, node, distance-1);
+        }
+    }
+    
+    int countPairs(TreeNode* root, int distance) {
+        buildParents(root, nullptr);
+        
+        ans = 0;
+        
+        for(TreeNode* leaf : leaves){
+            findGoodPairs(leaf, nullptr, distance);
+        }
+        
+        return ans;
+    }
+};
+
+//dfs, use visited array
 //Runtime: 680 ms, faster than 7.28% of C++ online submissions for Number of Good Leaf Nodes Pairs.
 //Memory Usage: 92.3 MB, less than 6.17% of C++ online submissions for Number of Good Leaf Nodes Pairs.
 /**
