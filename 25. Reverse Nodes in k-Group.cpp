fde0d3bbf458c44d17604090fd3024f3f58e413f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-07-31 21:06:04 +0800

    recursion

--- 25. Reverse Nodes in k-Group.cpp
+++ 25. Reverse Nodes in k-Group.cpp
@@ -60,3 +60,70 @@ public:
         return tail;
     }
 };
+
+//recursion
+//https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11423/Short-but-recursive-Java-code-with-comments
+//Runtime: 24 ms, faster than 26.27% of C++ online submissions for Reverse Nodes in k-Group.
+//Memory Usage: 10.1 MB, less than 77.96% of C++ online submissions for Reverse Nodes in k-Group.
+//space: O(n/k) used by recursion stack
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* cur = head;
+        
+        int steps = 0;
+        
+        for(; steps < k && cur; ++steps){
+            cur = cur->next;
+        }
+        
+        //list's length < k, don't need to reverse
+        if(steps < k) return head;
+        
+        //now cur is the head of next group, reverse that group first
+        cur = reverseKGroup(cur, k);
+        
+        /*
+        123456, k = 3
+        assume the later group is already reversed: 123654
+        head: 1, cur: 6
+        "head" is the head of list to be reversed, 
+        and it should be connected to the head of reversed list,
+        so we set head->next to "cur"
+        tmp: 2, head->next: 6,
+        and later do the following:
+        cur: 1, head: 2
+        so that cur is still the head of reversed list and
+        "head" is still the head of the list to be reversed
+        
+        we do this for k times,
+        tmp: 3, head->next: 1, cur: 2, head: 3
+        tmp: nullptr, head->next: 2, cur: 3, head: nullptr
+        
+        to summary, in each iteration,
+        we cut the head of forward list and append it to the head of reversed list,
+        and this process is done for k times,
+        meaning cutting for k times and appending for k times
+        */
+        for(int i = 0; i < k; ++i){
+            //"head": head of the list to be reversed
+            ListNode* tmp = head->next;
+            //cur: head of reversed list
+            head->next = cur;
+            cur = head;
+            head = tmp;
+        }
+        
+        return cur;
+    }
+};
