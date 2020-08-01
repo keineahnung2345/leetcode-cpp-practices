fc81348d8704945350d422b077d70fa9253b797f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-01 23:16:24 +0800

    math

--- 780. Reaching Points.cpp
+++ 780. Reaching Points.cpp
@@ -23,3 +23,26 @@ public:
         return backtrack(sx, sy);
     }
 };
+
+//https://leetcode.com/problems/reaching-points/discuss/114856/JavaC%2B%2BPython-Modulo-from-the-End
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Reaching Points.
+//Memory Usage: 5.9 MB, less than 50.00% of C++ online submissions for Reaching Points.
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx > sx && ty > sy){
+            /*
+            (x,y) -> (x,x+y) or (x+y,x)
+            so the larger one in {tx, ty} must be 
+            the one increased from last step,
+            to restore to the previous state,
+            we reduce the addend from it
+            */
+            if(tx > ty) tx %= ty;
+            else ty %= tx;
+        }
+        
+        return tx == sx && ty >= sy && (ty-sy)%sx == 0 ||
+            ty == sy && tx >= sx && (tx-sx)%sy == 0;
+    }
+};
