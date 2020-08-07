0bc862ae7d378d7db3ee400920256e9c80073202
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-07 22:11:14 +0800

    one pass

--- 1535. Find the Winner of an Array Game.cpp
+++ 1535. Find the Winner of an Array Game.cpp
@@ -24,3 +24,30 @@ public:
         return max;
     }
 };
+
+//https://leetcode.com/problems/find-the-winner-of-an-array-game/discuss/768007/JavaC%2B%2BPython-One-Pass-O(1)-Space
+//Runtime: 268 ms, faster than 60.00% of C++ online submissions for Find the Winner of an Array Game.
+//Memory Usage: 63.1 MB, less than 100.00% of C++ online submissions for Find the Winner of an Array Game.
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        if(k >= n) return *max_element(arr.begin(), arr.end());
+        
+        int winner = arr[0], win_count = 0;
+        for(int i = 1; i < n && win_count < k; ++i){
+            if(winner < arr[i]){
+                winner = arr[i];
+                win_count = 0;
+            }
+            /*
+            either the winner remains the same or changes,
+            we need to increase win_count!!
+            */
+            ++win_count;
+        }
+        
+        return winner;
+    }
+};
