c9f0fe656e51ac4c857c272e696e60e8fc1d9f6e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-09 18:28:16 +0800

    stack

--- 1544. Make The String Great.cpp
+++ 1544. Make The String Great.cpp
@@ -1,3 +1,4 @@
+//Brute force
 //Runtime: 4 ms, faster than 83.33% of C++ online submissions for Make The String Great.
 //Memory Usage: 6.2 MB, less than 83.33% of C++ online submissions for Make The String Great.
 class Solution {
@@ -16,5 +17,33 @@ public:
         }
         
         return s;
+    }
+};
+
+//stack
+//https://leetcode.com/problems/make-the-string-great/discuss/781009/Java-Simple-Solution-using-Stack-Explained
+//Runtime: 4 ms, faster than 83.33% of C++ online submissions for Make The String Great.
+//Memory Usage: 6.8 MB, less than 33.33% of C++ online submissions for Make The String Great.
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> stk;
+        int n = s.size();
+        
+        for(int i = 0; i < n; ++i){
+            if(!stk.empty() && abs(stk.top()-s[i]) == 32){
+                stk.pop();
+            }else{
+                stk.push(s[i]);
+            }
+        }
+        
+        string ans(stk.size(), '#');
+        
+        for(int i = stk.size()-1; i >= 0; --i){
+            ans[i] = stk.top(); stk.pop();
+        }
+        
+        return ans;
     }
 };
