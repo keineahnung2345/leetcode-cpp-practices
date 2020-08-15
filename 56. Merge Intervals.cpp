effcd942248018f1e7d49a5070cfb85a9bdb80ee
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-15 23:25:14 +0800

    update greedy approach

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -24,25 +24,26 @@ public:
     }
 };
 
-//Approach 2: Sorting
+//Approach 2: Sorting, Greedy
 //the above method removes merged intervals, this method add unmerged intervals into ans
-//Runtime: 32 ms, faster than 29.99% of C++ online submissions for Merge Intervals.
-//Memory Usage: 10 MB, less than 100.00% of C++ online submissions for Merge Intervals.
+//Runtime: 52 ms, faster than 58.81% of C++ online submissions for Merge Intervals.
+//Memory Usage: 14.3 MB, less than 84.61% of C++ online submissions for Merge Intervals.
 //time: O(NlogN), space: O(1) if it's in-place sorting or O(N)
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if(intervals.size() < 2) return intervals;
         
+        //sort by start point
         sort(intervals.begin(), intervals.end());
         
         vector<vector<int>> ans;
         
         for(vector<int>& interval : intervals){
-            if(ans.empty() || ans[ans.size()-1][1] < interval[0]){
+            if(ans.empty() || ans.back()[1] < interval[0]){
                 ans.push_back(interval);
             }else{
-                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], interval[1]);
+                ans[ans.size()-1][1] = max(ans.back()[1], interval[1]);
             }
         }
         
