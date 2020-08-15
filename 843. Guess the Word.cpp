d5700e2933812ae8c2943d357968cd80438fc292
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-15 17:53:32 +0800

    843. Guess the Word.cpp

---
+++ 843. Guess the Word.cpp
@@ -0,0 +1,40 @@
+//Solution 2.1: Shuffle the Wordlist
+//https://leetcode.com/problems/guess-the-word/discuss/133862/Random-Guess-and-Minimax-Guess-with-Comparison
+//WA
+//1 / 6 test cases passed.
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    int match(string& a, string& b){
+        int diff = 0;
+        
+        for(int i = 0; i < a.size(); ++i){
+            if(a[i] != b[i]) ++diff;
+        }
+        
+        return diff;
+    };
+    
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        random_shuffle(wordlist.begin(), wordlist.end());
+        
+        for(int i = 0; i < 10 && !wordlist.empty(); ++i){
+            int diff = master.guess(wordlist[0]);
+            //only retain the words who has "diff" different char with the guessed word
+            vector<string> tmp;
+            copy_if(wordlist.begin(), wordlist.end(), 
+                    back_inserter(tmp), 
+                    [&diff, &wordlist, this](string& w){
+                        return match(wordlist[0], w) == diff;} 
+                   );
+            swap(wordlist, tmp);
+        }
+    }
+};
