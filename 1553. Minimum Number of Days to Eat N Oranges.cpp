d54abe4d8f74feba6bf42004d47fdf2014f7db5a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-16 12:27:36 +0800

    Greedy + DFS + memo

--- 1553. Minimum Number of Days to Eat N Oranges.cpp
+++ 1553. Minimum Number of Days to Eat N Oranges.cpp
@@ -26,6 +26,36 @@ public:
     }
 };
 
+//Greedy + DFS + memo
+//https://leetcode.com/problems/minimum-number-of-days-to-eat-n-oranges/discuss/794162/C%2B%2B-5-lines
+//Runtime: 28 ms, faster than 42.86% of C++ online submissions for Minimum Number of Days to Eat N Oranges.
+//Memory Usage: 10 MB, less than 57.14% of C++ online submissions for Minimum Number of Days to Eat N Oranges.
+class Solution {
+public:
+    unordered_map<int, int> memo;
+    
+    int dfs(int n){
+        if(memo.find(n) != memo.end()){
+            return memo[n];
+        }
+        
+        //eat one orange for n%2 days and then eat n/2 oranges
+        //eat one orange for n%3 days and then eat n*2/3 oranges
+        int days = min(n%2+dfs(n/2), n%3+dfs(n/3));
+        
+        // cout << n << ", " << days << endl;
+        return memo[n] = days+1;
+    };
+    
+    int minDays(int n) {
+        memo[0] = 0;
+        //this is also base case, o.w. memo[1] will be calculated as 2!
+        memo[1] = 1;
+        return dfs(n);
+    }
+};
+
+
 //BFS
 //Runtime: 140 ms, faster than 28.57% of C++ online submissions for Minimum Number of Days to Eat N Oranges.
 //Memory Usage: 22.5 MB, less than 14.29% of C++ online submissions for Minimum Number of Days to Eat N Oranges.
