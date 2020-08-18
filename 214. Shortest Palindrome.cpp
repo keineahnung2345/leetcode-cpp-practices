917084fc7f15b1df1e462e2761bda67c3ee6915d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-18 21:05:41 +0800

    Approach #3 KMP [Accepted]

--- 214. Shortest Palindrome.cpp
+++ 214. Shortest Palindrome.cpp
@@ -82,3 +82,53 @@ public:
         return remain_rev + shortestPalindrome(s.substr(0, i)) + remain;
     }
 };
+
+//Approach #3 KMP [Accepted]
+//Runtime: 8 ms, faster than 96.25% of C++ online submissions for Shortest Palindrome.
+//Memory Usage: 8.2 MB, less than 19.45% of C++ online submissions for Shortest Palindrome.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        string srev(s.rbegin(), s.rend());
+        string snew = s + "#" + srev;
+        int nnew = snew.size();
+        vector<int> lps(nnew, 0);
+        
+        // cout << "snew: " << snew << endl;
+        for(int i = 1, len = 0; i < nnew; /*no i++ here!*/){
+            // cout << "i: " << i << " ";
+            if(snew[i] == snew[len]){
+                // cout << i << " and " << len << " match" << endl;
+                ++len;
+                lps[i] = len;
+                ++i;
+            }else{
+                if(len > 0){
+                    //fallback
+                    // cout << "fallback from " << len << " to " << lps[len-1] << endl;
+                    len = lps[len-1];
+                }else{
+                    // cout << "move i forward" << endl;
+                    lps[i] = 0;
+                    ++i;
+                }
+            }
+        }
+        
+        // for(int i = 0; i < nnew; ++i){
+        //     cout << "lps[" << i << "]: " << lps[i] << endl; 
+        // }
+        
+        /*
+        for s: aacecaaa, (n = 8)
+        snew: aacecaaa#aaacecaa (nnew = 17)
+        "aacecaa" starting from 0 match "aacecaa" starting from 10,
+        i.e. for the srev "aaacecaa", it starts the match from its index 1.
+        Note that lps[nnew-1] is 7(the former 7 chars of s match the later 7 chars of srev).
+        So we only need one more char to make "s" a palindrome
+        */
+        return srev.substr(0, n - lps[nnew-1]) + s;
+    }
+};
