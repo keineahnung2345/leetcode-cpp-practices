2a4ecf3e8016fb40b7712981d08169f566dc0d36
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-18 14:12:53 +0800

    two pointer

--- 214. Shortest Palindrome.cpp
+++ 214. Shortest Palindrome.cpp
@@ -53,3 +53,32 @@ public:
         return "";
     }
 };
+
+//Approach #2 Two pointers and recursion [Accepted]
+//Runtime: 8 ms, faster than 96.25% of C++ online submissions for Shortest Palindrome.
+//Memory Usage: 7.2 MB, less than 93.85% of C++ online submissions for Shortest Palindrome.
+//time: O(N^2), space: O(N)
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        
+        int i = 0;
+        for(int j = n-1; j >= 0; --j){
+            if(s[i] == s[j]){
+                ++i;
+            }
+        }
+        
+        //there are "i(=n)" char matched
+        if(i == n) return s;
+        
+        //the unmatched part
+        string remain = s.substr(i);
+        string remain_rev = remain;
+        reverse(remain_rev.begin(), remain_rev.end());
+        
+        // cout << "i: " << i << ", " << remain_rev << " + ... + " << remain << endl;
+        return remain_rev + shortestPalindrome(s.substr(0, i)) + remain;
+    }
+};
