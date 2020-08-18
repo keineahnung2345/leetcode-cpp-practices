326c36a241f7a91ba337dab476648133a3a6d241
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-18 21:41:48 +0800

    Rolling hash, Rabin-Karp

--- 214. Shortest Palindrome.cpp
+++ 214. Shortest Palindrome.cpp
@@ -158,3 +158,35 @@ public:
         return srev.substr(0, n - lps[nnew-1]) + s;
     }
 };
+
+//Rolling hash, Rabin-Karp
+//https://leetcode.com/problems/shortest-palindrome/discuss/60153/8-line-O(n)-method-using-Rabin-Karp-rolling-hash
+//Runtime: 8 ms, faster than 96.25% of C++ online submissions for Shortest Palindrome.
+//Memory Usage: 7.1 MB, less than 98.13% of C++ online submissions for Shortest Palindrome.
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        
+        int MOD = 1e9+7;
+        int len = 0;
+        //all variable used in multiplication should be declared as "long long"
+        long long p;
+        long long B = 29;
+        long long hash1 = 0, hash2 = 0;
+        
+        for(int i = 0, p = 1L; i < n; ++i, p = (p * B) % MOD){
+            //all variable used in multiplication should be declared as "long long"
+            long long d = s[i]-'a';
+            hash1 = (hash1 * B + d) % MOD;
+            hash2 = (hash2 + d * p) % MOD;
+            //the length of palindrome
+            if(hash1 == hash2) len = i+1;
+        }
+        
+        string prepend = s.substr(len);
+        reverse(prepend.begin(), prepend.end());
+        
+        return prepend + s; 
+    }
+};
