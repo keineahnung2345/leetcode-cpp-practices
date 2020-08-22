da07701998793e831e1cf41a31b3245342d76086
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-22 13:08:39 +0800

    99. Recover Binary Search Tree.cpp

---
+++ 99. Recover Binary Search Tree.cpp
@@ -0,0 +1,58 @@
+//Runtime: 56 ms, faster than 20.38% of C++ online submissions for Recover Binary Search Tree.
+//Memory Usage: 55.2 MB, less than 5.06% of C++ online submissions for Recover Binary Search Tree.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> nodes;
+    vector<int> vals;
+    
+    void inorder(TreeNode* node){
+        if(!node) return;
+        inorder(node->left);
+        nodes.push_back(node);
+        vals.push_back(node->val);
+        inorder(node->right);
+    }
+    
+    void inorder_revise(TreeNode* node, int& order, vector<int>& indices_to_revise, vector<int>& vals_to_revise){
+        if(!node) return;
+        inorder_revise(node->left, order, indices_to_revise, vals_to_revise);
+        auto it = find(indices_to_revise.begin(), indices_to_revise.end(), order);
+        if(it != indices_to_revise.end()){
+            // cout << "revise " << node->val << " to " << vals_to_revise[it-indices_to_revise.begin()] << endl;
+            node->val = vals_to_revise[it-indices_to_revise.begin()];
+        }
+        ++order;
+        inorder_revise(node->right, order, indices_to_revise, vals_to_revise);
+    }
+    
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        
+        //the values in right order
+        sort(vals.begin(), vals.end());
+        
+        vector<int> indices_to_revise;
+        vector<int> vals_to_revise;
+        for(int i = 0; i < nodes.size(); ++i){
+            //the node's value isn't the value it should be
+            if(nodes[i]->val != vals[i]){
+                indices_to_revise.push_back(i);
+                vals_to_revise.push_back(vals[i]);
+            }
+        }
+        
+        int order = 0;
+        inorder_revise(root, order, indices_to_revise, vals_to_revise);
+    }
+};
