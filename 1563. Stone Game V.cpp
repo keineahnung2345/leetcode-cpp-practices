9906faeeb72c97dd6f83ce1a8ccbd4fa6a099690
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-23 22:08:21 +0800

    1563. Stone Game V.cpp

---
+++ 1563. Stone Game V.cpp
@@ -0,0 +1,51 @@
+//dfs + memorization, using 2d array rather than 2d vector for speed up
+//Runtime: 204 ms, faster than 37.50% of C++ online submissions for Stone Game V.
+//Memory Usage: 10.7 MB, less than 50.00% of C++ online submissions for Stone Game V.
+class Solution {
+public:
+    int memo[501][501];
+    
+    int stoneGameV(vector<int>& stoneValue, int start, int end) {
+        int score = 0;
+        if(start == -1) start = 0;
+        if(end == -1) end = stoneValue.size();
+        if(end-start <= 1) return score;
+        
+        if(memo[start][end] != -1){
+            return memo[start][end];
+        }
+        
+        int sum = accumulate(stoneValue.begin()+start, stoneValue.begin()+end, 0);
+        int leftsum = 0;
+
+        //left part: [0...i]
+        for(int i = start; i < end; ++i){
+            leftsum += stoneValue[i];
+            
+            if(leftsum > sum-leftsum){
+                //discard left
+                //remain [pos+1...]
+                // cout << "add " << sum-leftsum << endl;
+                score = max(score, sum-leftsum + stoneGameV(stoneValue, i+1, end));
+            }else if(leftsum < sum-leftsum){
+                //discard right
+                //remain [0...pos]
+                // cout << "add " << leftsum << endl;
+                score = max(score, leftsum + stoneGameV(stoneValue, start, i+1));
+            }else{
+                //leftsum == sum-leftsum
+                int leftres = stoneGameV(stoneValue, start, i+1);
+                int rightres = stoneGameV(stoneValue, i+1, end);
+                // cout << "add " << leftsum << endl;
+                score = max(score, leftsum + max(leftres, rightres));
+            }
+        }
+        
+        return memo[start][end] = score;
+    } 
+    
+    int stoneGameV(vector<int>& stoneValue) {
+        memset(&memo[0][0], -1, sizeof(memo));
+        return stoneGameV(stoneValue, -1, -1);
+    }
+};
