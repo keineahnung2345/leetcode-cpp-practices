a0b5df165121123c2fb30717c3f2a946bdd27196
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-23 22:02:26 +0800

    only care about start and end

--- 1560. Most Visited Sector in a Circular Track.cpp
+++ 1560. Most Visited Sector in a Circular Track.cpp
@@ -43,3 +43,39 @@ public:
         return ans;
     }
 };
+
+//only care about start and end
+//https://leetcode.com/problems/most-visited-sector-in-a-circular-track/discuss/806814/JavaC%2B%2BPython-From-Start-to-End
+//Runtime: 8 ms, faster than 66.67% of C++ online submissions for Most Visited Sector in a Circular Track.
+//Memory Usage: 11.4 MB, less than 50.00% of C++ online submissions for Most Visited Sector in a Circular Track.
+class Solution {
+public:
+    vector<int> mostVisited(int n, vector<int>& rounds) {
+        int start = rounds.front(), end = rounds.back();
+        vector<int> ans;
+        
+        if(start <= end){
+            ans = vector<int>(end-start+1);
+            iota(ans.begin(), ans.end(), start);
+            return ans;
+        }
+        
+        //start > end
+        //divide it to [start, n] and [1, end]
+        
+        //Method 1: slower
+        // for(int i = 1; i <= end; ++i){
+        //     ans.push_back(i);
+        // }
+        // for(int i = start; i <= n; ++i){
+        //     ans.push_back(i);
+        // }
+        
+        //Method 2: faster
+        ans = vector<int>(end + n-start+1);
+        iota(ans.begin(), ans.begin()+end, 1);
+        iota(ans.begin()+end, ans.end(), start);
+        
+        return ans;
+    }
+};
