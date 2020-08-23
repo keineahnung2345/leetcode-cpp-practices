f71e2edf86eab73e5c1a22db5949a465ea58de91
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-23 22:50:45 +0800

    prefix sum

--- 1563. Stone Game V.cpp
+++ 1563. Stone Game V.cpp
@@ -102,3 +102,68 @@ public:
         return stoneGameV(stoneValue, -1, -1);
     }
 };
+
+//dfs + memorization, using 2d array rather than unordered_map for speed up, prefix sum
+//https://leetcode.com/problems/stone-game-v/discuss/806723/C%2B%2B-Prefix-Sum-%2B-DP-(Memoization)
+//Runtime: 328 ms, faster than 37.50% of C++ online submissions for Stone Game V.
+//Memory Usage: 11.3 MB, less than 50.00% of C++ online submissions for Stone Game V.
+class Solution {
+public:
+    int memo[501][501];
+    vector<int> prefixSum;
+    
+    int stoneGameV(vector<int>& stoneValue, int start, int end) {
+        int score = 0;
+        if(end-start <= 1) return score;
+        
+        if(memo[start][end] != -1){
+            return memo[start][end];
+        }
+        
+        int leftsum, rightsum;
+
+        //left part: [start...i], right part: [i+1...end)
+        for(int i = start; i < end; ++i){
+            //[start...i]
+            //prefixSum[i+1]: [0...i]
+            //prefixSum[start]: [0...start-1]
+            leftsum = prefixSum[i+1] - prefixSum[start];
+            //[i+1...end)
+            //prefixSum[end]: [0...end)
+            //prefixSum[i+1]: [0...i]
+            rightsum = prefixSum[end] - prefixSum[i+1];
+            
+            if(leftsum > rightsum){
+                //discard left
+                //remain [pos+1...]
+                // cout << "add " << sum-leftsum << endl;
+                score = max(score, rightsum + stoneGameV(stoneValue, i+1, end));
+            }else if(leftsum < rightsum){
+                //discard right
+                //remain [0...pos]
+                // cout << "add " << leftsum << endl;
+                score = max(score, leftsum + stoneGameV(stoneValue, start, i+1));
+            }else{
+                //leftsum == rightsum
+                int leftres = stoneGameV(stoneValue, start, i+1);
+                int rightres = stoneGameV(stoneValue, i+1, end);
+                // cout << "add " << leftsum << endl;
+                score = max(score, leftsum + max(leftres, rightres));
+            }
+        }
+        
+        return memo[start][end] = score;
+    } 
+    
+    int stoneGameV(vector<int>& stoneValue) {
+        memset(&memo[0][0], -1, sizeof(memo));
+        
+        int n = stoneValue.size();
+        prefixSum = vector<int>(n+1, 0);
+        for(int i = 0; i < n; ++i){
+            prefixSum[i+1] = prefixSum[i] + stoneValue[i];
+        }
+        
+        return stoneGameV(stoneValue, 0, n);
+    }
+};
