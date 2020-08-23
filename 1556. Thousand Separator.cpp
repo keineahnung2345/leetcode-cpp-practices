355103439f84818a34fe6f9f97ca68862053ac4d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-23 21:14:07 +0800

    build res

--- 1556. Thousand Separator.cpp
+++ 1556. Thousand Separator.cpp
@@ -28,3 +28,28 @@ public:
         return ans;
     }
 };
+
+//https://leetcode.com/problems/thousand-separator/discuss/805704/C%2B%2B-Iterative
+//Runtime: 4 ms, faster than 20.00% of C++ online submissions for Thousand Separator.
+//Memory Usage: 5.9 MB, less than 60.00% of C++ online submissions for Thousand Separator.
+class Solution {
+public:
+    string thousandSeparator(int n) {
+        string s = to_string(n);
+        string res = "";
+        
+        for(int i = 0; i < s.size(); ++i){
+            /*
+            there are "s.size()-i" chars in s[i:],
+            we add "." before the digit such that 
+            there are 3's multiple digits behind it(including)
+            */
+            if(i > 0 && (s.size()-i)%3 == 0){
+                res += ".";
+            }
+            res += s[i];
+        }
+        
+        return res;
+    }
+};
