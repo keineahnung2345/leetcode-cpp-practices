8f6f5815faf3739b9c60f839241c137312b7ed61
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-26 20:37:17 +0800

    discussion's answer

--- 1163. Last Substring in Lexicographical Order.cpp
+++ 1163. Last Substring in Lexicographical Order.cpp
@@ -34,3 +34,42 @@ public:
         return candidates[candidates.size()-1];
     }
 };
+
+//https://leetcode.com/problems/last-substring-in-lexicographical-order/discuss/363662/Short-python-code-O(n)-time-and-O(1)-space-with-proof-and-visualization
+//Runtime: 104 ms, faster than 77.02% of C++ online submissions for Last Substring in Lexicographical Order.
+//Memory Usage: 14.4 MB, less than 34.68% of C++ online submissions for Last Substring in Lexicographical Order.
+class Solution {
+public:
+    string lastSubstring(string s) {
+        int i = 0; //the start index of final substr
+        int j = i+1; //the start index of the substr for comparison
+        int k = 0; //k = the length of final substr - 1
+        int n = s.size();
+        
+        while(j + k < n){
+            // cout << s.substr(i, k+1) << " " << s.substr(j, k+1) << endl;
+            if(s[i+k] == s[j+k]){
+                ++k;
+            }else if(s[i+k] < s[j+k]){
+                /*
+                all char in s[i+1...j-1] are smaller than s[j],
+                so we can safely skip them
+                */
+                i = j;
+                ++j;
+                k = 0;
+            }else{
+                //s[i+k] > s[j+k]
+                /*
+                because s[j...j+k] must be <= s[i],
+                (otherwise i will be updated by j),
+                so we can start from j+k+1
+                */
+                j = j+k+1;
+                k = 0;
+            }
+        }
+        
+        return s.substr(i);
+    }
+};
