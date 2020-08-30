466f02b0490ee5a08d0a8889af4b2238a01daab4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-30 21:23:40 +0800

    Union find + precompute prime numbers

--- 952. Largest Component Size by Common Factor.cpp
+++ 952. Largest Component Size by Common Factor.cpp
@@ -119,3 +119,80 @@ public:
         return ans;
     }
 };
+
+//Union find + precompute prime numbers
+//TLE
+//78 / 100 test cases passed.
+class DSU{
+public:
+    vector<int> parent;
+    
+    DSU(int n){
+        parent = vector<int>(n, 0);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
+    }
+    
+    void unite(int x, int y){
+        //merge the larger to the smaller
+        parent[find(y)] = find(x);
+    }
+};
+
+class Solution {
+public:
+    vector<bool> isprime;
+    
+    void SieveOfEratosthenes(int n){
+        for(int p=2; p*p<=n; p++){ 
+            if(isprime[p]){
+                for(int i=p*p; i<=n; i += p)
+                    isprime[i] = false;
+            }
+        }
+    };
+    
+    int largestComponentSize(vector<int>& A) {
+        int n = A.size();
+        
+        int max_ele = *max_element(A.begin(), A.end());
+        isprime = vector<bool>(max_ele+1, true);
+        SieveOfEratosthenes(max_ele);
+        
+        // cout << "max: " << max_ele << endl;
+        
+        unordered_set<int> primes;
+        for(int p = 2; p <= max_ele; ++p){
+            if(isprime[p]){
+                primes.insert(p);
+            }
+        }
+        
+        DSU dsu(max_ele+1);
+        
+        for(int p : primes){
+            for(int i = 0; i < n; ++i){
+                if(A[i]%p == 0){
+                    dsu.unite(p, A[i]);
+                }
+            }
+        }
+        
+        unordered_map<int, int> group2count;
+        int ans = 0;
+        
+        for(int i = 0; i < n; ++i){
+            ++group2count[dsu.find(A[i])];
+            ans = max(ans, group2count[dsu.find(A[i])]);
+        }
+        
+        return ans;
+    }
+};
