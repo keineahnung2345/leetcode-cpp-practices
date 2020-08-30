970fc290a33de3a616acd71dc8e249ad2e0b17ae
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-30 21:01:47 +0800

    union find

--- 952. Largest Component Size by Common Factor.cpp
+++ 952. Largest Component Size by Common Factor.cpp
@@ -1,3 +1,4 @@
+//Graph, BFS
 //TLE
 //71 / 100 test cases passed.
 class Solution {
@@ -54,6 +55,65 @@ public:
             }
             
             ans = max(ans, comp_size);
+        }
+        
+        return ans;
+    }
+};
+
+//Union find
+//TLE
+//74 / 100 test cases passed.
+class DSU{
+public:
+    vector<int> parent;
+    
+    DSU(int n){
+        parent = vector<int>(n, 0);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
+    }
+    
+    void unite(int x, int y){
+        //merge the larger to the smaller
+        parent[find(y)] = find(x);
+    }
+};
+
+class Solution {
+public:
+    int gcd(int x, int y){
+        if(x < y) swap(x, y);
+        if(y == 0) return x;
+        return gcd(y, x%y);
+    };
+    
+    int largestComponentSize(vector<int>& A) {
+        int n = A.size();
+        
+        DSU dsu(n);
+        
+        for(int i = 0; i < n; ++i){
+            for(int j = i+1; j < n; ++j){
+                if(gcd(A[i], A[j]) > 1){
+                    dsu.unite(i, j);
+                }
+            }
+        }
+        
+        unordered_map<int, int> group2count;
+        int ans = 0;
+        
+        for(int i = 0; i < n; ++i){
+            ++group2count[dsu.find(i)];
+            ans = max(ans, group2count[dsu.find(i)]);
         }
         
         return ans;
