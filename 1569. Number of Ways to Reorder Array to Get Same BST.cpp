99c406dbc316ca5fd69017fba1a73920e05a06ce
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-08-31 21:53:49 +0800

    1569. Number of Ways to Reorder Array to Get Same BST.cpp

---
+++ 1569. Number of Ways to Reorder Array to Get Same BST.cpp
@@ -0,0 +1,139 @@
+//Runtime: 128 ms, faster than 80.00% of C++ online submissions for Number of Ways to Reorder Array to Get Same BST.
+//Memory Usage: 28.5 MB, less than 100.00% of C++ online submissions for Number of Ways to Reorder Array to Get Same BST.
+class Node{
+public:
+    Node *left, *right;
+    int val;
+    
+    Node(int v) : val(v), left(nullptr), right(nullptr){};
+};
+
+class BST{
+public:
+    Node* root;
+    
+    void insert(int v, Node* node = nullptr){
+        if(root == nullptr){
+            root = new Node(v);
+        }else{
+            if(node == nullptr)
+                node = root;
+            if(v < node->val){
+                if(node->left == nullptr){
+                    // cout << node->val << " l-> " << v << endl;
+                    node->left = new Node(v);
+                }else{
+                    // cout << node->left->val << " l-> " << v << endl;
+                    insert(v, node->left);
+                }
+            }else{
+                if(node->right == nullptr){
+                    // cout << node->val << " r-> " << v << endl;
+                    node->right = new Node(v);
+                }else{
+                    // cout << node->right->val << " r-> " << v << endl;
+                    insert(v, node->right);
+                }
+            }
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int MOD = 1e9+7;
+    unordered_map<Node*, int> counter;
+    
+    int dfs(Node* node){
+        if(node == nullptr) return 0;
+        counter[node] = dfs(node->left) + dfs(node->right) + 1;
+        return counter[node];
+    };
+
+    // Returns factorial of n 
+    int fact(int n) 
+    { 
+        long long res = 1; 
+        for (int i = 2; i <= n; i++) 
+            res = (res * i) % MOD;
+        return res; 
+    };
+    
+    int power(int x, unsigned int y){ 
+        if (y == 0) 
+            return 1; 
+        long long p = power(x, y/2) % MOD; 
+        p = (p * p) % MOD; 
+
+        return (y%2 == 0)? p : (x * p) % MOD; 
+    };
+
+    // Function to return gcd of a and b 
+    int gcd(int a, int b){ 
+        if (a == 0) 
+            return b; 
+        return gcd(b%a, a); 
+    };
+
+    // Function to find modular inverse of a under modulo m 
+    // Assumption: m is prime 
+    int modInverse(int a){ 
+        int g = gcd(a, MOD); 
+        if (g != 1){
+            //Fermat's little theorem only works only when m is prime!
+            return -1;
+        }else{ 
+            // If a and m are relatively prime, then modulo inverse 
+            // is a^(m-2) mod m 
+            /*
+            From Fermat's little theorem,
+            when a is not divisible by p,
+            a^(p-1) mod m = 1
+            here we choose p as m,
+            so a^(m-1) mod m = 1
+            this is equal to saying that
+            a * a^(m-2) mod m = 1
+            so we can say that a^(m-2) mod m is the inverse of a
+            */
+            return power(a, MOD-2);
+        } 
+    };
+  
+    int nCr(int n, int r) 
+    { 
+        long long fr_inverse = modInverse(fact(r));
+        long long fnr_inverse = modInverse(fact(n - r));
+        return ((fact(n) * fr_inverse) % MOD * fnr_inverse) % MOD; 
+    };
+    
+    int countInsertionOrderings(Node* node) {
+        if(!node) return 1;
+        
+       int m = counter[node->left];
+       int n = counter[node->right];
+       
+        long long a = nCr(m + n, n);
+        long long b = countInsertionOrderings(node->left);
+        long long c = countInsertionOrderings(node->right);
+        
+        return (((a*b) % MOD) * c) % MOD;
+    }
+    
+    int numOfWays(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        
+        BST* bst = new BST();
+        
+        for(int i = 0; i < n; ++i){
+            bst->insert(nums[i]);
+        }
+        
+        Node* node = bst->root;
+        
+        dfs(bst->root);
+        
+        return countInsertionOrderings(bst->root) - 1;
+    }
+};
