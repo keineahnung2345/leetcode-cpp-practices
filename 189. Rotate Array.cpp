e8be470c36b567f492c753678593ba0ce8a4a1b7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-02 23:39:26 +0800

    Approach 3: Using Cyclic Replacements

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -43,3 +43,30 @@ public:
         }
     }
 };
+
+//Approach 3: Using Cyclic Replacements
+//Runtime: 8 ms, faster than 88.24% of C++ online submissions for Rotate Array.
+//Memory Usage: 9.9 MB, less than 70.95% of C++ online submissions for Rotate Array.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        
+        int count = 0;
+        
+        for(int start = 0; count < n; ++start){
+            int cur = start;
+            //temporarily store the value k position before nums[next]
+            int prev = nums[cur];
+            do{
+                int next = (cur+k) % n;
+                // cout << "nums[" << next << "]: " << prev << endl;
+                swap(nums[next], prev);
+                cur = next;
+                ++count;
+            }while(cur != start);
+        }
+    }
+};
