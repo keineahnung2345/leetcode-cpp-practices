58ed165bb4ea35e1c3573d906f4b887363a9b14e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-02 23:44:34 +0800

    Approach 4: Using Reverse

--- 189. Rotate Array.cpp
+++ 189. Rotate Array.cpp
@@ -70,3 +70,16 @@ public:
         }
     }
 };
+
+//Approach 4: Using Reverse
+//Runtime: 8 ms, faster than 88.24% of C++ online submissions for Rotate Array.
+//Memory Usage: 10.1 MB, less than 37.73% of C++ online submissions for Rotate Array.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
