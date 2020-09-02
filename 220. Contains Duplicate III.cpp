96362d1813585b947cc2027e306176637a1756ea
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-02 21:47:08 +0800

    bucket

--- 220. Contains Duplicate III.cpp
+++ 220. Contains Duplicate III.cpp
@@ -17,3 +17,51 @@ public:
         return false;
     }
 };
+
+//Bucket
+//https://leetcode.com/problems/contains-duplicate-iii/discuss/61645/AC-O(N)-solution-in-Java-using-buckets-with-explanation
+//Runtime: 28 ms, faster than 79.23% of C++ online submissions for Contains Duplicate III.
+//Memory Usage: 11.4 MB, less than 36.26% of C++ online submissions for Contains Duplicate III.
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        //abs(i-j) = k < 1: not reasonable
+        //abs(nums[i]-nums[j]) = t < 0: not reasonable
+        if(k < 1 || t < 0) return false;
+        
+        unordered_map<long long, long long> buckets;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; ++i){
+            //since nums[i] could be negative, so we add INT_MIN to make it starts from 0
+            long long remappedNum = (long long)nums[i] - INT_MIN;
+            long long bucket = remappedNum / ((long long)t+1);
+            
+            //find a number in [num-t,num+t] in the range [i-k,i+k]
+            if(buckets.find(bucket) != buckets.end() || //duplicate key: valid j found!
+               buckets.find(bucket-1) != buckets.end() && //find in previous bucket
+               remappedNum - buckets[bucket-1] <= t ||
+               buckets.find(bucket+1) != buckets.end() && //find in next bucket
+               buckets[bucket+1] - remappedNum <= t
+              ){
+                return true;
+            }
+            
+            if(buckets.size() >= k){
+                long long lastRemappedNum = (long long)nums[i-k] - INT_MIN;
+                long long lastBucket = lastRemappedNum / ((long long)t+1);
+                /*
+                buckets[lastBucket] must only contains nums[i-k],
+                if not, that means we already found a valid j,
+                at that situation, we are already returned
+                */
+                buckets.erase(lastBucket);
+            }
+            
+            //currently buckets[bucket] is empty
+            buckets[bucket] = remappedNum;
+        }
+        
+        return false;
+    }
+};
