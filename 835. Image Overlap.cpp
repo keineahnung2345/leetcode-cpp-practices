2b27e276752973914a72e20a4445e1d6da2bf041
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-06 19:06:41 +0800

    Linear Transformation

--- 835. Image Overlap.cpp
+++ 835. Image Overlap.cpp
@@ -1,6 +1,7 @@
 //Brute force
 //Runtime: 116 ms, faster than 53.82% of C++ online submissions for Image Overlap.
 //Memory Usage: 9.3 MB, less than 57.43% of C++ online submissions for Image Overlap.
+//time: O(N^4), space: O(1)
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
@@ -21,5 +22,52 @@ public:
         }
         
         return max_overlap;
+    }
+};
+
+//Approach 2: Linear Transformation
+//Runtime: 212 ms, faster than 26.11% of C++ online submissions for Image Overlap.
+//Memory Usage: 12.8 MB, less than 13.66% of C++ online submissions for Image Overlap.
+//time: O(N^4), space: O(N^2)
+class Solution {
+public:
+    struct pair_hash_int {
+        inline std::size_t operator()(const std::pair<int,int> & v) const {
+            return v.first*31+v.second;
+        }
+    };
+    
+    void non_zero_cells(vector<vector<int>>& mat,
+                       vector<pair<int, int>>& ones){
+        int n = mat.size();
+        
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(mat[i][j]){
+                    ones.push_back({i, j});
+                }
+            }
+        }
+    };
+    
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        vector<pair<int, int>> A_ones;
+        vector<pair<int, int>> B_ones;
+        
+        non_zero_cells(A, A_ones);
+        non_zero_cells(B, B_ones);
+        
+        int maxOverlaps = 0;
+        unordered_map<pair<int, int>, int, pair_hash_int> groupCount;
+        
+        for(pair<int, int>& a : A_ones){
+            for(pair<int, int>& b : B_ones){
+                pair<int, int> v = {b.first-a.first, b.second-a.second};
+                ++groupCount[v];
+                maxOverlaps = max(maxOverlaps, groupCount[v]);
+            }
+        }
+        
+        return maxOverlaps;
     }
 };
