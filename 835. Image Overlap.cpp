5c3926caa84f386f845d1a4c83b7e8241279f35e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-06 21:42:50 +0800

    Approach 3: Imagine Convolution

--- 835. Image Overlap.cpp
+++ 835. Image Overlap.cpp
@@ -71,3 +71,50 @@ public:
         return maxOverlaps;
     }
 };
+
+//Approach 3: Imagine Convolution
+//Runtime: 228 ms, faster than 25.30% of C++ online submissions for Image Overlap.
+//Memory Usage: 9.8 MB, less than 41.77% of C++ online submissions for Image Overlap.
+//time: O(N^4), space: O(N^2)
+class Solution {
+public:
+    int n;
+    
+    int convolute(vector<vector<int>>& A, vector<vector<int>>& kernel, 
+        int rshift, int cshift){
+        int res = 0;
+        
+        for(int r = 0; r < n; ++r){
+            for(int c = 0; c < n; ++c){
+                res += A[r][c] * kernel[r+rshift][c+cshift];
+            }
+        }
+        
+        return res;
+    };
+    
+    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+        n = A.size();
+        
+        //padding every side for n-1
+        //the original B is now at the center of B_padded
+        vector<vector<int>> B_padded(3*n-2, vector<int>(3*n-2, 0));
+        
+        for(int r = 0; r < n; ++r){
+            for(int c = 0; c < n; ++c){
+                B_padded[r+n-1][c+n-1] = B[r][c];
+            }
+        }
+        
+        int maxOverlaps = 0;
+        
+        for(int rshift = 0; rshift+n-1 < 3*n-2; ++rshift){
+            for(int cshift = 0; cshift+n-1 < 3*n-2; ++cshift){
+                maxOverlaps = max(maxOverlaps,
+                    convolute(A, B_padded, rshift, cshift));
+            }
+        }
+        
+        return maxOverlaps;
+    }
+};
