af439500237ef680368da7f675c8cce08e38ad6c
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-06 21:58:25 +0800

    cleaner

--- 1578. Minimum Deletion Cost to Avoid Repeating Letters.cpp
+++ 1578. Minimum Deletion Cost to Avoid Repeating Letters.cpp
@@ -23,3 +23,31 @@ public:
         return ans;
     }
 };
+
+//cleaner
+//https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/discuss/831588/JavaC%2B%2BPython-Straight-Forward
+//Runtime: 388 ms, faster than 33.33% of C++ online submissions for Minimum Deletion Cost to Avoid Repeating Letters.
+//Memory Usage: 95.5 MB, less than 33.33% of C++ online submissions for Minimum Deletion Cost to Avoid Repeating Letters.
+class Solution {
+public:
+    int minCost(string s, vector<int>& cost) {
+        int n = s.size();
+        int max_cost = 0, sum_cost = 0;
+        int res = 0;
+        
+        for(int i = 0; i < n; ++i){
+            if(i-1 >= 0 && s[i] != s[i-1]){
+                //end the old session and start a new one
+                res += sum_cost - max_cost;
+                sum_cost = max_cost = 0;
+            }
+            //in a session, update sum_cst and max_cost
+            sum_cost += cost[i];
+            max_cost = max(max_cost, cost[i]);
+        }
+        
+        res += sum_cost - max_cost;
+        
+        return res;
+    }
+};
