dad65b23eb046d0884312fd7ffe4f465aa04789b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-13 20:06:13 +0800

    two pass, O(N^2)

--- 1582. Special Positions in a Binary Matrix.cpp
+++ 1582. Special Positions in a Binary Matrix.cpp
@@ -38,3 +38,39 @@ public:
         return ans;
     }
 };
+
+//two pass, O(N^2)
+//Runtime: 44 ms, faster than 100.00% of C++ online submissions for Special Positions in a Binary Matrix.
+//Memory Usage: 13.2 MB, less than 33.33% of C++ online submissions for Special Positions in a Binary Matrix.
+//https://leetcode.com/problems/special-positions-in-a-binary-matrix/discuss/843949/C%2B%2B-2-passes
+//time: O(N^2)
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        
+        vector<int> rows(m), cols(n);
+        
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(mat[i][j] == 1){
+                    ++rows[i];
+                    ++cols[j];
+                }
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(mat[i][j] == 1 && rows[i] == 1 && cols[j] == 1){
+                    ++ans;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
