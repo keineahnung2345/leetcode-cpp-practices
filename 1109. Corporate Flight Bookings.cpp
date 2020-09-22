083f235a46ab6c1eae892ee2774cb7af86b94b8b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-22 22:52:04 +0800

    add time and space complexity

--- 1109. Corporate Flight Bookings.cpp
+++ 1109. Corporate Flight Bookings.cpp
@@ -1,6 +1,7 @@
 //line sweep
 //Runtime: 556 ms, faster than 28.20% of C++ online submissions for Corporate Flight Bookings.
 //Memory Usage: 67.8 MB, less than 83.30% of C++ online submissions for Corporate Flight Bookings.
+//time: O(N+bookings), space: O(N)
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
