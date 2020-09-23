a184c149a452c448f412947b411aacbeb65b72b1
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-23 18:41:09 +0800

    hashmap

--- 1590. Make Sum Divisible by P.cpp
+++ 1590. Make Sum Divisible by P.cpp
@@ -56,3 +56,41 @@ public:
         return -1;
     }
 };
+
+//hashmap
+//TLE
+//128 / 142 test cases passed.
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        
+        int k = accumulate(nums.begin(), nums.end(), 0LL) % p;
+        
+        if(k == 0) return 0;
+        
+        vector<int> lastidx(p, -1);
+        int ans = INT_MAX;
+        int runsum = 0;
+        
+        for(int i = 0; i < n; ++i){
+            /*
+            nums[0...i]: psum[i]
+            nums[0...lastidx]: psum[i]-k
+            (psum[i] - psum[lastidx]) % p == k
+            -> (psum[i]-k)%p = psum[lastidx]
+            */
+            runsum = (runsum+nums[i])%p;
+            if(lastidx[(runsum-k+p)%p] >= 0){
+                ans = min(ans, i - lastidx[(runsum-k+p)%p]);
+            }
+            if(lastidx[k] >= 0){
+                ans = min(ans, lastidx[k]+1);
+            }
+            lastidx[runsum%p] = i;
+            // cout << "lastidx[" << psum[i]%p << "] = " << i << endl;
+        }
+        
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
