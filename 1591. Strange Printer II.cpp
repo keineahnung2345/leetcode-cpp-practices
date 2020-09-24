e2c9ac366cfaeefe833e567f5f0eb0c4902df5c8
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-24 20:58:54 +0800

    topological sort

--- 1591. Strange Printer II.cpp
+++ 1591. Strange Printer II.cpp
@@ -145,3 +145,78 @@ public:
         return true;
     }
 };
+
+//topological sort
+//https://leetcode.com/problems/strange-printer-ii/discuss/854219/JavaTopological-Sort
+//Runtime: 500 ms, faster than 19.08% of C++ online submissions for Strange Printer II.
+//Memory Usage: 32.2 MB, less than 20.18% of C++ online submissions for Strange Printer II.
+class Solution {
+public:
+    bool isPrintable(vector<vector<int>>& targetGrid) {
+        //valid range: [1,60]
+        vector<unordered_set<int>> graph(61);
+        vector<unordered_set<int>> revgraph(61);
+        int m = targetGrid.size();
+        int n = targetGrid[0].size();
+        
+        for(int c = 1; c <= 60; ++c){
+            int left = INT_MAX, up = INT_MAX;
+            int right = INT_MIN, down = INT_MIN;
+            
+            for(int i = 0; i < m; ++i){
+                for(int j = 0; j < n; ++j){
+                    if(targetGrid[i][j] == c){
+                        left = min(left, j);
+                        right = max(right, j);
+                        up = min(up, i);
+                        down = max(down, i);
+                    }
+                }
+            }
+            
+            for(int i = up; i <= down; ++i){
+                for(int j = left; j <= right; ++j){
+                    if(targetGrid[i][j] != c){
+                        //need to paint targetGrid[i][j] before c
+                        graph[targetGrid[i][j]].insert(c);
+                        revgraph[c].insert(targetGrid[i][j]);
+                    }
+                }
+            }
+        }
+        
+        //topological sort
+        
+        //colors which have no dependencies, they should be painted first
+        queue<int> nodep; 
+        
+        vector<int> indegree(61);
+        
+        for(int c = 1; c <= 60; ++c){
+            indegree[c] = revgraph[c].size();
+        }
+        
+        for(int c = 1; c <= 60; ++c){
+            if(revgraph[c].size() == 0){
+                nodep.push(c);
+            }
+        }
+        
+        unordered_set<int> visited;
+        while(!nodep.empty()){
+            int c = nodep.front(); nodep.pop();
+            //if already visited
+            if(!visited.insert(c).second) continue;
+            
+            for(const int& nextc : graph[c]){
+                // cout << nextc << " : " << indegree[nextc] << " -> " << indegree[nextc]-1 << endl;
+                --indegree[nextc];
+                if(indegree[nextc] == 0){
+                    nodep.push(nextc);
+                }
+            }
+        }
+        
+        return visited.size() == 60;
+    }
+};
