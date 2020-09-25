5a7b615e762a67bbdc369927f8bd383d3bd7851b
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-25 23:50:24 +0800

    Approach 2: Visit by Row

--- 6. ZigZag Conversion.cpp
+++ 6. ZigZag Conversion.cpp
@@ -71,3 +71,34 @@ public:
         return ans;
     }
 };
+
+//Approach 2: Visit by Row
+//Runtime: 12 ms, faster than 99.63% of C++ online submissions for ZigZag Conversion.
+//Memory Usage: 8.6 MB, less than 58.45% of C++ online submissions for ZigZag Conversion.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows == 1) return s;
+        
+        int n = s.size();
+        int cycleLen = 2*(numRows - 1);
+        string ans;
+        
+        /*
+        first row: k*cycleLen
+        last row: k*cycleLen+numRows-1
+        internal rows: k*cycleLen+i or (k+1)*cycleLen-i
+        */
+        for(int i = 0; i < numRows; ++i){
+            for(int k = 0; i + k * cycleLen < n; ++k){
+                ans += s[i+k*cycleLen];
+                if(i != 0 && i != numRows-1 && (k+1)*cycleLen-i < n){
+                    ans += s[(k+1)*cycleLen-i];
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
