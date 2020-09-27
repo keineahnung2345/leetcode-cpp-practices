e7e3b543e1a86b5b9f00538ab0e42a3bb971fae2
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-27 10:08:58 +0800

    Approach 1: One pass

--- 495. Teemo Attacking.cpp
+++ 495. Teemo Attacking.cpp
@@ -22,3 +22,25 @@ public:
         return ans;
     }
 };
+
+//Approach 1: One pass
+//Runtime: 92 ms, faster than 92.37% of C++ online submissions for Teemo Attacking.
+//Memory Usage: 26.2 MB, less than 10.17% of C++ online submissions for Teemo Attacking.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n = timeSeries.size();
+        //edge case
+        if(n == 0) return 0;
+        
+        int ans = 0;
+        
+        for(int i = 1; i < n; ++i){
+            ans += min(duration, timeSeries[i]-timeSeries[i-1]);
+        }
+        
+        //last attack: duration
+        return ans + duration;
+    }
+};
