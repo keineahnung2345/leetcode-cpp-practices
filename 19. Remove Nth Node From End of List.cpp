7da16ec971f50f622421def9bb676b38c40cbdf3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-29 18:39:50 +0800

    use dummy head to avoid corner case

--- 19. Remove Nth Node From End of List.cpp
+++ 19. Remove Nth Node From End of List.cpp
@@ -44,3 +44,52 @@ public:
         return head;
     }
 };
+
+//Approach 1: Two pass algorithm
+//use dummy head to avoid corner case
+//Runtime: 8 ms, faster than 35.30% of C++ online submissions for Remove Nth Node From End of List.
+//Memory Usage: 10.9 MB, less than 18.24% of C++ online submissions for Remove Nth Node From End of List.
+//time: O(L), space: O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode* cur;
+        int len = 0;
+        
+        cur = dummy->next;
+        while(cur){
+            ++len;
+            cur = cur->next;
+        }
+        
+        // cout << "len: " << len << endl;
+        
+        cur = dummy;
+        for(int i = 0; i < len; ++i, cur = cur->next){
+            //i is 1-based
+            // cout << i << " : " << cur->val << endl;
+            if(i == len-n){
+                //this is the node previous to the node to be deleted
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+                break;
+            }
+        }
+        
+        return dummy->next;
+    }
+};
