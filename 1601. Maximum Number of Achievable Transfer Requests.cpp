191ed54b4810966b7519ecf6d7d7049faded5dcf
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-29 22:48:19 +0800

    enumerate all combination of requests

--- 1601. Maximum Number of Achievable Transfer Requests.cpp
+++ 1601. Maximum Number of Achievable Transfer Requests.cpp
@@ -89,3 +89,36 @@ public:
         return ans;
     }
 };
+
+//enumerate all combination of requests
+//https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/discuss/866456/Python-Check-All-Combinations
+//Runtime: 1468 ms, faster than 10.64% of C++ online submissions for Maximum Number of Achievable Transfer Requests.
+//Memory Usage: 357.3 MB, less than 5.03% of C++ online submissions for Maximum Number of Achievable Transfer Requests.
+//time: O((N+R)*(2^R)), space: O(N)
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        int nr = requests.size();
+        int ans = 0;
+        
+        //enumerate all combination of requests
+        for(int comb = 0; comb < (1 << nr); ++comb){
+            vector<int> out(n), in(n);
+            
+            for(int i = 0; i < nr; ++i){
+                // cout << comb << " " << i << endl;
+                if(comb & (1 << i)){
+                    //requests[i] is chosen
+                    ++out[requests[i][0]];
+                    ++in[requests[i][1]];
+                }
+            }
+            
+            if(in == out){
+                ans = max(ans, accumulate(out.begin(), out.end(), 0));
+            }
+        }
+        
+        return ans;
+    }
+};
