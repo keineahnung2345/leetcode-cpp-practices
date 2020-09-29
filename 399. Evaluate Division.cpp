9a3a0e279e140031ee87875259f67c47437408f2
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-29 13:35:39 +0800

    dfs

--- 399. Evaluate Division.cpp
+++ 399. Evaluate Division.cpp
@@ -163,3 +163,59 @@ public:
         return ans;
     }
 };
+
+//DFS
+//https://leetcode.com/problems/evaluate-division/discuss/171649/1ms-DFS-with-Explanations
+//Runtime: 4 ms, faster than 58.89% of C++ online submissions for Evaluate Division.
+//Memory Usage: 8.5 MB, less than 8.58% of C++ online submissions for Evaluate Division.
+class Solution {
+public:
+    double dfs(string start, string end, unordered_map<string, unordered_map<string, double>>& graph, unordered_set<string>& visited){
+        if(graph.find(start) == graph.end()){
+            return -1;
+        }
+        
+        if(graph[start].find(end) != graph[start].end()){
+            return graph[start][end];
+        }
+        
+        for(const auto& nei : graph[start]){
+            if(find(visited.begin(), visited.end(), nei.first) != visited.end())
+                continue;
+            visited.insert(nei.first);
+            double res;
+            if((res = dfs(nei.first, end, graph, visited)) != -1){
+                return graph[start][nei.first] * res;
+            }
+        }
+        
+        return -1;
+    }
+    
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, unordered_map<string, double>> quot;
+        
+        int N = equations.size();
+        
+        for(int i = 0; i < N; i++){
+            const vector<string>& equation = equations[i];
+            const string& num = equation[0], den = equation[1];
+            const double& value = values[i];
+            quot[num][num] = 1.0;
+            quot[den][den] = 1.0;
+            quot[num][den] = value;
+            quot[den][num] = 1.0/value;
+        }
+        
+        vector<double> ans;
+        unordered_set<string> visited;
+        
+        for(const vector<string>& query : queries){
+            const string& num = query[0], den = query[1];
+            visited = {num};
+            ans.push_back(dfs(num, den, quot, visited));
+        }
+        
+        return ans;
+    }
+};
