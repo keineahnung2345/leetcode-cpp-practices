e2dba084eba1cd806d0496ac27d211655bff7317
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-09-30 23:07:31 +0800

    Single Pass Approach

--- 31. Next Permutation.cpp
+++ 31. Next Permutation.cpp
@@ -28,3 +28,41 @@ public:
         sort(nums.begin(), nums.end());
     }
 };
+
+//Approach 2: Single Pass Approach
+//Runtime: 4 ms, faster than 92.92% of C++ online submissions for Next Permutation.
+//Memory Usage: 12.4 MB, less than 18.40% of C++ online submissions for Next Permutation.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        
+        int i = n-2;
+        
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            --i;
+        }
+        
+        if(i >= 0){
+            //nums[i] < nums[i+1]
+            int j = n-1;
+            
+            /*
+            find a j > i s.t. nums[j] > nums[i] and 
+            is the smallest in nums[i+1:],
+            to do this, we find from the end,
+            because nums[i+1:] is in descending order
+            */
+            while(j >= 0 && nums[j] <= nums[i]){
+                --j;
+            }
+            
+            swap(nums[i], nums[j]);
+            
+            //after swapping, nums[i+1:] is still in descending order
+        }
+        
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
