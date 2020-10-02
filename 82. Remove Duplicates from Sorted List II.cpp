d0b121a797d89dcc2b844d3f09f7a5ad7569fb90
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-02 16:49:35 +0800

    solution from discussion

--- 82. Remove Duplicates from Sorted List II.cpp
+++ 82. Remove Duplicates from Sorted List II.cpp
@@ -45,3 +45,40 @@ public:
         return dummy->next;
     }
 };
+
+//cleaner
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28335/My-accepted-Java-code
+//Runtime: 12 ms, faster than 63.08% of C++ online submissions for Remove Duplicates from Sorted List II.
+//Memory Usage: 11.4 MB, less than 19.33% of C++ online submissions for Remove Duplicates from Sorted List II.
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        
+        dummy->next = head;
+        
+        ListNode *slow = dummy, *fast = head;
+        
+        while(fast){
+            while(fast && fast->next && fast->val == fast->next->val){
+                fast = fast->next;
+            }
+            
+            if(slow->next == fast){
+                //fast points to non-duplicate value
+                //accept it and move forward
+                slow = slow->next;
+            }else{
+                //fast points to duplicate value
+                //so discard it and temporarily connect slow and fast->next
+                slow->next = fast->next;
+                //move fast forward
+                //in next iteration, we still need to check whether fast points to duplicate value
+                fast = fast->next;
+            }
+        }
+        
+        
+        return dummy->next;
+    }
+};
