fa99352d55da8e897082a3e15f0e4b788116e939
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-02 09:15:35 +0800

    two pointer

--- 80. Remove Duplicates from Sorted Array II.cpp
+++ 80. Remove Duplicates from Sorted Array II.cpp
@@ -30,3 +30,27 @@ public:
         return slow;
     }
 };
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/27976/3-6-easy-lines-C%2B%2B-Java-Python-Ruby
+//Runtime: 4 ms, faster than 99.33% of C++ online submissions for Remove Duplicates from Sorted Array II.
+//Memory Usage: 11.3 MB, less than 14.37% of C++ online submissions for Remove Duplicates from Sorted Array II.
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int slow = 0;
+        int k = 2;
+        
+        for(int fast = 0; fast < n; ++fast){
+            if(slow < k || nums[fast] != nums[slow-k]){
+                /*
+                only update nums[slow] when 
+                there are < k nums[fast] in the new array
+                */
+                nums[slow++] = nums[fast];
+            }
+        }
+        
+        return slow;
+    }
+};
