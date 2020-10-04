1ed1f5f80184b6c6ffefdc07167a664d89ca7fc4
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-04 21:40:43 +0800

    1606. Find Servers That Handled Most Number of Requests.cpp

---
+++ 1606. Find Servers That Handled Most Number of Requests.cpp
@@ -0,0 +1,35 @@
+//TLE
+//105 / 108 test cases passed.
+class Solution {
+public:
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) {
+        vector<int> servers(k);
+        vector<int> counts(k);
+        
+        int n = arrival.size();
+        int min_sid = 0;
+        for(int i = 0; i < n; ++i){
+            if(arrival[i] < servers[min_sid]) continue;
+            for(int j = 0; j < k; ++j){
+                int sid = (i%k+j)%k;
+                if(servers[sid] <= arrival[i]){
+                    servers[sid] = arrival[i] + load[i];
+                    ++counts[sid];
+                    min_sid = min_element(servers.begin(), servers.end()) - servers.begin();
+                    break;
+                }
+            }
+        }
+        
+        int max_count = *max_element(counts.begin(), counts.end());
+        vector<int> ans;
+        
+        for(int i = 0; i < counts.size(); ++i){
+            if(counts[i] == max_count){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
