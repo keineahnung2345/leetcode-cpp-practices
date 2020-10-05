1413ef8b31a3d13dcb2df408edd5141d57dd52ce
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-05 10:24:48 +0800

    use heap and set

--- 1606. Find Servers That Handled Most Number of Requests.cpp
+++ 1606. Find Servers That Handled Most Number of Requests.cpp
@@ -34,3 +34,60 @@ public:
         return ans;
     }
 };
+
+//use set and priority_queue to finding available servers
+//https://leetcode.com/problems/find-servers-that-handled-most-number-of-requests/discuss/876793/Java-O(nlogn)-use-both-TreeSet-and-PriorityQueue
+//Runtime: 1016 ms, faster than 50.00% of C++ online submissions for Find Servers That Handled Most Number of Requests.
+//Memory Usage: 118.3 MB, less than 25.00% of C++ online submissions for Find Servers That Handled Most Number of Requests.
+//time: O(nlogk), space: O(k)
+class Solution {
+public:
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) {
+        vector<int> counts(k);
+        set<int> availables;
+        
+        for(int i = 0; i < k; ++i){
+            availables.insert(i);
+        }
+        
+        //(end time, server id)
+        //the smaller end time, the earlier popped
+        auto comp = [](pair<int,int>& p1, pair<int,int>& p2){
+            return p1.first > p2.first;};
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> busys(comp);
+        
+        int n = arrival.size();
+        int min_sid = 0;
+        for(int i = 0; i < n; ++i){
+            int start = arrival[i];
+            int end = arrival[i] + load[i];
+            while(!busys.empty() && start >= busys.top().first){
+                pair<int,int> p = busys.top(); busys.pop();
+                availables.insert(p.second);
+            }
+            if(availables.empty()) continue;
+            //find >=
+            auto it = availables.lower_bound(i%k);
+            int sid;
+            if(it == availables.end()){
+                sid = *availables.begin();
+            }else{
+                sid = *it;
+            }
+            busys.push({end, sid});
+            ++counts[sid];
+            availables.erase(sid);
+        }
+        
+        int max_count = *max_element(counts.begin(), counts.end());
+        vector<int> ans;
+        
+        for(int i = 0; i < counts.size(); ++i){
+            if(counts[i] == max_count){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
