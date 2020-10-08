d3d8ed51a3b493976e15f7119e4c6850704e1930
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-08 18:36:34 +0800

    clean recursion

--- 98. Validate Binary Search Tree.cpp
+++ 98. Validate Binary Search Tree.cpp
@@ -1,3 +1,18 @@
+//Naive, only compare with its left and right children
+//WA: 70 / 75 test cases passed.
+//[10,5,15,null,null,6,20]
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        if(root->left && root->left->val >= root->val) return false;
+        if(root->right && root->right->val <= root->val) return false;
+        
+        return isValidBST(root->left) && isValidBST(root->right);
+    }
+};
+
+//Recursion
 //Runtime: 8 ms, faster than 99.49% of C++ online submissions for Validate Binary Search Tree.
 //Memory Usage: 22.2 MB, less than 5.05% of C++ online submissions for Validate Binary Search Tree.
 /**
@@ -54,5 +69,30 @@ public:
     bool isValidBST(TreeNode* root) {
         int rootmin = INT_MAX, rootmax = INT_MIN;
         return isValidBST(root, rootmin, rootmax);
+    }
+};
+
+//Approach 1: Recursion(cleaner)
+//Runtime: 16 ms, faster than 82.11% of C++ online submissions for Validate Binary Search Tree.
+//Memory Usage: 22 MB, less than 15.55% of C++ online submissions for Validate Binary Search Tree.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    bool recurse(TreeNode* node, long long lower, long long upper){
+        if(!node) return true;
+        
+        if(node->val <= lower) return false;
+        if(node->val >= upper) return false;
+        
+        if(!recurse(node->left, lower, node->val)) return false;
+        if(!recurse(node->right, node->val, upper)) return false;
+        
+        return true;
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        // cout << LLONG_MIN << endl;
+        // cout << LLONG_MAX << endl;
+        return recurse(root, LLONG_MIN, LLONG_MAX);
     }
 };
