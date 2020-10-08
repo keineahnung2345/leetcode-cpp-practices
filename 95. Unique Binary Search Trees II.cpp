42d0cf646233293fc2128636d696f5a230fc81b0
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-08 23:48:42 +0800

    bottom-up DP

--- 95. Unique Binary Search Trees II.cpp
+++ 95. Unique Binary Search Trees II.cpp
@@ -121,3 +121,50 @@ public:
         return memo[1][n];
     }
 };
+
+//bottom-up DP
+//https://leetcode.com/problems/unique-binary-search-trees-ii/discuss/31493/Java-Solution-with-DP
+//Runtime: 20 ms, faster than 66.68% of C++ online submissions for Unique Binary Search Trees II.
+//Memory Usage: 12.8 MB, less than 83.42% of C++ online submissions for Unique Binary Search Trees II.
+class Solution {
+public:
+    TreeNode* addOffset(TreeNode* node, int offset){
+        /*
+        clone the whole tree rooted at "node",
+        but adding "offset" to all of its descendents
+        */
+        if(!node) return nullptr;
+        TreeNode* newnode = new TreeNode(node->val+offset);
+        
+        newnode->left = addOffset(node->left, offset);
+        newnode->right = addOffset(node->right, offset);
+        
+        return newnode;
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {
+        if(n == 0) return {};
+        
+        vector<vector<TreeNode*>> dp(n+1);
+        
+        dp[0] = {nullptr};
+        
+        //count: size of tree
+        for(int count = 1; count <= n; ++count){
+            //val: root's value
+            for(int val = 1; val <= count; ++val){
+                // cout << count << ", " << val << endl;
+                for(TreeNode* ltree : dp[val-1]){
+                    for(TreeNode* rtree : dp[count-val]){
+                        TreeNode* node = new TreeNode(val);
+                        node->left = ltree;
+                        node->right = addOffset(rtree, val);
+                        dp[count].push_back(node);
+                    }
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
