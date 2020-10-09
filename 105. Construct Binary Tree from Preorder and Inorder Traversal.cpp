99ea4eb66f118abb42e72797f456669349a471d7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-09 10:32:58 +0800

    recursion

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,3 +1,4 @@
+//recursion
 //Runtime: 76 ms, faster than 12.67% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
 //Memory Usage: 75.4 MB, less than 5.21% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
 /**
@@ -29,5 +30,47 @@ public:
         root->right = buildTree(rpreorder, rinorder);
         
         return root;
+    }
+};
+
+//recursion
+//use index as argument to avoid copy the whole vector
+//use hashmap to record "split" values
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/discuss/34538/My-Accepted-Java-Solution
+//Runtime: 24 ms, faster than 77.75% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+//Memory Usage: 26.9 MB, less than 5.10% of C++ online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+class Solution {
+public:
+    unordered_map<int, int> inorder_indices;
+    
+    TreeNode* buildTree(vector<int>& preorder, int prestart, int preend, 
+        vector<int>& inorder, int instart, int inend){
+        //[prestart, preend], [inorder, inend] are inclusive
+        if(prestart > preend) return nullptr;
+        
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        
+        // int split = find(inorder.begin()+instart, 
+        //     inorder.begin()+inend+1, root->val)-inorder.begin();
+        int split = inorder_indices[root->val];
+        int left_size = split - instart;
+        
+        root->left = buildTree(preorder, prestart+1, prestart+1+left_size-1, 
+                               inorder, instart, split-1);
+        
+        root->right = buildTree(preorder, prestart+left_size+1, preend, 
+                                inorder, split+1, inend);
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = preorder.size();
+        
+        for(int i = 0; i < n; ++i){
+            inorder_indices[inorder[i]] = i;
+        }
+        
+        return buildTree(preorder, 0, n-1, inorder, 0, n-1);
     }
 };
