841c785cf40ae592d08b19b242031e4d2c69a679
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-10 22:37:38 +0800

    Approach 4: Single Pass Approach with Constant Space, slope based

--- 135. Candy.cpp
+++ 135. Candy.cpp
@@ -174,3 +174,56 @@ public:
         return accumulate(candies.begin(), candies.end(), 0);
     }
 };
+
+//Approach 4: Single Pass Approach with Constant Space, slope based
+//not understand
+//Runtime: 32 ms, faster than 98.43% of C++ online submissions for Candy.
+//Memory Usage: 17.5 MB, less than 5.93% of C++ online submissions for Candy.
+//time: O(N), space: O(1)
+class Solution {
+public:
+    int count(int n){
+        return n * (n+1) / 2;
+    }
+    
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        
+        if(n <= 1) return n;
+        
+        int candies = 0;
+        int up = 0, down = 0;
+        int old_slope = 0, new_slope;
+        
+        for(int i = 1; i < n; ++i){
+            if(ratings[i] > ratings[i-1]){
+                new_slope = 1;
+            }else if(ratings[i] == ratings[i-1]){
+                new_slope = 0;
+            }else{
+                new_slope = -1;
+            }
+            
+            if((old_slope >0 && new_slope == 0) ||
+              (old_slope < 0 && new_slope >= 0)){
+                candies += count(up) + count(down) + max(up, down);
+                up = down = 0;
+            }
+            
+            if(new_slope > 0){
+                ++up;
+            }else if(new_slope < 0){
+                ++down;
+            }else{
+                //new_slope == 0
+                ++candies;
+            }
+            
+            old_slope = new_slope;
+        }
+        
+        candies += count(up) + count(down) + max(up, down) + 1;
+        
+        return candies;
+    }
+};
