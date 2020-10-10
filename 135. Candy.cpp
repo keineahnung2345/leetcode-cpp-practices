113eb4119f53c03bdff10a6cc39651d7d664c2f3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-10 21:30:29 +0800

    Brute Force

--- 135. Candy.cpp
+++ 135. Candy.cpp
@@ -71,3 +71,38 @@ public:
         return accumulate(candies.begin(), candies.end(), 0);
     }
 };
+
+//Approach 1: Brute Force
+//TLE
+//47 / 48 test cases passed.
+//time: O(N^2), space: O(N)
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        
+        //give each child one candy at first time
+        vector<int> candies(n, 1);
+        
+        bool stop = false;
+        while(!stop){
+            stop = true;
+            for(int i = 0; i < n; ++i){
+                if(i > 0 && ratings[i] > ratings[i-1]){
+                    if(candies[i] <= candies[i-1]){
+                        stop = false;
+                        candies[i] = candies[i-1]+1;
+                    }
+                }
+                if(i < n-1 && ratings[i] > ratings[i+1]){
+                    if(candies[i] <= candies[i+1]){
+                        stop = false;
+                        candies[i] = candies[i+1]+1;
+                    }
+                }
+            }   
+        }
+        
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
