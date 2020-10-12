edeb5ec5041d534c5abb7d4e309f7ea296009bac
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-12 22:31:06 +0800

    1614. Maximum Nesting Depth of the Parentheses.cpp

---
+++ 1614. Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,24 @@
+//Runtime: 0 ms, faster than 100.00% of C++ online submissions for Maximum Nesting Depth of the Parentheses.
+//Memory Usage: 6.4 MB, less than 50.00% of C++ online submissions for Maximum Nesting Depth of the Parentheses.
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> stk;
+        int ans = 0;
+        
+        for(char c : s){
+            if(c == '('){
+                stk.push('(');
+                ans = max(ans, (int)stk.size());
+            }else if(c == ')'){
+                if(!stk.empty() && stk.top() == '('){
+                    stk.pop();
+                }else{
+                    return -1;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
