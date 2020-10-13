3d5b3d1b6fb2f4ab261c9c38fb0385970fefb329
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-13 18:26:56 +0800

    using the map old2new

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -50,3 +50,42 @@ public:
         return dummy->next;
     }
 };
+
+//using the map old2new
+//Runtime: 16 ms, faster than 58.60% of C++ online submissions for Copy List with Random Pointer.
+//Memory Usage: 11.5 MB, less than 12.63% of C++ online submissions for Copy List with Random Pointer.
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+        
+        Node* dummy = new Node(-1);
+        Node* newprev = dummy;
+        unordered_map<Node*, Node*> old2new;
+        
+        Node* cur = head;
+        
+        while(cur){
+            Node* node = new Node(cur->val);
+            //temporarily point to random nodes in old list
+            //later we will update it
+            node->random = cur->random;
+            old2new[cur] = node;
+            newprev->next = node;
+            newprev = newprev->next;
+            cur = cur->next;
+        }
+        
+        Node* newcur = dummy->next;
+        
+        cur = head;
+        newcur = dummy->next;
+        while(cur){
+            newcur->random = (newcur->random) ? old2new[newcur->random] : nullptr;
+            cur = cur->next;
+            newcur = newcur->next;
+        }
+        
+        return dummy->next;
+    }
+};
