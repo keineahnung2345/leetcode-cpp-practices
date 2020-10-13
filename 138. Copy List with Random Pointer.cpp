848a79315dfdf5deb5aae579b1758fee82795e1f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-13 20:50:18 +0800

    build interleaving list to save space

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -89,3 +89,48 @@ public:
         return dummy->next;
     }
 };
+
+//build interleaving list to save space
+//Runtime: 12 ms, faster than 91.43% of C++ online submissions for Copy List with Random Pointer.
+//Memory Usage: 11.3 MB, less than 12.63% of C++ online submissions for Copy List with Random Pointer.
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return nullptr;
+        
+        Node* cur = head;
+        
+        //bulid the interweaving list
+        while(cur){
+            Node* node = new Node(cur->val);
+            node->next = cur->next;
+            cur->next  = node;
+            cur = node->next;
+        }
+        
+        //assign "random"
+        cur = head;
+        Node* newcur;
+        
+        while(cur){
+            newcur = cur->next;
+            newcur->random = cur->random ? cur->random->next : nullptr;
+            cur = cur->next->next;
+        }
+        
+        //split the interleaving list to two lists
+        Node* newhead = head->next;
+        newcur = newhead;
+        cur = head;
+        while(newcur){
+            Node* next = cur->next ? cur->next->next : nullptr;
+            Node* newnext = newcur->next ? newcur->next->next : nullptr;
+            cur->next = cur->next ? cur->next->next : nullptr;
+            newcur->next = newcur->next ? newcur->next->next : nullptr;
+            newcur = newnext;
+            cur = next;
+        }
+        
+        return newhead;
+    }
+};
