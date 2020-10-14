6912b28e9fbf95f41cba49d0e0552662116f7b13
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-14 20:55:56 +0800

    radix sort

--- 164. Maximum Gap.cpp
+++ 164. Maximum Gap.cpp
@@ -19,3 +19,53 @@ public:
         return ans;
     }
 };
+
+//radix sort
+//https://openhome.cc/Gossip/AlgorithmGossip/RadixSort.htm
+//Runtime: 20 ms, faster than 22.30% of C++ online submissions for Maximum Gap.
+//Memory Usage: 12.7 MB, less than 10.99% of C++ online submissions for Maximum Gap.
+class Solution {
+public:
+    void radixSort(vector<int>& nums){
+        vector<vector<int>> buckets(10);
+        
+        int max_digit = 1 + log10(*max_element(nums.begin(), nums.end()));
+        
+        //least significant digital
+        for(int digit = 1; digit <= max_digit; ++digit){
+            for(const int& num : nums){
+                buckets[num%(int)pow(10, digit)/pow(10, digit-1)].push_back(num);
+            }
+            
+            vector<int> tmp;
+            
+            for(const vector<int>& bucket : buckets){
+                tmp.insert(tmp.end(), bucket.begin(), bucket.end());
+            }
+            
+            nums.swap(tmp);
+            buckets = vector<vector<int>>(10);
+        }
+        
+    }
+    
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) return 0;
+        
+        radixSort(nums);
+        
+        // for(int num : nums){
+        //     cout << num << " ";
+        // }
+        // cout << endl;
+        
+        int ans = 0;
+        
+        for(int i = 1; i < n; ++i){
+            ans = max(ans, nums[i] - nums[i-1]);
+        }
+        
+        return ans;
+    }
+};
