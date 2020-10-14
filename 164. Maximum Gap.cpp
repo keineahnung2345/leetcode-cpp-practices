7bdb924cc571a7f5ab8a2ab195484a84d05e7345
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-14 21:20:53 +0800

    Approach 2: Radix Sort

--- 164. Maximum Gap.cpp
+++ 164. Maximum Gap.cpp
@@ -69,3 +69,69 @@ public:
         return ans;
     }
 };
+
+//Approach 2: Radix Sort(official)
+//Radix sort uses Counting sort as a subroutine.
+//Runtime: 16 ms, faster than 48.38% of C++ online submissions for Maximum Gap.
+//Memory Usage: 9.1 MB, less than 10.99% of C++ online submissions for Maximum Gap.
+// radix sort: time: O(d*(n+radix)), space: O(n)
+// counting sort: time: O(n + radix), space: O(radix)
+class Solution {
+public:
+    void radixSort(vector<int>& nums){
+        int exp = 1;
+        int radix = 10;
+        
+        int maxVal = *max_element(nums.begin(), nums.end());
+        
+        vector<int> tmp(nums.size());
+        
+        while(maxVal/exp > 0){
+            // counting sort to sort current digit
+            vector<int> counter(radix, 0);
+            
+            for(const int& num : nums){
+                //say exp = pow(radix, p), then (num/exp)%radix is pth digit
+                ++counter[(num/exp)%radix];
+            }
+            
+            vector<int> counter_acc(counter.size());
+            /*
+            this is just like:
+            for(int i = 1; i < radix; ++i){
+                counter[i] += counter[i-1];
+            }
+            */
+            partial_sum(counter.begin(), counter.end(), counter_acc.begin());
+            
+            //fill from begin: so the sorting is stable
+            for(auto it = nums.rbegin(); it != nums.rend(); ++it){
+                tmp[--counter_acc[(*it/exp)%radix]] = *it;
+            }
+            
+            swap(nums, tmp);
+            
+            exp *= 10;
+        }
+    }
+    
+    int maximumGap(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) return 0;
+        
+        radixSort(nums);
+        
+        // for(int num : nums){
+        //     cout << num << " ";
+        // }
+        // cout << endl;
+        
+        int ans = 0;
+        
+        for(int i = 1; i < n; ++i){
+            ans = max(ans, nums[i] - nums[i-1]);
+        }
+        
+        return ans;
+    }
+};
