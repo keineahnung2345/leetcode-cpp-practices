e9381d52c5ad97dead029b1e9db94e8b00f11169
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-15 23:01:04 +0800

    dfs, preorder

--- 199. Binary Tree Right Side View.cpp
+++ 199. Binary Tree Right Side View.cpp
@@ -40,3 +40,36 @@ public:
         return ans;
     }
 };
+
+//dfs, preorder
+//Runtime: 4 ms, faster than 79.52% of C++ online submissions for Binary Tree Right Side View.
+//Memory Usage: 12.2 MB, less than 5.48% of C++ online submissions for Binary Tree Right Side View.
+class Solution {
+public:
+    vector<int> ans;
+    
+    void preorder(TreeNode* node, int depth){
+        if(!node) return;
+        
+        /*
+        only push value to the vector 
+        when we meet first element in that depth
+        
+        note that right child is visited before left child,
+        so the first element in that depth will be
+        the rightmost node in that depth
+        */
+        if(depth == ans.size()){
+            ans.push_back(node->val);
+        }
+        
+        preorder(node->right, depth+1);
+        preorder(node->left, depth+1);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        preorder(root, 0);
+        
+        return ans;
+    }
+};
