cb67dda2b6c4175b5230e012a3f6385351f9483a
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-15 21:40:02 +0800

    official sol

--- 173. Binary Search Tree Iterator.cpp
+++ 173. Binary Search Tree Iterator.cpp
@@ -80,3 +80,43 @@ public:
     }
 };
  
+//official
+//Approach 2: Controlled Recursion
+//Runtime: 68 ms, faster than 83.26% of C++ online submissions for Binary Search Tree Iterator.
+//Memory Usage: 28 MB, less than 10.86% of C++ online submissions for Binary Search Tree Iterator.
+//time for hasNext(): O(1)
+//time for next(): O(n) in worst case, amortized O(1)
+//space: O(h)
+class BSTIterator {
+public:
+    stack<TreeNode*> stk;
+    
+    void push_left(TreeNode* node){
+        while(node){
+            stk.push(node);
+            node = node->left;
+        }
+    }
+    
+    BSTIterator(TreeNode* root) {
+        //go left
+        push_left(root);
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* cur = stk.top(); stk.pop();
+        //visit itself
+        int val = cur->val;
+        //go right
+        if(cur->right){
+            push_left(cur->right);
+        }
+        return val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !stk.empty();
+    }
+};
