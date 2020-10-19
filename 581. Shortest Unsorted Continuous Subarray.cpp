11c892a136b2f2ffa02cab059898d77e2cfce4bd
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-19 18:09:02 +0800

    Approach 2: Better Brute Force

--- 581. Shortest Unsorted Continuous Subarray.cpp
+++ 581. Shortest Unsorted Continuous Subarray.cpp
@@ -27,3 +27,27 @@ public:
         return end - start + 1;
     }
 };
+
+//Approach 2: Better Brute Force
+//TLE
+//211 / 307 test cases passed.
+//time: O(N^2), space: O(1)
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int l = n, r = -1;
+        
+        for(int i = 0; i < n; ++i){
+            for(int j = i+1; j < n; ++j){
+                if(nums[j] < nums[i]){
+                    l = min(l, i);
+                    r = max(r, j);
+                }
+            }
+        }
+        
+        //r-l+1 could be negative
+        return max(0, r-l+1);
+    }
+};
