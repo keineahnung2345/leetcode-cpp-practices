6a8878d304b2c3ea74ad47bea6907e511912c088
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-19 20:23:12 +0800

    Approach 4: Using Stack

--- 581. Shortest Unsorted Continuous Subarray.cpp
+++ 581. Shortest Unsorted Continuous Subarray.cpp
@@ -53,3 +53,44 @@ public:
         return max(0, r-l+1);
     }
 };
+
+//Approach 4: Using Stack
+//Runtime: 72 ms, faster than 63.01% of C++ online submissions for Shortest Unsorted Continuous Subarray.
+//Memory Usage: 28 MB, less than 7.65% of C++ online submissions for Shortest Unsorted Continuous Subarray.
+//time: O(NlogN), space: O(N)
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int n = nums.size();
+        
+        stack<int> stk;
+        
+        int l = n, r = -1;
+        
+        for(int i = 0; i < n; ++i){
+            //pop all previous index s.t. they are larger than current element
+            //find the min l s.t. nums[l] is not at its right position
+            while(!stk.empty() && nums[stk.top()] > nums[i]){
+                l = min(l, stk.top()); stk.pop();
+                // cout << "l: " << l << ", " << i << endl;
+                // r = i; //this is wrong!
+            }
+            
+            stk.push(i);
+        }
+        
+        stk = stack<int>(); //!
+        for(int i = n-1; i >= 0; --i){
+            //find the max r s.t. nums[r] is not at its right position
+            while(!stk.empty() && nums[stk.top()] < nums[i]){
+                r = max(r, stk.top()); stk.pop();
+                // cout << "r: " << r << ", " << i << endl;
+            }
+            stk.push(i);
+        }
+        
+        // cout << "[" << l << ", " << r << "]" << endl;
+        
+        return max(0, r-l+1);
+    }
+};
