2559620e67a2d8b2f54b1e2481c6319fcb67cc43
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-20 18:39:39 +0800

    simplify backtrack argument: use two int instead of a vector

--- 1626. Best Team With No Conflicts.cpp
+++ 1626. Best Team With No Conflicts.cpp
@@ -45,7 +45,7 @@ public:
 
 //sort + backtrack
 //TLE
-//50 / 147 test cases passed.
+//52 / 147 test cases passed.
 //how to do memorization? There are at most 1000 players, hard to use bitmask as state!
 class Solution {
 public:
@@ -71,8 +71,9 @@ public:
             here we compare current player's score with last chosen player's score,
             and we know that last chosen player's score is the highest in its age
             */
+            //actually we don't need to check age!
             if(chosen.empty() || 
-               !(ages[p] > ages[chosen.back()] && scores[p] < scores[chosen.back()])){
+               !(/*ages[p] > ages[chosen.back()] && */ scores[p] < scores[chosen.back()])){
                 //choose cur
                 chosen.push_back(p);
                 backtrack(scores, ages, players, pi+1, chosen);
@@ -95,6 +96,53 @@ public:
         
         vector<int> chosen;
         backtrack(scores, ages, players, 0, chosen);
+        
+        return max_score;
+    }
+};
+
+//simplify backtrack argument: use two int instead of a vector
+//TLE
+//60 / 147 test cases passed.
+class Solution {
+public:
+    int max_score;
+    
+    void backtrack(vector<int>& scores, vector<int>& ages, vector<int>& players, int pi, int last_chosen, int cur_score){
+        if(pi == players.size()){
+            max_score = max(max_score, cur_score);
+        }else{
+            //not choose cur
+            backtrack(scores, ages, players, pi+1, last_chosen, cur_score);
+            
+            int p = players[pi];
+            /*
+            sort scores ascending so 
+            the last player in its age's group will have max score,
+            here we compare current player's score with last chosen player's score,
+            and we know that last chosen player's score is the highest in its age
+            */
+            if(last_chosen == -1 || 
+               !(/*ages[p] > ages[chosen.back()] && */scores[p] < scores[last_chosen])){
+                //choose cur
+                backtrack(scores, ages, players, pi+1, p, cur_score+scores[p]);
+            }
+        }
+    }
+    
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        int n = scores.size();
+        vector<int> players(n);
+        iota(players.begin(), players.end(), 0);
+        
+        //ascending by age, ascending by score
+        sort(players.begin(), players.end(), [&](int& pi1, int& pi2){
+            return (ages[pi1] == ages[pi2]) ? scores[pi1] < scores[pi2] : ages[pi1] < ages[pi2];
+        });
+        
+        max_score = 0;
+        
+        backtrack(scores, ages, players, 0, -1, 0);
         
         return max_score;
     }
