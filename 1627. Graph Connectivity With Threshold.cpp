9348bc9060e87d2b7fd1517d429bb45ef195a5b8
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-21 16:18:47 +0800

    graph

--- 1627. Graph Connectivity With Threshold.cpp
+++ 1627. Graph Connectivity With Threshold.cpp
@@ -1,3 +1,4 @@
+//graph
 //WA
 //57 / 66 test cases passed.
 //indirectly connection also considered true?
@@ -35,6 +36,75 @@ public:
             if(q[0] > q[1]) swap(q[0], q[1]);
             
             ans[i] = (edges.find(encode(q[0], q[1], n)) != edges.end());
+        }
+        
+        return ans;
+    }
+};
+
+//graph
+//fix above: indirectly connected cities should also be seen as connected
+//TLE
+//64 / 66 test cases passed.
+class Solution {
+public:
+    int gcd(int x, int y){
+        if(y == 0) return x;
+        return gcd(y, x%y);
+    }
+    
+    int encode(int& i, int& j, int& n){
+        return i*(n+1)+j;
+    }
+    
+    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
+        int m = queries.size();
+        vector<bool> ans(m, true);
+        
+        if(threshold == 0){
+            return ans;
+        }
+        
+        vector<unordered_set<int>> adjList(n+1);
+        
+        for(int i = 1; i <= n; ++i){
+            for(int j = i+1; j <= n; ++j){
+                if(gcd(i, j) > threshold){
+                    adjList[i].insert(j);
+                    adjList[j].insert(i);
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; ++i){
+            vector<int>& query = queries[i];
+            if(query[0] > query[1]) swap(query[0], query[1]);
+            
+            bool connected = false;
+            
+            queue<int> q;
+            vector<bool> visited(n+1, false);
+            int cur;
+            
+            q.push(query[0]);
+            visited[query[0]] = true;
+            
+            while(!q.empty()){
+                cur = q.front(); q.pop();
+                
+                if(cur == query[1]){
+                    connected = true;
+                    break;
+                }
+                
+                for(const int& nei : adjList[cur]){
+                    if(visited[nei]) continue;
+                    visited[nei] = true;
+                    q.push(nei);
+                }
+            }
+            
+            ans[i] = connected;
         }
         
         return ans;
