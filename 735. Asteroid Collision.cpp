357fa20f6d7b5c77dd6e1e548578b1a582f63b9d
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-10-21 18:13:09 +0800

    official sol

--- 735. Asteroid Collision.cpp
+++ 735. Asteroid Collision.cpp
@@ -42,3 +42,49 @@ public:
         return asteroids;
     }
 };
+
+//official sol
+//stack, keep going left until exploded
+//Runtime: 20 ms, faster than 95.41% of C++ online submissions for Asteroid Collision.
+//Memory Usage: 18.3 MB, less than 95.21% of C++ online submissions for Asteroid Collision.
+//time: O(N), space: O(N)
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stk;
+        
+        for(int& cur : asteroids){
+            bool exploded = false;
+            while(!stk.empty() && stk.top() > 0 && cur < 0){
+                int prev = stk.top();
+                if(abs(prev) > abs(cur)){
+                    //do nothing
+                    exploded = true;
+                    break;
+                }else if(abs(prev) == abs(cur)){
+                    stk.pop();
+                    exploded = true;
+                    break;
+                }else{
+                    //abs(prev) < abs(cur)
+                    stk.pop();
+                    //cur keeps going left
+                }
+            }
+
+            if(!exploded){
+                stk.push(cur);
+            }
+        }
+
+        asteroids.clear();
+
+        while(!stk.empty()){
+            asteroids.insert(asteroids.begin(), stk.top());
+            stk.pop();
+        }
+        
+        return asteroids;
+    }
+};
+
